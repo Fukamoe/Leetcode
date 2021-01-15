a168fdfa874aa8621dfc1475b2a190ec078f5431
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-01-15 23:52:09 +0800

    Update LeetcodePractice.cpp

--- LeetcodePractice.cpp
+++ LeetcodePractice.cpp
@@ -67,6 +67,8 @@ int Find(vector<int>& parent, int index);
 void Union(vector<int>& parent, int index1, int index2);
 vector<int> findRedundantConnection(vector<vector<int>>& edges);
 vector<bool> prefixesDivBy5(vector<int>& A);
+void dfs(int x, vector<vector<int>>& edge, vector<int>& vis);
+int removeStones(vector<vector<int>>& stones);
 
 int main()
 {
@@ -447,6 +449,38 @@ vector<bool> prefixesDivBy5(vector<int>& A)
 		list.emplace_back(prefix == 0);
 	}
 	return list;
+}
+
+void dfs(int x, vector<vector<int>>& edge, vector<int>& vis)
+{
+	vis[x] = true;
+	for (auto& y : edge[x]) {
+		if (!vis[y]) {
+			dfs(y, edge, vis);
+		}
+	}
+}
+
+int removeStones(vector<vector<int>>& stones)
+{
+	int n = stones.size();
+	vector<vector<int>> edge(n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+				edge[i].push_back(j);
+			}
+		}
+	}
+	vector<int> vis(n);
+	int num = 0;
+	for (int i = 0; i < n; i++) {
+		if (!vis[i]) {
+			num++;
+			dfs(i, edge, vis);
+		}
+	}
+	return n - num;
 }
 
 
