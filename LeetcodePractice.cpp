7258c8106b047060ab3f2238307f40d484cdb873
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-01-28 20:35:29 +0800

    Update LeetcodePractice.cpp

--- LeetcodePractice.cpp
+++ LeetcodePractice.cpp
@@ -1065,3 +1065,17 @@ int maximumProduct(vector<int>& nums)
 //		return ans;
 //	}
 //};
+//class Solution {
+//public:
+//	int pivotIndex(vector<int>& nums) {
+//		int total = accumulate(nums.begin(), nums.end(), 0);
+//		int sum = 0;
+//		for (int i = 0; i < nums.size(); ++i) {
+//			if (2 * sum + nums[i] == total) {
+//				return i;
+//			}
+//			sum += nums[i];
+//		}
+//		return -1;
+//	}
+//};
