008e36f4fe5f24614375a4a98a1188e9db2e9fc6
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-02-13 23:34:38 +0800

    Update February.cpp

--- February.cpp
+++ February.cpp
@@ -343,3 +343,20 @@
 //        return C[rowIndex];
 //    }
 //};
+//class Solution {
+//public:
+//    vector<int> findDisappearedNumbers(vector<int>& nums) {
+//        int n = nums.size();
+//        for (auto& num : nums) {
+//            int x = (num - 1) % n;
+//            nums[x] += n;
+//        }
+//        vector<int> ret;
+//        for (int i = 0; i < n; i++) {
+//            if (nums[i] <= n) {
+//                ret.push_back(i + 1);
+//            }
+//        }
+//        return ret;
+//    }
+//};
