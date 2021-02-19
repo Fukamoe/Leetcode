e5c691c939be7798a02684ec94f527dbab76d3df
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-02-19 20:34:43 +0800

    Update February.cpp

--- February.cpp
+++ February.cpp
@@ -469,3 +469,20 @@
 //        return ans;
 //    }
 //};
+//class Solution {
+//public:
+//    int longestOnes(vector<int>& A, int K) {
+//        int n = A.size();
+//        vector<int> P(n + 1);
+//        for (int i = 1; i <= n; ++i) {
+//            P[i] = P[i - 1] + (1 - A[i - 1]);
+//        }
+//
+//        int ans = 0;
+//        for (int right = 0; right < n; ++right) {
+//            int left = lower_bound(P.begin(), P.end(), P[right + 1] - K) - P.begin();
+//            ans = max(ans, right - left + 1);
+//        }
+//        return ans;
+//    }
+//};
