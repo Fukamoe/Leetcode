0bdbc67b8bd57addf45c5b4a817519cb9733991e
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-02-24 22:37:09 +0800

    Update February.cpp

--- February.cpp
+++ February.cpp
@@ -569,3 +569,24 @@
 //        return total + maxIncrease;
 //    }
 //};
+//class Solution {
+//public:
+//    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+//        int n = A.size();
+//        for (int i = 0; i < n; i++) {
+//            int left = 0, right = n - 1;
+//            while (left < right) {
+//                if (A[i][left] == A[i][right]) {
+//                    A[i][left] ^= 1;
+//                    A[i][right] ^= 1;
+//                }
+//                left++;
+//                right--;
+//            }
+//            if (left == right) {
+//                A[i][left] ^= 1;
+//            }
+//        }
+//        return A;
+//    }
+//};
