8ce6076d99bf99d02a8df6693027d78e840ee4af
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-03-01 23:45:39 +0800

    Update February.cpp

--- February.cpp
+++ February.cpp
@@ -706,3 +706,19 @@
 //        return is_sorted(A.begin(), A.end()) || is_sorted(A.rbegin(), A.rend());
 //    }
 //};
+//class NumArray {
+//public:
+//    vector<int> sums;
+//
+//    NumArray(vector<int>& nums) {
+//        int n = nums.size();
+//        sums.resize(n + 1);
+//        for (int i = 0; i < n; i++) {
+//            sums[i + 1] = sums[i] + nums[i];
+//        }
+//    }
+//
+//    int sumRange(int i, int j) {
+//        return sums[j + 1] - sums[i];
+//    }
+//};
