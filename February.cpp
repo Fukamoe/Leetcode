eda1a291fb60578395121aebf069f02593d164b0
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-03-03 22:08:32 +0800

    Update February.cpp

--- February.cpp
+++ February.cpp
@@ -747,3 +747,22 @@
 //        return sum;
 //    }
 //};
+//class Solution {
+//public:
+//    int countOnes(int x) {
+//        int ones = 0;
+//        while (x > 0) {
+//            x &= (x - 1);
+//            ones++;
+//        }
+//        return ones;
+//    }
+//
+//    vector<int> countBits(int num) {
+//        vector<int> bits(num + 1);
+//        for (int i = 0; i <= num; i++) {
+//            bits[i] = countOnes(i);
+//        }
+//        return bits;
+//    }
+//};
