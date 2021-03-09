f5466f2a3abc704db31dc627239633cbced4529f
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-03-09 23:23:36 +0800

    Update February.cpp

--- February.cpp
+++ February.cpp
@@ -909,3 +909,18 @@
 //        return f[n - 1];
 //    }
 //};
+//class Solution {
+//public:
+//    string removeDuplicates(string S) {
+//        string stk;
+//        for (char ch : S) {
+//            if (!stk.empty() && stk.back() == ch) {
+//                stk.pop_back();
+//            }
+//            else {
+//                stk.push_back(ch);
+//            }
+//        }
+//        return stk;
+//    }
+//};
