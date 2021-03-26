4b5313ef551ebd05e42593ac0f96d640e9c5954c
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-03-26 23:32:08 +0800

    Update February.cpp

--- February.cpp
+++ February.cpp
@@ -1398,3 +1398,23 @@
 //        return dummy->next;
 //    }
 //}
+//class Solution {
+//public:
+//    ListNode* deleteDuplicates(ListNode* head) {
+//        if (!head) {
+//            return head;
+//        }
+//
+//        ListNode* cur = head;
+//        while (cur->next) {
+//            if (cur->val == cur->next->val) {
+//                cur->next = cur->next->next;
+//            }
+//            else {
+//                cur = cur->next;
+//            }
+//        }
+//
+//        return head;
+//    }
+//};
