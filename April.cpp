10fdb22286e71baf547f736bd8f4daaaf64a5d8f
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-04-19 21:23:41 +0800

    Update April.cpp

--- April.cpp
+++ April.cpp
@@ -428,3 +428,17 @@
 //        return slow;
 //    }
 //};
+//class Solution {
+//public:
+//    int removeElement(vector<int>& nums, int val) {
+//        int n = nums.size();
+//        int left = 0;
+//        for (int right = 0; right < n; right++) {
+//            if (nums[right] != val) {
+//                nums[left] = nums[right];
+//                left++;
+//            }
+//        }
+//        return left;
+//    }
+//};
