4a181b0f89dc79769a5921464f25ccd7cc6d5f38
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-04-27 22:55:03 +0800

    Update April.cpp

--- April.cpp
+++ April.cpp
@@ -584,30 +584,46 @@
 //        return dummyNode->right;
 //    }
 //};
-class Solution {
-public:
-    int shipWithinDays(vector<int>& weights, int D) {
-        // 确定二分查找左右边界
-        int left = *max_element(weights.begin(), weights.end()), right = accumulate(weights.begin(), weights.end(), 0);
-        while (left < right) {
-            int mid = (left + right) / 2;
-            // need 为需要运送的天数
-            // cur 为当前这一天已经运送的包裹重量之和
-            int need = 1, cur = 0;
-            for (int weight : weights) {
-                if (cur + weight > mid) {
-                    ++need;
-                    cur = 0;
-                }
-                cur += weight;
-            }
-            if (need <= D) {
-                right = mid;
-            }
-            else {
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
-};
+//class Solution {
+//public:
+//    int shipWithinDays(vector<int>& weights, int D) {
+//        // 确定二分查找左右边界
+//        int left = *max_element(weights.begin(), weights.end()), right = accumulate(weights.begin(), weights.end(), 0);
+//        while (left < right) {
+//            int mid = (left + right) / 2;
+//            // need 为需要运送的天数
+//            // cur 为当前这一天已经运送的包裹重量之和
+//            int need = 1, cur = 0;
+//            for (int weight : weights) {
+//                if (cur + weight > mid) {
+//                    ++need;
+//                    cur = 0;
+//                }
+//                cur += weight;
+//            }
+//            if (need <= D) {
+//                right = mid;
+//            }
+//            else {
+//                left = mid + 1;
+//            }
+//        }
+//        return left;
+//    }
+//};
+//class Solution {
+//public:
+//    int rangeSumBST(TreeNode* root, int low, int high) {
+//        if (root == nullptr) {
+//            return 0;
+//        }
+//        if (root->val > high) {
+//            return rangeSumBST(root->left, low, high);
+//        }
+//        if (root->val < low) {
+//            return rangeSumBST(root->right, low, high);
+//        }
+//        return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+//    }
+//};
+
