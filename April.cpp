816eab60375f15eaf0061191f6625ad71aaf66ae
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-05-01 23:33:51 +0800

    Update April.cpp

--- April.cpp
+++ April.cpp
@@ -672,3 +672,23 @@
 //        return ans;
 //    }
 //};
+//class Solution {
+//public:
+//    unordered_map<int, Employee*> mp;
+//
+//    int dfs(int id) {
+//        Employee* employee = mp[id];
+//        int total = employee->importance;
+//        for (int subId : employee->subordinates) {
+//            total += dfs(subId);
+//        }
+//        return total;
+//    }
+//
+//    int getImportance(vector<Employee*> employees, int id) {
+//        for (auto& employee : employees) {
+//            mp[employee->id] = employee;
+//        }
+//        return dfs(id);
+//    }
+//};
