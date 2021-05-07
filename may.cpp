97e81cbbfae13577f731a06fb52025593de20253
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-05-07 22:07:44 +0800

    Update may.cpp

--- may.cpp
+++ may.cpp
@@ -104,3 +104,13 @@
 //        return arr;
 //    }
 //};
+//class Solution {
+//public:
+//    int xorOperation(int n, int start) {
+//        int ans = 0;
+//        for (int i = 0; i < n; ++i) {
+//            ans ^= (start + i * 2);
+//        }
+//        return ans;
+//    }
+//};
