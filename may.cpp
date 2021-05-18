533ae211995a72411a769d3728ba9ce68002f6ea
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-05-18 23:05:36 +0800

    Update may.cpp

--- may.cpp
+++ may.cpp
@@ -466,3 +466,24 @@
 //        return x_depth == y_depth && x_parent != y_parent;
 //    }
 //};
+//class Solution {
+//public:
+//    int countTriplets(vector<int>& arr) {
+//        int n = arr.size();
+//        vector<int> s(n + 1);
+//        for (int i = 0; i < n; ++i) {
+//            s[i + 1] = s[i] ^ arr[i];
+//        }
+//        int ans = 0;
+//        for (int i = 0; i < n; ++i) {
+//            for (int j = i + 1; j < n; ++j) {
+//                for (int k = j; k < n; ++k) {
+//                    if (s[i] == s[k + 1]) {
+//                        ++ans;
+//                    }
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+//};
