fdda966cd430c1a39ccb065ca1202da68c0b5f60
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-05-22 22:59:59 +0800

    Update may.cpp

--- may.cpp
+++ may.cpp
@@ -542,4 +542,18 @@
 //        return dp[m][n];
 //    }
 //};
+//class Solution {
+//public:
+//    bool xorGame(vector<int>& nums) {
+//        if (nums.size() % 2 == 0) {
+//            return true;
+//        }
+//        int xorsum = 0;
+//        for (int num : nums) {
+//            xorsum ^= num;
+//        }
+//        return xorsum == 0;
+//    }
+//};
+
 
