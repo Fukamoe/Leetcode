bbde5cc940f775460a4eb8e6982c2987fa9ee121
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-05-26 21:23:21 +0800

    Update may.cpp

--- may.cpp
+++ may.cpp
@@ -681,5 +681,28 @@
 //        return f[0];
 //    }
 //};
+//class Solution {
+//public:
+//    string reverseParentheses(string s) {
+//        stack<string> stk;
+//        string str;
+//        for (auto& ch : s) {
+//            if (ch == '(') {
+//                stk.push(str);
+//                str = "";
+//            }
+//            else if (ch == ')') {
+//                reverse(str.begin(), str.end());
+//                str = stk.top() + str;
+//                stk.pop();
+//            }
+//            else {
+//                str.push_back(ch);
+//            }
+//        }
+//        return str;
+//    }
+//};
+
 
 
