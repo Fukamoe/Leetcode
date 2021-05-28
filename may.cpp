b05ca8e9daa78a4ab9f775d59ec718aa1f7ca87c
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-05-28 23:34:11 +0800

    Update may.cpp

--- may.cpp
+++ may.cpp
@@ -709,6 +709,20 @@
 //        return __builtin_popcount(x ^ y);
 //    }
 //};
+//class Solution {
+//public:
+//    int totalHammingDistance(vector<int>& nums) {
+//        int ans = 0, n = nums.size();
+//        for (int i = 0; i < 30; ++i) {
+//            int c = 0;
+//            for (int val : nums) {
+//                c += (val >> i) & 1;
+//            }
+//            ans += c * (n - c);
+//        }
+//        return ans;
+//    }
+//};
 
 
 
