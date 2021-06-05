a192825c840d4a688991b9c38ca37e9f3e8e5eb0
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-06-05 23:05:54 +0800

    Update may.cpp

--- may.cpp
+++ may.cpp
@@ -867,6 +867,16 @@
 //        return nullptr;
 //    }
 //};
+//class Solution {
+//public:
+//    ListNode* removeElements(ListNode* head, int val) {
+//        if (head == nullptr) {
+//            return head;
+//        }
+//        head->next = removeElements(head->next, val);
+//        return head->val == val ? head->next : head;
+//    }
+//};
 
 
 
