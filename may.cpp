8223819a2cd38fd2135c05da68e9cdc6fe999ba7
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-06-15 22:47:32 +0800

    Update may.cpp

--- may.cpp
+++ may.cpp
@@ -970,6 +970,20 @@
 //         return left;
 //     }
 // };
+//class Solution {
+//public:
+//    int peakIndexInMountainArray(vector<int>& arr) {
+//        int n = arr.size();
+//        int ans = -1;
+//        for (int i = 1; i < n - 1; ++i) {
+//            if (arr[i] > arr[i + 1]) {
+//                ans = i;
+//                break;
+//            }
+//        }
+//        return ans;
+//    }
+//};
 
 
 
