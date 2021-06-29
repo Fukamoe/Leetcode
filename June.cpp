37f521e1e6f43269f18ef4d63cb333aed62f42d0
Fukamoe <76830297+Fukamoe@users.noreply.github.com>
2021-06-29 17:05:12 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -131,3 +131,16 @@
 //        return ret == INT_MAX ? -1 : ret;
 //    }
 //};
+//class Solution {
+//public:
+//    string convertToTitle(int columnNumber) {
+//        string ans;
+//        while (columnNumber > 0) {
+//            int a0 = (columnNumber - 1) % 26 + 1;
+//            ans += a0 - 1 + 'A';
+//            columnNumber = (columnNumber - a0) / 26;
+//        }
+//        reverse(ans.begin(), ans.end());
+//        return ans;
+//    }
+//};
