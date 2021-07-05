c34d0a00cf2596809c530c45be07f35bb37e5314
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-07-05 21:12:23 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,89 +1,64 @@
 class Solution {
 public:
-    int numWays(int n, vector<vector<int>> &relation, int k) {
-        vector<vector<int>> edges(n);
-        for (auto &edge : relation) {
-            int src = edge[0], dst = edge[1];
-            edges[src].push_back(dst);
+    string countOfAtoms(string formula) {
+        int i = 0, n = formula.length();
+
+        auto parseAtom = [&]() -> string {
+            string atom;
+            atom += formula[i++]; // 扫描首字母
+            while (i < n && islower(formula[i])) {
+                atom += formula[i++]; // 扫描首字母后的小写字母
+            }
+            return atom;
+        };
+
+        auto parseNum = [&]() -> int {
+            if (i == n || !isdigit(formula[i])) {
+                return 1; // 不是数字，视作 1
+            }
+            int num = 0;
+            while (i < n && isdigit(formula[i])) {
+                num = num * 10 + int(formula[i++] - '0'); // 扫描数字
+            }
+            return num;
+        };
+
+        stack<unordered_map<string, int>> stk;
+        stk.push({});
+        while (i < n) {
+            char ch = formula[i];
+            if (ch == '(') {
+                i++;
+                stk.push({}); // 将一个空的哈希表压入栈中，准备统计括号内的原子数量
+            } else if (ch == ')') {
+                i++;
+                int num = parseNum(); // 括号右侧数字
+                auto atomNum = stk.top();
+                stk.pop(); // 弹出括号内的原子数量
+                for (auto &[atom, v] : atomNum) {
+                    stk.top()[atom] += v * num; // 将括号内的原子数量乘上 num，加到上一层的原子数量中
+                }
+            } else {
+                string atom = parseAtom();
+                int num = parseNum();
+                stk.top()[atom] += num; // 统计原子数量
+            }
         }
 
-        int ways = 0;
-        function<void(int, int)> dfs = [&](int index, int steps) {
-            if (steps == k) {
-                if (index == n - 1) {
-                    ++ways;
-                }
-                return;
-            }
-            for (int to : edges[index]) {
-                dfs(to, steps + 1);
-            }
-        };
-        dfs(0, 0);
-        return ways;
-    }
-};
-class Solution {
-public:
-    int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(), costs.end());
-        int count = 0;
-        int n = costs.size();
-        for (int i = 0; i < n; i++) {
-            int cost = costs[i];
-            if (coins >= cost) {
-                coins -= cost;
-                count++;
-            } else {
-                break;
+        auto &atomNum = stk.top();
+        vector<pair<string, int>> pairs;
+        for (auto &[atom, v] : atomNum) {
+            pairs.emplace_back(atom, v);
+        }
+        sort(pairs.begin(), pairs.end());
+
+        string ans;
+        for (auto &p : pairs) {
+            ans += p.first;
+            if (p.second > 1) {
+                ans += to_string(p.second);
             }
         }
-        return count;
-    }
-};
-class Solution {
-public:
-    string frequencySort(string s) {
-        unordered_map<char, int> mp;
-        int length = s.length();
-        for (auto &ch : s) {
-            mp[ch]++;
-        }
-        vector<pair<char, int>> vec;
-        for (auto &it : mp) {
-            vec.emplace_back(it);
-        }
-        sort(vec.begin(), vec.end(), [](const pair<char, int> &a, const pair<char, int> &b) {
-            return a.second > b.second;
-        });
-        string ret;
-        for (auto &[ch, num] : vec) {
-            for (int i = 0; i < num; i++) {
-                ret.push_back(ch);
-            }
-        }
-        return ret;
-    }
-};
-class Solution {
-public:
-    vector<int> findErrorNums(vector<int>& nums) {
-        vector<int> errorNums(2);
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int prev = 0;
-        for (int i = 0; i < n; i++) {
-            int curr = nums[i];
-            if (curr == prev) {
-                errorNums[0] = prev;
-            } else if (curr - prev > 1) {
-                errorNums[1] = prev + 1;
-            }
-            prev = curr;
-        }
-        if (nums[n - 1] != n) {
-            errorNums[1] = n;
-        }
-        return errorNums;
+        return ans;
     }
 };
