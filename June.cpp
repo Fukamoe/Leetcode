1436195c3a21d8568d92a7bf598535b49a00c0a4
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-07-08 22:25:50 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -61,3 +61,17 @@ public:
         return pairs;
     }
 };
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int sum = 0;
+        unordered_map<int, int> cnt;
+        int ret = 0;
+        for (auto& num : nums) {
+            cnt[sum]++;
+            sum += num;
+            ret += cnt[sum - goal];
+        }
+        return ret;
+    }
+};
