8e823d32af5da74894cf3ee50a15d85d3d26b5a0
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-07-09 22:30:10 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,77 +1,25 @@
 class Solution {
 public:
-    vector<vector<string>> displayTable(vector<vector<string>> &orders) {
-        // 从订单中获取餐品名称和桌号，统计每桌点餐数量
-        unordered_set<string> nameSet;
-        unordered_map<int, unordered_map<string, int>> foodsCnt;
-        for (auto &order : orders) {
-            nameSet.insert(order[2]);
-            int id = stoi(order[1]);
-            ++foodsCnt[id][order[2]];
-        }
-
-        // 提取餐品名称，并按字母顺序排列
-        int n = nameSet.size();
-        vector<string> names;
-        for (auto &name : nameSet) {
-            names.push_back(name);
-        }
-        sort(names.begin(), names.end());
-
-        // 提取桌号，并按餐桌桌号升序排列
-        int m = foodsCnt.size();
-        vector<int> ids;
-        for (auto &[id, _] : foodsCnt) {
-            ids.push_back(id);
-        }
-        sort(ids.begin(), ids.end());
-
-        // 填写点菜展示表
-        vector<vector<string>> table(m + 1, vector<string>(n + 1));
-        table[0][0] = "Table";
-        copy(names.begin(), names.end(), table[0].begin() + 1);
-        for (int i = 0; i < m; ++i) {
-            int id = ids[i];
-            auto &cnt = foodsCnt[id];
-            table[i + 1][0] = to_string(id);
-            for (int j = 0; j < n; ++j) {
-                table[i + 1][j + 1] = to_string(cnt[names[j]]);
+    int majorityElement(vector<int>& nums) {
+        int candidate = -1;
+        int count = 0;
+        for (int& num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
             }
         }
-        return table;
-    }
-};
-class Solution {
-public:
-    static constexpr int MOD = 1'000'000'007;
-
-    int countPairs(vector<int>& deliciousness) {
-        int maxVal = *max_element(deliciousness.begin(), deliciousness.end());
-        int maxSum = maxVal * 2;
-        int pairs = 0;
-        unordered_map<int, int> mp;
-        int n = deliciousness.size();
-        for (auto& val : deliciousness) {
-            for (int sum = 1; sum <= maxSum; sum <<= 1) {
-                int count = mp.count(sum - val) ? mp[sum - val] : 0;
-                pairs = (pairs + count) % MOD;
+        count = 0;
+        int length = nums.size();
+        for (int& num : nums) {
+            if (num == candidate) {
+                count++;
             }
-            mp[val]++;
         }
-        return pairs;
-    }
-};
-class Solution {
-public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int sum = 0;
-        unordered_map<int, int> cnt;
-        int ret = 0;
-        for (auto& num : nums) {
-            cnt[sum]++;
-            sum += num;
-            ret += cnt[sum - goal];
-        }
-        return ret;
+        return count * 2 > length ? candidate : -1;
     }
 };
