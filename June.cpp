a5acf6bda4044ae0d93218b2a964ae174cfd39e5
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-07-11 22:27:34 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,29 +1,4 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int candidate = -1;
-        int count = 0;
-        for (int& num : nums) {
-            if (count == 0) {
-                candidate = num;
-            }
-            if (num == candidate) {
-                count++;
-            } else {
-                count--;
-            }
-        }
-        count = 0;
-        int length = nums.size();
-        for (int& num : nums) {
-            if (num == candidate) {
-                count++;
-            }
-        }
-        return count * 2 > length ? candidate : -1;
-    }
-};
- class TimeMap {
+class TimeMap {
     unordered_map<string, vector<pair<int, string>>> m;
 
 public:
