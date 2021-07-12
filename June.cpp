4ce04555185f81461fd4c4f6b119ac3fe4fd8bfa
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-07-12 22:45:01 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,21 +1,28 @@
-class TimeMap {
-    unordered_map<string, vector<pair<int, string>>> m;
-
+class Solution {
 public:
-    TimeMap() {}
-
-    void set(string key, string value, int timestamp) {
-        m[key].emplace_back(timestamp, value);
-    }
-
-    string get(string key, int timestamp) {
-        auto &pairs = m[key];
-        // 使用一个大于所有 value 的字符串，以确保在 pairs 中含有 timestamp 的情况下也返回大于 timestamp 的位置
-        pair<int, string> p = {timestamp, string({127})};
-        auto i = upper_bound(pairs.begin(), pairs.end(), p);
-        if (i != pairs.begin()) {
-            return (i - 1)->second;
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int h = 0, i = citations.size() - 1;
+        while (i >= 0 && citations[i] > h) {
+            h++;
+            i--;
         }
-        return "";
+        return h;
+    }
+};
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (citations[mid] >= n - mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return n - left;
     }
 };
