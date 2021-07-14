bc0de3f2f217274257437d6b45bb0b9f9a355699
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-07-14 23:33:16 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,60 +1,23 @@
 class Solution {
 public:
-    int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end());
-        int h = 0, i = citations.size() - 1;
-        while (i >= 0 && citations[i] > h) {
-            h++;
-            i--;
-        }
-        return h;
-    }
-};
-class Solution {
-public:
-    int hIndex(vector<int>& citations) {
-        int n = citations.size();
-        int left = 0, right = n - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (citations[mid] >= n - mid) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return n - left;
-    }
-};
-class Solution {
-public:
-    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
-        auto cmp = [](const pair<int, int>& a, const pair<int, int>& b) -> bool { return a.second < b.second; };
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> que(cmp);
+    static constexpr int mod = 1'000'000'007;
 
-        vector<int> boundaries;
-        for (auto& building : buildings) {
-            boundaries.emplace_back(building[0]);
-            boundaries.emplace_back(building[1]);
-        }
-        sort(boundaries.begin(), boundaries.end());
-
-        vector<vector<int>> ret;
-        int n = buildings.size(), idx = 0;
-        for (auto& boundary : boundaries) {
-            while (idx < n && buildings[idx][0] <= boundary) {
-                que.emplace(buildings[idx][1], buildings[idx][2]);
-                idx++;
+    int minAbsoluteSumDiff(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> rec(nums1);
+        sort(rec.begin(), rec.end());
+        int sum = 0, maxn = 0;
+        int n = nums1.size();
+        for (int i = 0; i < n; i++) {
+            int diff = abs(nums1[i] - nums2[i]);
+            sum = (sum + diff) % mod;
+            int j = lower_bound(rec.begin(), rec.end(), nums2[i]) - rec.begin();
+            if (j < n) {
+                maxn = max(maxn, diff - (rec[j] - nums2[i]));
             }
-            while (!que.empty() && que.top().first <= boundary) {
-                que.pop();
-            }
-
-            int maxn = que.empty() ? 0 : que.top().second;
-            if (ret.size() == 0 || maxn != ret.back()[1]) {
-                ret.push_back({boundary, maxn});
+            if (j > 0) {
+                maxn = max(maxn, diff - (nums2[i] - rec[j - 1]));
             }
         }
-        return ret;
+        return (sum - maxn + mod) % mod;
     }
 };
