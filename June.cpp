4602c62c5334f3230ea0db90432cba2c9e20990a
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-07-15 23:32:14 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -21,3 +21,15 @@ public:
         return (sum - maxn + mod) % mod;
     }
 };
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int> &arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        arr[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            arr[i] = min(arr[i], arr[i - 1] + 1);
+        }
+        return arr.back();
+    }
+};
