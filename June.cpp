87ca5095ec9aff8b78522d538d28ddb62d94f6f8
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-07-17 23:56:53 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums, int target, bool lower) {
-        int left = 0, right = (int)nums.size() - 1, ans = (int)nums.size();
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] > target || (lower && nums[mid] >= target)) {
-                right = mid - 1;
-                ans = mid;
-            } else {
-                left = mid + 1;
-            }
+    int maxSubArray(vector<int>& nums) {
+        int pre = 0, maxAns = nums[0];
+        for (const auto &x: nums) {
+            pre = max(pre + x, x);
+            maxAns = max(maxAns, pre);
         }
-        return ans;
-    }
-
-    int search(vector<int>& nums, int target) {
-        int leftIdx = binarySearch(nums, target, true);
-        int rightIdx = binarySearch(nums, target, false) - 1;
-        if (leftIdx <= rightIdx && rightIdx < nums.size() && nums[leftIdx] == target && nums[rightIdx] == target) {
-            return rightIdx - leftIdx + 1;
-        }
-        return 0;
+        return maxAns;
     }
 };
