03d4d876269ac8ee889cfbaabcd690bf8b7f00c1
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-07-20 22:41:49 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,44 +1,11 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int pre = 0, maxAns = nums[0];
-        for (const auto &x: nums) {
-            pre = max(pre + x, x);
-            maxAns = max(maxAns, pre);
-        }
-        return maxAns;
-    }
-};
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        for (string& str: strs) {
-            string key = str;
-            sort(key.begin(), key.end());
-            mp[key].emplace_back(str);
-        }
-        vector<vector<string>> ans;
-        for (auto it = mp.begin(); it != mp.end(); ++it) {
-            ans.emplace_back(it->second);
-        }
-        return ans;
-    }
-};
-class Solution {
-public:
-    int maxFrequency(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
+    int minPairSum(vector<int>& nums) {
         int n = nums.size();
-        long long total = 0;
-        int l = 0, res = 1;
-        for (int r = 1; r < n; ++r) {
-            total += (long long)(nums[r] - nums[r - 1]) * (r - l);
-            while (total > k) {
-                total -= nums[r] - nums[l];
-                ++l;
-            }
-            res = max(res, r - l + 1);
+        int res = 0;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n / 2; ++i) {
+            res = max(res, nums[i] + nums[n - 1 - i]);
         }
         return res;
     }
