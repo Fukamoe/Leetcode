2ae2a93d92ab122f210e1b797bd5e52e88e4b4c9
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-07-25 21:47:46 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    string maximumTime(string time) {
-        if (time[0] == '?') {
-            time[0] = ('4' <= time[1] && time[1] <= '9') ? '1' : '2';
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        unordered_map<int, vector<int>> mp;
+        for (auto& adjacentPair : adjacentPairs) {
+            mp[adjacentPair[0]].push_back(adjacentPair[1]);
+            mp[adjacentPair[1]].push_back(adjacentPair[0]);
         }
-        if (time[1] == '?') {
-            time[1] = (time[0] == '2') ? '3' : '9';
+
+        int n = adjacentPairs.size() + 1;
+        vector<int> ret(n);
+        for (auto& [e, adj] : mp) {
+            if (adj.size() == 1) {
+                ret[0] = e;
+                break;
+            }
         }
-        if (time[3] == '?') {
-            time[3] = '5';
+
+        ret[1] = mp[ret[0]][0];
+        for (int i = 2; i < n; i++) {
+            auto& adj = mp[ret[i - 1]];
+            ret[i] = ret[i - 2] == adj[0] ? adj[1] : adj[0];
         }
-        if (time[4] == '?') {
-            time[4] = '9';
-        }
-        return time;
+        return ret;
     }
 };
