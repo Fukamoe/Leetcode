6e050e137a76d4f3b1809ff507f67ae1ad883bde
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-07-26 23:09:08 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
-        unordered_map<int, vector<int>> mp;
-        for (auto& adjacentPair : adjacentPairs) {
-            mp[adjacentPair[0]].push_back(adjacentPair[1]);
-            mp[adjacentPair[1]].push_back(adjacentPair[0]);
+    int minOperations(vector<int> &target, vector<int> &arr) {
+        int n = target.size();
+        unordered_map<int, int> pos;
+        for (int i = 0; i < n; ++i) {
+            pos[target[i]] = i;
         }
-
-        int n = adjacentPairs.size() + 1;
-        vector<int> ret(n);
-        for (auto& [e, adj] : mp) {
-            if (adj.size() == 1) {
-                ret[0] = e;
-                break;
+        vector<int> d;
+        for (int val : arr) {
+            if (pos.count(val)) {
+                int idx = pos[val];
+                auto it = lower_bound(d.begin(), d.end(), idx);
+                if (it != d.end()) {
+                    *it = idx;
+                } else {
+                    d.push_back(idx);
+                }
             }
         }
-
-        ret[1] = mp[ret[0]][0];
-        for (int i = 2; i < n; i++) {
-            auto& adj = mp[ret[i - 1]];
-            ret[i] = ret[i - 2] == adj[0] ? adj[1] : adj[0];
-        }
-        return ret;
+        return n - d.size();
     }
 };
