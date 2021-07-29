d89d63595d28f59ccca1b911f52a6313535b532d
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-07-29 23:46:16 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,45 +1,29 @@
 class Solution {
-    unordered_map<int, TreeNode*> parents;
-    vector<int> ans;
-
-    void findParents(TreeNode* node) {
-        if (node->left != nullptr) {
-            parents[node->left->val] = node;
-            findParents(node->left);
-        }
-        if (node->right != nullptr) {
-            parents[node->right->val] = node;
-            findParents(node->right);
-        }
-    }
-
-    void findAns(TreeNode* node, TreeNode* from, int depth, int k) {
-        if (node == nullptr) {
-            return;
-        }
-        if (depth == k) {
-            ans.push_back(node->val);
-            return;
-        }
-        if (node->left != from) {
-            findAns(node->left, node, depth + 1, k);
-        }
-        if (node->right != from) {
-            findAns(node->right, node, depth + 1, k);
-        }
-        if (parents[node->val] != from) {
-            findAns(parents[node->val], node, depth + 1, k);
-        }
-    }
-
 public:
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        // 从 root 出发 DFS，记录每个结点的父结点
-        findParents(root);
+    int getReverse(int label, int row) {
+        return (1 << row - 1) + (1 << row) - 1 - label;
+    }
 
-        // 从 target 出发 DFS，寻找所有深度为 k 的结点
-        findAns(target, nullptr, 0, k);
-
-        return ans;
+    vector<int> pathInZigZagTree(int label) {
+        int row = 1, rowStart = 1;
+        while (rowStart * 2 <= label) {
+            row++;
+            rowStart *= 2;
+        }
+        if (row % 2 == 0) {
+            label = getReverse(label, row);
+        }
+        vector<int> path;
+        while (row > 0) {
+            if (row % 2 == 0) {
+                path.push_back(getReverse(label, row));
+            } else {
+                path.push_back(label);
+            }
+            row--;
+            label >>= 1;
+        }
+        reverse(path.begin(), path.end());
+        return path;
     }
 };
