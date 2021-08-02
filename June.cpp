57b102e566ca092e7c6d95d103daadacbdf0a780
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-08-02 23:08:39 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        int m = mat.size(), n = mat[0].size();
-        vector<pair<int, int>> power;
-        for (int i = 0; i < m; ++i) {
-            int l = 0, r = n - 1, pos = -1;
-            while (l <= r) {
-                int mid = (l + r) / 2;
-                if (mat[i][mid] == 0) {
-                    r = mid - 1;
-                }
-                else {
-                    pos = mid;
-                    l = mid + 1;
-                }
-            }
-            power.emplace_back(pos + 1, i);
+    int networkDelayTime(vector<vector<int>> &times, int n, int k) {
+        const int inf = INT_MAX / 2;
+        vector<vector<int>> g(n, vector<int>(n, inf));
+        for (auto &t : times) {
+            int x = t[0] - 1, y = t[1] - 1;
+            g[x][y] = t[2];
         }
 
-        priority_queue q(greater<pair<int, int>>(), move(power));
-        vector<int> ans;
-        for (int i = 0; i < k; ++i) {
-            ans.push_back(q.top().second);
-            q.pop();
+        vector<int> dist(n, inf);
+        dist[k - 1] = 0;
+        vector<int> used(n);
+        for (int i = 0; i < n; ++i) {
+            int x = -1;
+            for (int y = 0; y < n; ++y) {
+                if (!used[y] && (x == -1 || dist[y] < dist[x])) {
+                    x = y;
+                }
+            }
+            used[x] = true;
+            for (int y = 0; y < n; ++y) {
+                dist[y] = min(dist[y], dist[x] + g[x][y]);
+            }
         }
-        return ans;
+
+        int ans = *max_element(dist.begin(), dist.end());
+        return ans == inf ? -1 : ans;
     }
 };
