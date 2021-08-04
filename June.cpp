38148038e70dc99d4562e14499b5b47132723a67
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-08-04 22:51:00 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>> &times, int n, int k) {
-        const int inf = INT_MAX / 2;
-        vector<vector<int>> g(n, vector<int>(n, inf));
-        for (auto &t : times) {
-            int x = t[0] - 1, y = t[1] - 1;
-            g[x][y] = t[2];
-        }
-
-        vector<int> dist(n, inf);
-        dist[k - 1] = 0;
-        vector<int> used(n);
+    int triangleNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int ans = 0;
         for (int i = 0; i < n; ++i) {
-            int x = -1;
-            for (int y = 0; y < n; ++y) {
-                if (!used[y] && (x == -1 || dist[y] < dist[x])) {
-                    x = y;
+            for (int j = i + 1; j < n; ++j) {
+                int left = j + 1, right = n - 1, k = j;
+                while (left <= right) {
+                    int mid = (left + right) / 2;
+                    if (nums[mid] < nums[i] + nums[j]) {
+                        k = mid;
+                        left = mid + 1;
+                    }
+                    else {
+                        right = mid - 1;
+                    }
                 }
-            }
-            used[x] = true;
-            for (int y = 0; y < n; ++y) {
-                dist[y] = min(dist[y], dist[x] + g[x][y]);
+                ans += k - j;
             }
         }
-
-        int ans = *max_element(dist.begin(), dist.end());
-        return ans == inf ? -1 : ans;
+        return ans;
     }
-};
-class Solution {
-public:
-    int findUnsortedSubarray(vector<int>& nums) {
-        if (is_sorted(nums.begin(), nums.end())) {
-            return 0;
-        }
-        vector<int> numsSorted(nums);
-        sort(numsSorted.begin(), numsSorted.end());
-        int left = 0;
-        while (nums[left] == numsSorted[left]) {
+};Sorted[left]) {
             left++;
         }
         int right = nums.size() - 1;
