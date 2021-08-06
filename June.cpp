96d1e821db4b4e89fc9bf87a50c24ae0342a4225
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-08-06 22:30:03 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>> &graph) {
+    int shortestPathLength(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> color(n);
-
-        function<bool(int)> safe = [&](int x) {
-            if (color[x] > 0) {
-                return color[x] == 2;
-            }
-            color[x] = 1;
-            for (int y : graph[x]) {
-                if (!safe(y)) {
-                    return false;
-                }
-            }
-            color[x] = 2;
-            return true;
-        };
-
-        vector<int> ans;
+        queue<tuple<int, int, int>> q;
+        vector<vector<int>> seen(n, vector<int>(1 << n));
         for (int i = 0; i < n; ++i) {
-            if (safe(i)) {
-                ans.push_back(i);
+            q.emplace(i, 1 << i, 0);
+            seen[i][1 << i] = true;
+        }
+
+        int ans = 0;
+        while (!q.empty()) {
+            auto [u, mask, dist] = q.front();
+            q.pop();
+            if (mask == (1 << n) - 1) {
+                ans = dist;
+                break;
+            }
+            // 搜索相邻的节点
+            for (int v: graph[u]) {
+                // 将 mask 的第 v 位置为 1
+                int mask_v = mask | (1 << v);
+                if (!seen[v][mask_v]) {
+                    q.emplace(v, mask_v, dist + 1);
+                    seen[v][mask_v] = true;
+                }
             }
         }
         return ans;
