549b0982af0154c54783e84fddf582f0383568b6
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-08-08 22:48:01 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
-    bool circularArrayLoop(vector<int>& nums) {
-        int n = nums.size();
-        auto next = [&](int cur) {
-            return ((cur + nums[cur]) % n + n) % n; // 保证返回值在 [0,n) 中
-        };
-
-        for (int i = 0; i < n; i++) {
-            if (!nums[i]) {
-                continue;
-            }
-            int slow = i, fast = next(i);
-            // 判断非零且方向相同
-            while (nums[slow] * nums[fast] > 0 && nums[slow] * nums[next(fast)] > 0) {
-                if (slow == fast) {
-                    if (slow != next(slow)) {
-                        return true;
-                    } else {
-                        break;
-                    }
-                }
-                slow = next(slow);
-                fast = next(next(fast));
-            }
-            int add = i;
-            while (nums[add] * nums[next(add)] > 0) {
-                int tmp = add;
-                add = next(add);
-                nums[tmp] = 0;
-            }
+    int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
         }
-        return false;
+        if (n <= 2) {
+            return 1;
+        }
+        int p = 0, q = 0, r = 1, s = 1;
+        for (int i = 3; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = s;
+            s = p + q + r;
+        }
+        return s;
     }
 };
