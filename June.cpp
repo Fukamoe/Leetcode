03e018d85f4869bae8fd515080ae5cdec6a7dce9
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-08-09 23:36:32 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int tribonacci(int n) {
-        if (n == 0) {
-            return 0;
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        unordered_set<long> seen;
+        priority_queue<long, vector<long>, greater<long>> heap;
+        seen.insert(1);
+        heap.push(1);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.top();
+            heap.pop();
+            ugly = (int)curr;
+            for (int prime : primes) {
+                long next = curr * prime;
+                if (seen.insert(next).second) {
+                    heap.push(next);
+                }
+            }
         }
-        if (n <= 2) {
-            return 1;
-        }
-        int p = 0, q = 0, r = 1, s = 1;
-        for (int i = 3; i <= n; ++i) {
-            p = q;
-            q = r;
-            r = s;
-            s = p + q + r;
-        }
-        return s;
+        return ugly;
     }
 };
