e1daf33134b575532b5f51299830425877638fbb
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-08-15 20:41:28 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    int unhappyFriends(int n, vector<vector<int>>& preferences, vector<vector<int>>& pairs) {
-        vector<vector<int>> order(n, vector<int>(n));
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n - 1; ++j) {
-                order[i][preferences[i][j]] = j;
-            }
-        }
-        vector<int> match(n);
-        for (const auto& pr: pairs) {
-            match[pr[0]] = pr[1];
-            match[pr[1]] = pr[0];
-        }
+    static constexpr int MOD = 1'000'000'007;
 
-        int unhappyCount = 0;
-        for (int x = 0; x < n; ++x) {
-            int y = match[x];
-            int index = order[x][y];
-            for (int i = 0; i < index; ++i) {
-                int u = preferences[x][i];
-                int v = match[u];
-                if (order[u][x] < order[u][v]) {
-                    ++unhappyCount;
-                    break;
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int outCounts = 0;
+        vector<vector<vector<int>>> dp(maxMove + 1, vector<vector<int>>(m, vector<int>(n)));
+        dp[0][startRow][startColumn] = 1;
+        for (int i = 0; i < maxMove; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < n; k++) {
+                    int count = dp[i][j][k];
+                    if (count > 0) {
+                        for (auto &direction : directions) {
+                            int j1 = j + direction[0], k1 = k + direction[1];
+                            if (j1 >= 0 && j1 < m && k1 >= 0 && k1 < n) {
+                                dp[i + 1][j1][k1] = (dp[i + 1][j1][k1] + count) % MOD;
+                            } else {
+                                outCounts = (outCounts + count) % MOD;
+                            }
+                        }
+                    }
                 }
             }
         }
-        return unhappyCount;
+        return outCounts;
     }
 };
