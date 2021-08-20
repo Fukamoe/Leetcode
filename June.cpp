fb74a8739b8dd0730f8b5b0ea55b0c4de7b729ef
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-08-20 21:47:47 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,24 +1,9 @@
 class Solution {
 public:
-    string reverseVowels(string s) {
-        auto isVowel = [vowels = "aeiouAEIOU"s](char ch) {
-            return vowels.find(ch) != string::npos;
-        };
-
-        int n = s.size();
-        int i = 0, j = n - 1;
-        while (i < j) {
-            while (i < n && !isVowel(s[i])) {
-                ++i;
-            }
-            while (j > 0 && !isVowel(s[j])) {
-                --j;
-            }
-            if (i < j) {
-                swap(s[i], s[j]);
-                ++i;
-                --j;
-            }
+    string reverseStr(string s, int k) {
+        int n = s.length();
+        for (int i = 0; i < n; i += 2 * k) {
+            reverse(s.begin() + i, s.begin() + min(i + k, n));
         }
         return s;
     }
