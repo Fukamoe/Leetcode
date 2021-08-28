c89eb8d211197478a10ac587774bebcd57df9854
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-08-28 23:16:23 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,21 +1,4 @@
-class Solution {
-public:
-    int numRescueBoats(vector<int> &people, int limit) {
-        int ans = 0;
-        sort(people.begin(), people.end());
-        int light = 0, heavy = people.size() - 1;
-        while (light <= heavy) {
-            if (people[light] + people[heavy] > limit) {
-                --heavy;
-            } else {
-                ++light;
-                --heavy;
-            }
-            ++ans;
-        }
-        return ans;
-    }
-};class MedianFinder {
+class MedianFinder {
 public:
     priority_queue<int, vector<int>, less<int>> queMin;
     priority_queue<int, vector<int>, greater<int>> queMax;
@@ -45,8 +28,3 @@ public:
         return (queMin.top() + queMax.top()) / 2.0;
     }
 };
-
-作者：LeetCode-Solution
-链接：https://leetcode-cn.com/problems/find-median-from-data-stream/solution/shu-ju-liu-de-zhong-wei-shu-by-leetcode-ktkst/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
