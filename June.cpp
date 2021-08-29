fd83728a5239146c8adbc895b8a3ba4daead48da
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-08-29 22:56:57 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -28,3 +28,18 @@ public:
         return (queMin.top() + queMax.top()) / 2.0;
     }
 };
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum = 0;
+        int n = arr.length;
+        for (int start = 0; start < n; start++) {
+            for (int length = 1; start + length <= n; length += 2) {
+                int end = start + length - 1;
+                for (int i = start; i <= end; i++) {
+                    sum += arr[i];
+                }
+            }
+        }
+        return sum;
+    }
+}
