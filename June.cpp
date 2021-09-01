b472f01b70d6c37443448ee6467ede8f7ff077fd
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-09-01 21:49:27 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int> nums(n);
-        for (auto& booking : bookings) {
-            nums[booking[0] - 1] += booking[2];
-            if (booking[1] < n) {
-                nums[booking[1]] -= booking[2];
+    int compareVersion(string version1, string version2) {
+        int n = version1.length(), m = version2.length();
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            int x = 0;
+            for (; i < n && version1[i] != '.'; ++i) {
+                x = x * 10 + version1[i] - '0';
+            }
+            ++i; // 跳过点号
+            int y = 0;
+            for (; j < m && version2[j] != '.'; ++j) {
+                y = y * 10 + version2[j] - '0';
+            }
+            ++j; // 跳过点号
+            if (x != y) {
+                return x > y ? 1 : -1;
             }
         }
-        for (int i = 1; i < n; i++) {
-            nums[i] += nums[i - 1];
-        }
-        return nums;
+        return 0;
     }
 };
