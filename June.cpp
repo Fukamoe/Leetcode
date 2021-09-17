f82f0cc1a3b1d108360cf8f7ccfb7932096365b8
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-09-17 23:16:49 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,9 +1,27 @@
-class Solution:
-    def findLongestWord(self, s: str, dictionary: List[str]) -> str:
-        res = ""
-        for t in dictionary:
-            i = j = 0
-            while i < len(t) and j < len(s):
-                if t[i] == s[j]:
-                    i += 1
-  
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int rows[9][9];
+        int columns[9][9];
+        int subboxes[3][3][9];
+        
+        memset(rows,0,sizeof(rows));
+        memset(columns,0,sizeof(columns));
+        memset(subboxes,0,sizeof(subboxes));
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    int index = c - '0' - 1;
+                    rows[i][index]++;
+                    columns[j][index]++;
+                    subboxes[i / 3][j / 3][index]++;
+                    if (rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i / 3][j / 3][index] > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
