210ee64cd6f732840e01431cf8fb0710d118f74b
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-09-18 23:13:37 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,27 +1,5 @@
-class Solution {
-public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        int rows[9][9];
-        int columns[9][9];
-        int subboxes[3][3][9];
-        
-        memset(rows,0,sizeof(rows));
-        memset(columns,0,sizeof(columns));
-        memset(subboxes,0,sizeof(subboxes));
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char c = board[i][j];
-                if (c != '.') {
-                    int index = c - '0' - 1;
-                    rows[i][index]++;
-                    columns[j][index]++;
-                    subboxes[i / 3][j / 3][index]++;
-                    if (rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i / 3][j / 3][index] > 1) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-};
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/nim-game/solution/nim-you-xi-by-leetcode-solution-95g8/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
