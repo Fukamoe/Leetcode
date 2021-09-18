9cca057263a0b398c175d5b765f139cb26869105
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-09-18 23:13:51 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,5 +1,6 @@
-
-作者：LeetCode-Solution
-链接：https://leetcode-cn.com/problems/nim-game/solution/nim-you-xi-by-leetcode-solution-95g8/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4 != 0;
+    }
+};
