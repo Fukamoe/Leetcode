c56911b0948222f61c84a1b5fafe242fb1c83f99
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-09-19 21:31:33 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
-    bool canWinNim(int n) {
-        return n % 4 != 0;
+    int minSteps(int n) {
+        vector<int> f(n + 1);
+        for (int i = 2; i <= n; ++i) {
+            f[i] = INT_MAX;
+            for (int j = 1; j * j <= i; ++j) {
+                if (i % j == 0) {
+                    f[i] = min(f[i], f[j] + i / j);
+                    f[i] = min(f[i], f[i / j] + j);
+                }
+            }
+        }
+        return f[n];
     }
 };
