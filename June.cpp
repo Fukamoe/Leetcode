c190ce8cd3fe1f6c8a2b9a5a3a8978a91981fd60
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-09-23 22:25:21 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,26 +1,9 @@
 class Solution {
 public:
-    vector<ListNode*> splitListToParts(ListNode* head, int k) {
-        int n = 0;
-        ListNode *temp = head;
-        while (temp != nullptr) {
-            n++;
-            temp = temp->next;
+    bool isPowerOfThree(int n) {
+        while (n && n % 3 == 0) {
+            n /= 3;
         }
-        int quotient = n / k, remainder = n % k;
-
-        vector<ListNode*> parts(k,nullptr);
-        ListNode *curr = head;
-        for (int i = 0; i < k && curr != nullptr; i++) {
-            parts[i] = curr;
-            int partSize = quotient + (i < remainder ? 1 : 0);
-            for (int j = 1; j < partSize; j++) {
-                curr = curr->next;
-            }
-            ListNode *next = curr->next;
-            curr->next = nullptr;
-            curr = next;
-        }
-        return parts;
+        return n == 1;
     }
 };
