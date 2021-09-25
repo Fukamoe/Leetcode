075d64209ddbc51807002b6c623f94c27eec7613
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-09-25 22:19:50 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-    Node* flatten(Node* head) {
-        function<Node*(Node*)> dfs = [&](Node* node) {
-            Node* cur = node;
-            // 记录链表的最后一个节点
-            Node* last = nullptr;
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
 
-            while (cur) {
-                Node* next = cur->next;
-                //  如果有子节点，那么首先处理子节点
-                if (cur->child) {
-                    Node* child_last = dfs(cur->child);
-
-                    next = cur->next;
-                    //  将 node 与 child 相连
-                    cur->next = cur->child;
-                    cur->child->prev = cur;
-
-                    //  如果 next 不为空，就将 last 与 next 相连
-                    if (next) {
-                        child_last->next = next;
-                        next->prev = child_last;
-                    }
-
-                    // 将 child 置为空
-                    cur->child = nullptr;
-                    last = child_last;
+        for (int i = 1; i <= m; i++) {
+            char c1 = word1[i - 1];
+            for (int j = 1; j <= n; j++) {
+                char c2 = word2[j - 1];
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
-                else {
-                    last = cur;
-                }
-                cur = next;
             }
-            return last;
-        };
+        }
 
-        dfs(head);
-        return head;
+        int lcs = dp[m][n];
+        return m - lcs + n - lcs;
     }
 };
