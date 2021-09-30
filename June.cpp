7bd112c33574c95f290c56068a9f7212c5145859
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-09-30 23:40:35 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-    int findMinMoves(vector<int> &machines) {
-        int tot = accumulate(machines.begin(), machines.end(), 0);
-        int n = machines.size();
-        if (tot % n) {
-            return -1;
-        }
-        int avg = tot / n;
-        int ans = 0, sum = 0;
-        for (int num: machines) {
-            num -= avg;
-            sum += num;
-            ans = max(ans, max(abs(sum), num));
-        }
-        return ans;
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1 = (ax2 - ax1) * (ay2 - ay1), area2 = (bx2 - bx1) * (by2 - by1);
+        int overlapWidth = min(ax2, bx2) - max(ax1, bx1), overlapHeight = min(ay2, by2) - max(ay1, by1);
+        int overlapArea = max(overlapWidth, 0) * max(overlapHeight, 0);
+        return area1 + area2 - overlapArea;
     }
 };
