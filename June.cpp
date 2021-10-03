e286b244dfaa21cb00342776c04731fad1cac84f
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-10-03 18:33:47 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,17 +1,41 @@
-class Solution {
-public:
-    string toHex(int num) {
-        if (num == 0) {
-            return "0";
-        }
-        string sb;
-        for (int i = 7; i >= 0; i --) {
-            int val = (num >> (4 * i)) & 0xf;
-            if (sb.length() > 0 || val > 0) {
-                char digit = val < 10 ? (char) ('0' + val) : (char) ('a' + val - 10);
-                sb.push_back(digit);
-            }
-        }
-        return sb;
+func fractionToDecimal(numerator, denominator int) string {
+    if numerator%denominator == 0 {
+        return strconv.Itoa(numerator / denominator)
     }
-};
+
+    s := []byte{}
+    if numerator < 0 != (denominator < 0) {
+        s = append(s, '-')
+    }
+
+    // 整数部分
+    numerator = abs(numerator)
+    denominator = abs(denominator)
+    integerPart := numerator / denominator
+    s = append(s, strconv.Itoa(integerPart)...)
+    s = append(s, '.')
+
+    // 小数部分
+    indexMap := map[int]int{}
+    remainder := numerator % denominator
+    for remainder != 0 && indexMap[remainder] == 0 {
+        indexMap[remainder] = len(s)
+        remainder *= 10
+        s = append(s, '0'+byte(remainder/denominator))
+        remainder %= denominator
+    }
+    if remainder > 0 { // 有循环节
+        insertIndex := indexMap[remainder]
+        s = append(s[:insertIndex], append([]byte{'('}, s[insertIndex:]...)...)
+        s = append(s, ')')
+    }
+
+    return string(s)
+}
+
+func abs(x int) int {
+    if x < 0 {
+        return -x
+    }
+    return x
+}
