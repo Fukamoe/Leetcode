55c5644f08c7a2a3154a3bb3a83c36a0ded8895f
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-10-12 20:06:39 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,44 +1,80 @@
 class Solution {
 public:
-    vector<string> singles = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-    vector<string> teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    vector<string> tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    vector<string> thousands = {"", "Thousand", "Million", "Billion"};
-
-    string numberToWords(int num) {
-        if (num == 0) {
-            return "Zero";
-        }
-        string sb;
-        for (int i = 3, unit = 1000000000; i >= 0; i--, unit /= 1000) {
-            int curNum = num / unit;
-            if (curNum != 0) {
-                num -= curNum * unit;
-                string curr;
-                recursion(curr, curNum);
-                curr = curr + thousands[i] + " ";
-                sb = sb + curr;
+    int divide(int dividend, int divisor) {
+        // 考虑被除数为最小值的情况
+        if (dividend == INT_MIN) {
+            if (divisor == 1) {
+                return INT_MIN;
+            }
+            if (divisor == -1) {
+                return INT_MAX;
             }
         }
-        while (sb.back() == ' ') {
-            sb.pop_back();
+        // 考虑除数为最小值的情况
+        if (divisor == INT_MIN) {
+            return dividend == INT_MIN ? 1 : 0;
         }
-        return sb;
-    }
+        // 考虑被除数为 0 的情况
+        if (dividend == 0) {
+            return 0;
+        }
+        
+        // 一般情况，使用二分查找
+        // 将所有的正数取相反数，这样就只需要考虑一种情况
+        bool rev = false;
+        if (dividend > 0) {
+            dividend = -dividend;
+            rev = !rev;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+            rev = !rev;
+        }
 
-    void recursion(string & curr, int num) {
-        if (num == 0) {
-            return;
-        } else if (num < 10) {
-            curr = curr + singles[num] + " ";
-        } else if (num < 20) {
-            curr = curr + teens[num - 10] + " ";
-        } else if (num < 100) {
-            curr = curr + tens[num / 10] + " ";
-            recursion(curr, num % 10);
-        } else {
-            curr = curr + singles[num / 100] + " Hundred ";
-            recursion(curr, num % 100);
+        // 快速乘
+        auto quickAdd = [](int y, int z, int x) {
+            // x 和 y 是负数，z 是正数
+            // 需要判断 z * y >= x 是否成立
+            int result = 0, add = y;
+            while (z) {
+                if (z & 1) {
+                    // 需要保证 result + add >= x
+                    if (result < x - add) {
+                        return false;
+                    }
+                    result += add;
+                }
+                if (z != 1) {
+                    // 需要保证 add + add >= x
+                    if (add < x - add) {
+                        return false;
+                    }
+                    add += add;
+                }
+                // 不能使用除法
+                z >>= 1;
+            }
+            return true;
+        };
+        
+        int left = 1, right = INT_MAX, ans = 0;
+        while (left <= right) {
+            // 注意溢出，并且不能使用除法
+            int mid = left + ((right - left) >> 1);
+            bool check = quickAdd(divisor, mid, dividend);
+            if (check) {
+                ans = mid;
+                // 注意溢出
+                if (mid == INT_MAX) {
+                    break;
+                }
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
         }
+
+        return rev ? -ans : ans;
     }
 };
