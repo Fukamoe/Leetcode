2825a6b9088cb12b7d6983a144413bbcd451b21a
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-10-15 23:06:05 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        int n = arr.size();
-        int ans = -1;
-        for (int i = 1; i < n - 1; ++i) {
-            if (arr[i] > arr[i + 1]) {
-                ans = i;
-                break;
+    string countAndSay(int n) {
+        string prev = "1";
+        for (int i = 2; i <= n; ++i) {
+            string curr = "";
+            int start = 0;
+            int pos = 0;
+
+            while (pos < prev.size()) {
+                while (pos < prev.size() && prev[pos] == prev[start]) {
+                    pos++;
+                }
+                curr += to_string(pos - start) + prev[start];
+                start = pos;
             }
+            prev = curr;
         }
-        return ans;
+        
+        return prev;
     }
 };
