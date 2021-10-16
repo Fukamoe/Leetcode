22043b8a6c96eec8b42bab86bd8c450dbe576821
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-10-16 21:55:50 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    string countAndSay(int n) {
-        string prev = "1";
-        for (int i = 2; i <= n; ++i) {
-            string curr = "";
-            int start = 0;
-            int pos = 0;
+    vector<string> addOperators(string num, int target) {
+        int n = num.length();
+        vector<string> ans;
 
-            while (pos < prev.size()) {
-                while (pos < prev.size() && prev[pos] == prev[start]) {
-                    pos++;
+        function<void(string&, int, long, long)> backtrack = [&](string &expr, int i, long res, long mul) {
+            if (i == n) {
+                if (res == target) {
+                    ans.emplace_back(expr);
                 }
-                curr += to_string(pos - start) + prev[start];
-                start = pos;
+                return;
             }
-            prev = curr;
-        }
-        
-        return prev;
+            int signIndex = expr.size();
+            if (i > 0) {
+                expr.push_back(0); // 占位，下面填充符号
+            }
+            long val = 0;
+            // 枚举截取的数字长度（取多少位），注意数字可以是单个 0 但不能有前导零
+            for (int j = i; j < n && (j == i || num[i] != '0'); ++j) {
+                val = val * 10 + num[j] - '0';
+                expr.push_back(num[j]);
+                if (i == 0) { // 表达式开头不能添加符号
+                    backtrack(expr, j + 1, val, val);
+                } else { // 枚举符号
+                    expr[signIndex] = '+'; backtrack(expr, j + 1, res + val, val);
+                    expr[signIndex] = '-'; backtrack(expr, j + 1, res - val, -val);
+                    expr[signIndex] = '*'; backtrack(expr, j + 1, res - mul + mul * val, mul * val);
+                }
+            }
+            expr.resize(signIndex);
+        };
+
+        string expr;
+        backtrack(expr, 0, 0, 0);
+        return ans;
     }
 };
