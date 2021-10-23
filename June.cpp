d89b4bacb53aa76b9e12eede56c6b27d0f93a663
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-10-23 23:43:10 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans;
-        unordered_map<int, int> cnt;
-
-        for (auto & v : nums) {
-            cnt[v]++;
+    vector<int> constructRectangle(int area) {
+        int w = sqrt(1.0 * area);
+        while (area % w) {
+            --w;
         }
-        for (auto & v : cnt) {
-            if (v.second > n / 3) {
-                ans.push_back(v.first);
-            }
-        }
-
-        return ans;
+        return {area / w, w};
     }
 };
