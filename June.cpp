71b2c89673d4943e28e2d6bc2739ccbbb7a841c1
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-10-30 21:27:45 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    bool isSelfCrossing(vector<int>& distance) {
-        int n = distance.size();
-        for (int i = 3; i < n; ++i) {
-            // 第 1 类路径交叉的情况
-            if (distance[i] >= distance[i - 2] && distance[i - 1] <= distance[i - 3]) {
-                return true;
-            }
-
-            // 第 2 类路径交叉的情况
-            if (i == 4 && (distance[3] == distance[1]
-                && distance[4] >= distance[2] - distance[0])) {
-                return true;
-            }
-
-            // 第 3 类路径交叉的情况
-            if (i >= 5 && (distance[i - 3] - distance[i - 5] <= distance[i - 1]
-                && distance[i - 1] <= distance[i - 3]
-                && distance[i] >= distance[i - 2] - distance[i - 4]
-                && distance[i - 2] > distance[i - 4])) {
-                return true;
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (int num: nums) {
+            ++freq[num];
+        }
+        vector<int> ans;
+        for (const auto& [num, occ]: freq) {
+            if (occ == 1) {
+                ans.push_back(num);
             }
         }
-        return false;
+        return ans;
     }
 };
