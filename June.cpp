004834e58770877acd392b828918860780f0d688
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-11-02 19:53:24 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int distributeCandies(vector<int> &candyType) {
-        return min(unordered_set<int>(candyType.begin(), candyType.end()).size(), candyType.size() / 2);
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
     }
 };
