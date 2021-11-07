3050f2be6c95b604e2a6b05d9dbb199b0b90bcbb
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-11-07 21:33:09 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i) {
-                return i;
-            }
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        int mina = m, minb = n;
+        for (const auto& op: ops) {
+            mina = min(mina, op[0]);
+            minb = min(minb, op[1]);
         }
-        return n;
+        return mina * minb;
     }
 };
