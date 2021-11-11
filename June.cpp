9f68984ebb2a0bbaf48bbe983466748349bb791e
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-11-11 20:44:22 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,16 +1,23 @@
 class Solution {
+private:
+    static constexpr int mod = 1000000007;
+
 public:
-    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
-        int ans = 0;
-        int expired = 0;
-        for (int i = 0; i < timeSeries.size(); ++i) {
-            if (timeSeries[i] >= expired) {
-                ans += duration;
-            } else {
-                ans += timeSeries[i] + duration - expired;
+    int kInversePairs(int n, int k) {
+        vector<vector<int>> f(2, vector<int>(k + 1));
+        f[0][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= k; ++j) {
+                int cur = i & 1, prev = cur ^ 1;
+                f[cur][j] = (j - 1 >= 0 ? f[cur][j - 1] : 0) - (j - i >= 0 ? f[prev][j - i] : 0) + f[prev][j];
+                if (f[cur][j] >= mod) {
+                    f[cur][j] -= mod;
+                }
+                else if (f[cur][j] < 0) {
+                    f[cur][j] += mod;
+                }
             }
-            expired = timeSeries[i] + duration;
         }
-        return ans;
+        return f[n & 1][k];
     }
 };
