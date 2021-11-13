f762854d563a2ede3c5c3e02853a375a0c4d225c
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-11-13 19:52:34 +0800

    Create June.cpp

--- June.cpp
+++ June.cpp
@@ -1,23 +1,17 @@
 class Solution {
-private:
-    static constexpr int mod = 1000000007;
-
 public:
-    int kInversePairs(int n, int k) {
-        vector<vector<int>> f(2, vector<int>(k + 1));
-        f[0][0] = 1;
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 0; j <= k; ++j) {
-                int cur = i & 1, prev = cur ^ 1;
-                f[cur][j] = (j - 1 >= 0 ? f[cur][j - 1] : 0) - (j - i >= 0 ? f[prev][j - i] : 0) + f[prev][j];
-                if (f[cur][j] >= mod) {
-                    f[cur][j] -= mod;
-                }
-                else if (f[cur][j] < 0) {
-                    f[cur][j] += mod;
-                }
+    bool detectCapitalUse(string word) {
+        // 若第 1 个字母为小写，则需额外判断第 2 个字母是否为小写
+        if (word.size() >= 2 && islower(word[0]) && isupper(word[1])) {
+            return false;
+        }
+        
+        // 无论第 1 个字母是否大写，其他字母必须与第 2 个字母的大小写相同
+        for (int i = 2; i < word.size(); ++i) {
+            if (islower(word[i]) ^ islower(word[1])) {
+                return false;
             }
         }
-        return f[n & 1][k];
+        return true;
     }
 };
