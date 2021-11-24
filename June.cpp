092c658ccb3bb3a1fd5d03b1f323164f34918da9
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-11-24 21:52:55 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    bool buddyStrings(string s, string goal) {
-        if (s.size() != goal.size()) {
-            return false;
+    string originalDigits(string s) {
+        unordered_map<char, int> c;
+        for (char ch: s) {
+            ++c[ch];
         }
-        
-        if (s == goal) {
-            vector<int> count(26);
-            for (int i = 0; i < s.size(); i++) {
-                count[s[i] - 'a']++;
-                if (count[s[i] - 'a'] > 1) {
-                    return true;
-                }
-            }
-            return false;
-        } else {
-            int first = -1, second = -1;
-            for (int i = 0; i < s.size(); i++) {
-                if (s[i] != goal[i]) {
-                    if (first == -1)
-                        first = i;
-                    else if (second == -1)
-                        second = i;
-                    else
-                        return false;
-                }
-            }
 
-            return (second != -1 && s[first] == goal[second] && s[second] == goal[first]);
+        vector<int> cnt(10);
+        cnt[0] = c['z'];
+        cnt[2] = c['w'];
+        cnt[4] = c['u'];
+        cnt[6] = c['x'];
+        cnt[8] = c['g'];
+
+        cnt[3] = c['h'] - cnt[8];
+        cnt[5] = c['f'] - cnt[4];
+        cnt[7] = c['s'] - cnt[6];
+
+        cnt[1] = c['o'] - cnt[0] - cnt[2] - cnt[4];
+
+        cnt[9] = c['i'] - cnt[5] - cnt[6] - cnt[8];
+
+        string ans;
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < cnt[i]; ++j) {
+                ans += char(i + '0');
+            }
         }
+        return ans;
     }
 };
