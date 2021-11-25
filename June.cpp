12025c928dc7ccb4a133344206dcce485200ffe5
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-11-25 21:27:37 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    string originalDigits(string s) {
-        unordered_map<char, int> c;
-        for (char ch: s) {
-            ++c[ch];
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        if (buckets == 1) {
+            return 0;
         }
-
-        vector<int> cnt(10);
-        cnt[0] = c['z'];
-        cnt[2] = c['w'];
-        cnt[4] = c['u'];
-        cnt[6] = c['x'];
-        cnt[8] = c['g'];
-
-        cnt[3] = c['h'] - cnt[8];
-        cnt[5] = c['f'] - cnt[4];
-        cnt[7] = c['s'] - cnt[6];
-
-        cnt[1] = c['o'] - cnt[0] - cnt[2] - cnt[4];
-
-        cnt[9] = c['i'] - cnt[5] - cnt[6] - cnt[8];
-
-        string ans;
-        for (int i = 0; i < 10; ++i) {
-            for (int j = 0; j < cnt[i]; ++j) {
-                ans += char(i + '0');
+        vector<vector<int>> combinations(buckets + 1,vector<int>(buckets + 1));
+        combinations[0][0] = 1;
+        int iterations = minutesToTest / minutesToDie;
+        vector<vector<int>> f(buckets,vector<int>(iterations + 1));
+        for (int i = 0; i < buckets; i++) {
+            f[i][0] = 1;
+        }
+        for (int j = 0; j <= iterations; j++) {
+            f[0][j] = 1;
+        }
+        for (int i = 1; i < buckets; i++) {
+            combinations[i][0] = 1;
+            combinations[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                combinations[i][j] = combinations[i - 1][j - 1] + combinations[i - 1][j];
+            }
+            for (int j = 1; j <= iterations; j++) {
+                for (int k = 0; k <= i; k++) {
+                    f[i][j] += f[k][j - 1] * combinations[i][i - k];
+                }
+            }
+            if (f[i][iterations] >= buckets) {
+                return i;
             }
         }
-        return ans;
+        return 0;
     }
 };
