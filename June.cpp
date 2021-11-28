de87e1516cddd57d2c9a9f9f3288fe7b505fa8fd
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-11-28 22:07:31 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    Solution(int m, int n) {
-        this->m = m;
-        this->n = n;
-        this->total = m * n;
-        srand(time(nullptr));
-    }
-    
-    vector<int> flip() {
-        int x = rand() % total;
+    vector<int> findAnagrams(string s, string p) {
+        int sLen = s.size(), pLen = p.size();
+
+        if (sLen < pLen) {
+            return vector<int>();
+        }
+
         vector<int> ans;
-        total--;   
-        // 查找位置 x 对应的映射
-        if (map.count(x)) {
-            ans = {map[x] / n, map[x] % n};
-        } else {
-            ans = {x / n, x % n};
+        vector<int> sCount(26);
+        vector<int> pCount(26);
+        for (int i = 0; i < pLen; ++i) {
+            ++sCount[s[i] - 'a'];
+            ++pCount[p[i] - 'a'];
         }
-        // 将位置 x 对应的映射设置为位置 total 对应的映射
-        if (map.count(total)) {
-            map[x] = map[total];
-        } else {
-            map[x] = total;
+
+        if (sCount == pCount) {
+            ans.emplace_back(0);
         }
+
+        for (int i = 0; i < sLen - pLen; ++i) {
+            --sCount[s[i] - 'a'];
+            ++sCount[s[i + pLen] - 'a'];
+
+            if (sCount == pCount) {
+                ans.emplace_back(i + 1);
+            }
+        }
+
         return ans;
     }
-    
-    void reset() {
-        total = m * n;
-        map.clear();
-    }
-private:
-    int m;
-    int n;
-    int total;
-    unordered_map<int, int> map;
 };
