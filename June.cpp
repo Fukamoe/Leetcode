4c9494b3c98459ac11775e5e9ed47c65db5bd719
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-12-02 16:08:01 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int maxPower(string s) {
-        int ans = 1, cnt = 1;
-        for (int i = 1; i < s.length(); ++i) {
-            if (s[i] == s[i - 1]) {
-                ++cnt;
-                ans = max(ans, cnt);
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        string desc[3] = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+        vector<pair<int, int>> arr;
+
+        for (int i = 0; i < n; ++i) {
+            arr.emplace_back(make_pair(-score[i], i));
+        }
+        sort(arr.begin(), arr.end());
+        vector<string> ans(n);
+        for (int i = 0; i < n; ++i) {
+            if (i >= 3) {
+                ans[arr[i].second] = to_string(i + 1);
             } else {
-                cnt = 1;
+                ans[arr[i].second] = desc[i];
             }
         }
         return ans;
