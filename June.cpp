39e07644f7ff2acee5023e2c5d724c969eb028bd
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-12-04 23:22:11 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    int largestSumAfterKNegations(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
-        for (int num: nums) {
-            freq[num] += 1;
+    bool canConstruct(string ransomNote, string magazine) {
+        if (ransomNote.size() > magazine.size()) {
+            return false;
         }
-        int ans = accumulate(nums.begin(), nums.end(), 0);
-        for (int i = -100; i < 0; ++i) {
-            if (freq[i]) {
-                int ops = min(k, freq[i]);
-                ans += (-i) * ops * 2;
-                freq[i] -= ops;
-                freq[-i] += ops;
-                k -= ops;
-                if (k == 0) {
-                    break;
-                }
+        vector<int> cnt(26);
+        for (auto & c : magazine) {
+            cnt[c - 'a']++;
+        }
+        for (auto & c : ransomNote) {
+            cnt[c - 'a']--;
+            if (cnt[c - 'a'] < 0) {
+                return false;
             }
         }
-        if (k > 0 && k % 2 == 1 && !freq[0]) {
-            for (int i = 1; i <= 100; ++i) {
-                if (freq[i]) {
-                    ans -= i * 2;
-                    break;
-                }
-            }
-        }
-        return ans;
+        return true;
     }
 };
