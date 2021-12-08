3ce54dfb96e056a883aa7bf5bb17168ae19d59ea
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-12-08 22:35:38 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,35 +1,18 @@
-typedef pair<int, int> pii;
-
 class Solution {
 public:
-    vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col, int color) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        vector<pii> borders;
-        int originalColor = grid[row][col];
-        visited[row][col] = true;
-        dfs(grid, row, col, visited, borders, originalColor);
-        for (auto & [x, y] : borders) {
-            grid[x][y] = color;
+    vector<int> maxSumOfOneSubarray(vector<int> &nums, int k) {
+        vector<int> ans;
+        int sum1 = 0, maxSum1 = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum1 += nums[i];
+            if (i >= k - 1) {
+                if (sum1 > maxSum1) {
+                    maxSum1 = sum1;
+                    ans = {i - k + 1};
+                }
+                sum1 -= nums[i - k + 1];
+            }
         }
-        return grid;
-    }
-
-    void dfs(vector<vector<int>>& grid, int x, int y, vector<vector<bool>> & visited, vector<pii> & borders, int originalColor) {
-        int m = grid.size(), n = grid[0].size();
-        bool isBorder = false;
-        int direc[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        for (int i = 0; i < 4; i++) {
-            int nx = direc[i][0] + x, ny = direc[i][1] + y;
-            if (!(nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == originalColor)) {
-                isBorder = true;
-            } else if (!visited[nx][ny]) {
-                visited[nx][ny] = true;
-                dfs(grid, nx, ny, visited, borders, originalColor);
-            }                
-        }
-        if (isBorder) {
-            borders.emplace_back(x, y);
-        }
+        return ans;
     }
 };
