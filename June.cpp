48d235a5c53895e28685b90cd2608ee901b57f89
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-12-10 00:07:36 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,18 +1,12 @@
-class Solution {
-public:
-    vector<int> maxSumOfOneSubarray(vector<int> &nums, int k) {
-        vector<int> ans;
-        int sum1 = 0, maxSum1 = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            sum1 += nums[i];
-            if (i >= k - 1) {
-                if (sum1 > maxSum1) {
-                    maxSum1 = sum1;
-                    ans = {i - k + 1};
-                }
-                sum1 -= nums[i - k + 1];
-            }
-        }
-        return ans;
-    }
-};
+class Solution:
+    def maxSumOfOneSubarray(self, nums: List[int], k: int) -> List[int]:
+        ans = []
+        sum1, maxSum1 = 0, 0
+        for i, num in enumerate(nums):
+            sum1 += num
+            if i >= k - 1:
+                if sum1 > maxSum1:
+                    maxSum1 = sum1
+                    ans = [i - k + 1]
+                sum1 -= nums[i - k + 1]
+        return ans
