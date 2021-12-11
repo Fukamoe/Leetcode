50d9f0633f98e45ead962d3c4367258e6f6ebc46
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-12-11 23:05:26 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,40 +1,24 @@
-class Solution {
+class TopVotedCandidate {
 public:
-    bool validTicTacToe(vector<string>& board) {
-        int xCount = 0, oCount = 0;
-        for (string & row : board) {
-            for (char c : row) {
-                xCount = (c == 'X') ? (xCount + 1) : xCount;
-                oCount = (c == 'O') ? (oCount + 1) : oCount;
-            }
-        }
-        if (oCount != xCount && oCount != xCount - 1) {
-            return false;
-        }
-        if (win(board, 'X') && oCount != xCount - 1) {
-            return false;
-        }
-        if (win(board, 'O') && oCount != xCount) {
-            return false;
-        }
-        return true;
-    }
+    vector<int> tops;
+    vector<int> times;
 
-    bool win(vector<string>& board, char p) {
-        for (int i = 0; i < 3; ++i) {
-            if (p == board[i][0] && p == board[i][1] && p == board[i][2]) {
-                return true;
+    TopVotedCandidate(vector<int>& persons, vector<int>& times) {
+        unordered_map<int, int> voteCounts;
+        voteCounts[-1] = -1;
+        int top = -1;
+        for (auto & p : persons) {
+            voteCounts[p]++;
+            if (voteCounts[p] >= voteCounts[top]) {
+                top = p;
             }
-            if (p == board[0][i] && p == board[1][i] && p == board[2][i]) {
-                return true;
-            }
+            tops.emplace_back(top);
         }
-        if (p == board[0][0] && p == board[1][1] && p == board[2][2]) {
-            return true;
-        }
-        if (p == board[0][2] && p == board[1][1] && p == board[2][0]) {
-            return true;
-        }
-        return false;
+        this->times = times;
+    }
+    
+    int q(int t) {
+        int pos = upper_bound(times.begin(), times.end(), t) - times.begin() - 1;
+        return tops[pos];
     }
 };
