8c64cbf40079df6668ca41c94f28b114b450a8c5
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-12-12 22:49:11 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,24 +1,9 @@
-class TopVotedCandidate {
+class Solution {
 public:
-    vector<int> tops;
-    vector<int> times;
-
-    TopVotedCandidate(vector<int>& persons, vector<int>& times) {
-        unordered_map<int, int> voteCounts;
-        voteCounts[-1] = -1;
-        int top = -1;
-        for (auto & p : persons) {
-            voteCounts[p]++;
-            if (voteCounts[p] >= voteCounts[top]) {
-                top = p;
-            }
-            tops.emplace_back(top);
+    string toLowerCase(string s) {
+        for (char& ch: s) {
+            ch = tolower(ch);
         }
-        this->times = times;
-    }
-    
-    int q(int t) {
-        int pos = upper_bound(times.begin(), times.end(), t) - times.begin() - 1;
-        return tops[pos];
+        return s;
     }
 };
