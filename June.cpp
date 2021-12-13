0c8771b92924ce83a7cfb911cc04716100585bf6
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-12-13 19:32:13 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
-    string toLowerCase(string s) {
-        for (char& ch: s) {
-            ch = tolower(ch);
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> rowMax(n);
+        vector<int> colMax(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rowMax[i] = max(rowMax[i], grid[i][j]);
+                colMax[j] = max(colMax[j], grid[i][j]);
+            }
         }
-        return s;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans += min(rowMax[i], colMax[j]) - grid[i][j];
+            }
+        }
+        return ans;
     }
 };
