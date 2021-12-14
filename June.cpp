a2dd73e88f21ee02bc78afdc7ee5105d69344d3f
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-12-14 21:36:21 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<int> rowMax(n);
-        vector<int> colMax(n);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                rowMax[i] = max(rowMax[i], grid[i][j]);
-                colMax[j] = max(colMax[j], grid[i][j]);
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(), courses.end(), [](const auto& c0, const auto& c1) {
+            return c0[1] < c1[1];
+        });
+
+        priority_queue<int> q;
+        // 优先队列中所有课程的总时间
+        int total = 0;
+
+        for (const auto& course: courses) {
+            int ti = course[0], di = course[1];
+            if (total + ti <= di) {
+                total += ti;
+                q.push(ti);
+            }
+            else if (!q.empty() && q.top() > ti) {
+                total -= q.top() - ti;
+                q.pop();
+                q.push(ti);
             }
         }
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                ans += min(rowMax[i], colMax[j]) - grid[i][j];
-            }
-        }
-        return ans;
+
+        return q.size();
     }
 };
