aad2969dfbe8f12a9da3a18bef22e0da11ab7721
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-12-22 21:04:33 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
-    int dayOfYear(string date) {
-        int year = stoi(date.substr(0, 4));
-        int month = stoi(date.substr(5, 2));
-        int day = stoi(date.substr(8, 2));
-
-        int amount[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-            ++amount[1];
+    int strStr(string haystack, string needle) {
+        int n = haystack.size(), m = needle.size();
+        if (m == 0) {
+            return 0;
         }
 
-        int ans = 0;
-        for (int i = 0; i < month - 1; ++i) {
-            ans += amount[i];
+        long long k1 = 1e9 + 7;
+        long long k2 = 1337;
+        srand((unsigned)time(NULL));
+        long long kMod1 = rand() % k1 + k1;
+        long long kMod2 = rand() % k2 + k2;
+
+        long long hash_needle = 0;
+        for (auto c : needle) {
+            hash_needle = (hash_needle * kMod2 + c) % kMod1;
         }
-        return ans + day;
+        long long hash_haystack = 0, extra = 1;
+        for (int i = 0; i < m - 1; i++) {
+            hash_haystack = (hash_haystack * kMod2 + haystack[i % n]) % kMod1;
+            extra = (extra * kMod2) % kMod1;
+        }
+        for (int i = m - 1; (i - m + 1) < n; i++) {
+            hash_haystack = (hash_haystack * kMod2 + haystack[i % n]) % kMod1;
+            if (hash_haystack == hash_needle) {
+                return i - m + 1;
+            }
+            hash_haystack = (hash_haystack - extra * haystack[(i - m + 1) % n]) % kMod1;
+            hash_haystack = (hash_haystack + kMod1) % kMod1;
+        }
+        return -1;
+    }
+
+    int repeatedStringMatch(string a, string b) {
+        int an = a.size(), bn = b.size();
+        int index = strStr(a, b);
+        if (index == -1) {
+            return -1;
+        }
+        if (an - index >= bn) {
+            return 1;
+        }
+        return (bn + index - an - 1) / an + 2;
     }
 };
