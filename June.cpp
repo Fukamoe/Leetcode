2602c5a0a6f295b80c424151ac251302c168d086
Fuka <76830297+Fukamoe@users.noreply.github.com>
2021-12-29 21:38:40 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,66 +1,19 @@
-struct Trie {
-    bool isEnd;
-    vector<Trie *> children;
-    Trie() {
-        this->children = vector<Trie *>(26, nullptr);
-        this->isEnd = false;
-    }
-};
-
 class Solution {
 public:
-    Trie * trie = new Trie();
-
-    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        vector<string> ans;
-        sort(words.begin(), words.end(), [&](const string & a, const string & b){
-            return a.size() < b.size(); 
-        });
-        for (int i = 0; i < words.size(); i++) {
-            string word = words[i];
-            if (word.size() == 0) {
-                continue;
-            }
-            if (dfs(word, 0)) {
-                ans.emplace_back(word);
-            } else {
-                insert(word);
-            }
-        }
-        return ans;
-    }
-
-    bool dfs(const string & word, int start) {
-        if (word.size() == start) {
-            return true;
-        }
-        Trie * node = trie;
-        for (int i = start; i < word.size(); i++) {
-            char ch = word[i];
-            int index = ch - 'a';
-            node = node->children[index];
-            if (node == nullptr) {
-                return false;
-            }
-            if (node->isEnd) {
-                if (dfs(word, i + 1)) {
-                    return true;
+    int countQuadruplets(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for (int a = 0; a < n; ++a) {
+            for (int b = a + 1; b < n; ++b) {
+                for (int c = b + 1; c < n; ++c) {
+                    for (int d = c + 1; d < n; ++d) {
+                        if (nums[a] + nums[b] + nums[c] == nums[d]) {
+                            ++ans;
+                        }
+                    }
                 }
             }
         }
-        return false;
-    }
-
-    void insert(const string & word) {
-        Trie * node = trie;
-        for (int i = 0; i < word.size(); i++) {
-            char ch = word[i];
-            int index = ch - 'a';
-            if (node->children[index] == nullptr) {
-                node->children[index] = new Trie();
-            }
-            node = node->children[index];
-        }
-        node->isEnd = true;
+        return ans;
     }
 };
