8533d313b9266ef17cc49e7a114e72818d53aafd
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-01-06 22:13:48 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,18 +1,43 @@
 class Solution {
 public:
-    string modifyString(string s) {
-        int n = s.size();
-        for (int i = 0; i < n; ++i) {
-            if (s[i] == '?') {
-                for (char ch = 'a'; ch <= 'c'; ++ch) {
-                    if ((i > 0 && s[i - 1] == ch) || (i < n - 1 && s[i + 1] == ch)) {
-                        continue;
-                    }
-                    s[i] = ch;
-                    break;
+    string simplifyPath(string path) {
+        auto split = [](const string& s, char delim) -> vector<string> {
+            vector<string> ans;
+            string cur;
+            for (char ch: s) {
+                if (ch == delim) {
+                    ans.push_back(move(cur));
+                    cur.clear();
+                }
+                else {
+                    cur += ch;
                 }
             }
+            ans.push_back(move(cur));
+            return ans;
+        };
+
+        vector<string> names = split(path, '/');
+        vector<string> stack;
+        for (string& name: names) {
+            if (name == "..") {
+                if (!stack.empty()) {
+                    stack.pop_back();
+                }
+            }
+            else if (!name.empty() && name != ".") {
+                stack.push_back(move(name));
+            }
         }
-        return s;
+        string ans;
+        if (stack.empty()) {
+            ans = "/";
+        }
+        else {
+            for (string& name: stack) {
+                ans += "/" + move(name);
+            }
+        }
+        return ans;
     }
 };
