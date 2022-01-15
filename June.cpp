9f433362912d616744faaac911abc6f753c2f408
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-01-15 21:45:34 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int dominantIndex(vector<int>& nums) {
-        int m1 = -1, m2 = -1;
-        int index = -1;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > m1) {
-                m2 = m1;
-                m1 = nums[i];
-                index = i;
-            } else if (nums[i] > m2) {
-                m2 = nums[i];
-            }
-        }
-        return m1 >= m2 * 2 ? index : -1;
+    int totalMoney(int n) {
+        // 所有完整的周存的钱
+        int weekNumber = n / 7;
+        int firstWeekMoney = (1 + 7) * 7 / 2;
+        int lastWeekMoney = firstWeekMoney + 7 * (weekNumber - 1);
+        int weekMoney = (firstWeekMoney + lastWeekMoney) * weekNumber / 2;
+        // 剩下的不能构成一个完整的周的天数里存的钱
+        int dayNumber = n % 7;
+        int firstDayMoney = 1 + weekNumber;
+        int lastDayMoney = firstDayMoney + dayNumber - 1;
+        int dayMoney = (firstDayMoney + lastDayMoney) * dayNumber / 2;
+        return weekMoney + dayMoney;
     }
 };
