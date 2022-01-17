32b44db0ab3dc364ba00c72a3b1acdf33b108f10
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-01-17 20:36:55 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,15 +1,22 @@
 class Solution {
-    vector<int> arr;
-
 public:
-    Solution(ListNode *head) {
-        while (head) {
-            arr.emplace_back(head->val);
-            head = head->next;
+    int countVowelPermutation(int n) {
+        long long mod = 1e9 + 7;
+        vector<long long> dp(5, 1);
+        vector<long long> ndp(5);
+        for (int i = 2; i <= n; ++i) {
+            /* a前面可以为e,u,i */
+            ndp[0] = (dp[1] + dp[2] + dp[4]) % mod;
+            /* e前面可以为a,i */
+            ndp[1] = (dp[0] + dp[2]) % mod;
+            /* i前面可以为e,o */
+            ndp[2] = (dp[1] + dp[3]) % mod;
+            /* o前面可以为i */
+            ndp[3] = dp[2];
+            /* u前面可以为i,o */
+            ndp[4] = (dp[2] + dp[3]) % mod;
+            dp = ndp;
         }
-    }
-
-    int getRandom() {
-        return arr[rand() % arr.size()];
+        return accumulate(dp.begin(), dp.end(), 0LL) % mod;
     }
 };
