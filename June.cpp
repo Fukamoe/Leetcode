ef92e56ba0664a6521f0aef895b6542053b10504
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-01-22 23:44:17 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,40 +1,12 @@
 class Solution {
 public:
-    int minJumps(vector<int>& arr) {
-        unordered_map<int, vector<int>> idxSameValue;
-        for (int i = 0; i < arr.size(); i++) {
-            idxSameValue[arr[i]].push_back(i);
-        }
-        unordered_set<int> visitedIndex;
-        queue<pair<int, int>> q;
-        q.emplace(0, 0);
-        visitedIndex.emplace(0);
-        while (!q.empty()) {
-            auto [idx, step] = q.front();
-            q.pop();
-            if (idx == arr.size() - 1) {
-                return step;
-            }
-            int v = arr[idx];
-            step++;
-            if (idxSameValue.count(v)) {
-                for (auto & i : idxSameValue[v]) {
-                    if (!visitedIndex.count(i)) {
-                        visitedIndex.emplace(i);
-                        q.emplace(i, step);
-                    }
-                }
-                idxSameValue.erase(v);
-            }
-            if (idx + 1 < arr.size() && !visitedIndex.count(idx + 1)) {
-                visitedIndex.emplace(idx + 1);
-                q.emplace(idx + 1, step);
-            }
-            if (idx - 1 >= 0 && !visitedIndex.count(idx - 1)) {
-                visitedIndex.emplace(idx - 1);
-                q.emplace(idx - 1, step);
+    int removePalindromeSub(string s) {
+        int n = s.size();
+        for (int i = 0; i < n / 2; ++i) {
+            if (s[i] != s[n - 1 - i]) {
+                return 2;
             }
         }
-        return -1;
+        return 1;
     }
 };
