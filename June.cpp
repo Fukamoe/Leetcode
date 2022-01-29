51d91cbbf2deb6ed0e496917efd783163c6c1f7a
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-01-29 19:58:41 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,46 +1,30 @@
+int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
 class Solution {
 public:
-    int countValidWords(string sentence) {
-        int n = sentence.length();
-        int l = 0, r = 0;
-        int ret = 0;
-        string_view slice(sentence);
-        while (true) {
-            while (l < n && sentence[l] == ' ') {
-                l++;
-            }
-            if (l >= n) {
-                break;
-            }
-            r = l + 1;
-            while (r < n && sentence[r] != ' ') {
-                r++;
-            }
-            if (isValid(slice.substr(l, r - l))) { // 判断根据空格分解出来的 token 是否有效
-                ret++;
-            }
-            l = r + 1;
-        }
-        return ret;
-    }
-
-    bool isValid(const string_view &word) {
-        int n = word.length();
-        bool has_hyphens = false;
-        for (int i = 0; i < n; i++) {
-            if (word[i] >= '0' && word[i] <= '9') {
-                return false;
-            } else if (word[i] == '-') {
-                if (has_hyphens == true || i == 0 || i == n - 1 || !islower(word[i - 1]) || !islower(word[i + 1])) {
-                    return false;
-                }
-                has_hyphens = true;
-            } else if (word[i] == '!' || word[i] == '.' || word[i] == ',') {
-                if (i != n - 1) {
-                    return false;
+    vector<vector<int>> highestPeak(vector<vector<int>> &isWater) {
+        int m = isWater.size(), n = isWater[0].size();
+        vector<vector<int>> ans(m, vector<int>(n, -1)); // -1 表示该格子尚未被访问过
+        queue<pair<int, int>> q;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (isWater[i][j]) {
+                    ans[i][j] = 0;
+                    q.emplace(i, j); // 将所有水域入队
                 }
             }
         }
-        return true;
+        while (!q.empty()) {
+            auto &p = q.front();
+            for (auto &dir : dirs) {
+                int x = p.first + dir[0], y = p.second + dir[1];
+                if (0 <= x && x < m && 0 <= y && y < n && ans[x][y] == -1) {
+                    ans[x][y] = ans[p.first][p.second] + 1;
+                    q.emplace(x, y);
+                }
+            }
+            q.pop();
+        }
+        return ans;
     }
 };
