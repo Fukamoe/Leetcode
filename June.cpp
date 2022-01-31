e3295b4eea389f2240f896dfe1ddca59befd3a60
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-01-31 23:36:04 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
-    vector<string> uncommonFromSentences(string s1, string s2) {
-        unordered_map<string, int> freq;
-        
-        auto insert = [&](const string& s) {
-            stringstream ss(s);
-            string word;
-            while (ss >> word) {
-                ++freq[move(word)];
-            }
-        };
-
-        insert(s1);
-        insert(s2);
-
-        vector<string> ans;
-        for (const auto& [word, occ]: freq) {
-            if (occ == 1) {
-                ans.push_back(word);
-            }
+    int numberOfSteps(int num) {
+        int ret = 0;
+        while (num) {
+            ret += (num > 1 ? 1 : 0) + (num & 0x01);
+            num >>= 1;
         }
-        return ans;
+        return ret;
     }
 };
