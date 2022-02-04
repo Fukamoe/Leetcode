99c4eea5423a330583eeb2fd795177322fff87b6
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-02-04 22:24:17 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int findMinFibonacciNumbers(int k) {
-        vector<int> f;
-        f.emplace_back(1);
-        int a = 1, b = 1;
-        while (a + b <= k) {
-            int c = a + b;
-            f.emplace_back(c);
-            a = b;
-            b = c;
-        }
-        int ans = 0;
-        for (int i = f.size() - 1; i >= 0 && k > 0; i--) {
-            int num = f[i];
-            if (k >= num) {
-                k -= num;
-                ans++;
+    int countGoodRectangles(vector<vector<int>>& rectangles) {
+        int res = 0, maxLen = 0;
+        for (auto & rectangle : rectangles) {
+            int l = rectangle[0], w = rectangle[1];
+            int k = min(l, w);
+            if (k == maxLen) {
+                ++res;
+            } else if (k > maxLen) {
+                res = 1;
+                maxLen = k;
             }
         }
-        return ans;
+        return res;
     }
 };
