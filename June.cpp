02fc8b134926d36be35586639b019a5b38c67113
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-02-07 22:27:03 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    int sumOfUnique(vector<int> &nums) {
-        unordered_map<int, int> cnt;
-        for (int num : nums) {
-            ++cnt[num];
-        }
-        int ans = 0;
-        for (auto &[num, c] : cnt) {
-            if (c == 1) {
-                ans += num;
+    string longestDiverseString(int a, int b, int c) {
+        string res;
+        vector<pair<int, char>> arr = {{a, 'a'}, {b, 'b'}, {c, 'c'}};
+        
+        while (true) {
+            sort(arr.begin(), arr.end(), [](const pair<int, char> & p1, const pair<int, char> & p2) {
+                return p1.first > p2.first;
+            });
+            bool hasNext = false;
+            for (auto & [freq, ch] : arr) {
+                int m = res.size();
+                if (freq <= 0) {
+                    break;
+                }
+                if (m >= 2 && res[m - 2] == ch && res[m - 1] == ch) {
+                    continue;
+                }
+                hasNext = true;
+                res.push_back(ch);
+                freq--;
+                break;
+            }
+            if (!hasNext) {
+                break;
             }
         }
-        return ans;
+      
+        return res;
     }
 };
