075a635e5864145647d044b22a16c1c2d635ca32
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-02-08 22:47:58 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,32 +1,50 @@
 class Solution {
 public:
-    string longestDiverseString(int a, int b, int c) {
-        string res;
-        vector<pair<int, char>> arr = {{a, 'a'}, {b, 'b'}, {c, 'c'}};
-        
-        while (true) {
-            sort(arr.begin(), arr.end(), [](const pair<int, char> & p1, const pair<int, char> & p2) {
-                return p1.first > p2.first;
-            });
-            bool hasNext = false;
-            for (auto & [freq, ch] : arr) {
-                int m = res.size();
-                if (freq <= 0) {
-                    break;
-                }
-                if (m >= 2 && res[m - 2] == ch && res[m - 1] == ch) {
-                    continue;
-                }
-                hasNext = true;
-                res.push_back(ch);
-                freq--;
-                break;
+    vector<int> gridIllumination(int n, vector<vector<int>> &lamps, vector<vector<int>> &queries) {
+        unordered_map<int, int> row, col, diagonal, antiDiagonal;
+        auto hash_p = [](const pair<int, int> &p) -> size_t {
+            static hash<long long> hash_ll;
+            return hash_ll(p.first + (static_cast<long long>(p.second) << 32));
+        };
+        unordered_set<pair<int, int>, decltype(hash_p)> points(0, hash_p);
+        for (auto &lamp : lamps) {
+            if (points.count({lamp[0], lamp[1]}) > 0) {
+                continue;
             }
-            if (!hasNext) {
-                break;
+            points.insert({lamp[0], lamp[1]});
+            row[lamp[0]]++;
+            col[lamp[1]]++;
+            diagonal[lamp[0] - lamp[1]]++;
+            antiDiagonal[lamp[0] + lamp[1]]++;
+        }
+        vector<int> ret(queries.size());
+        for (int i = 0; i < queries.size(); i++) {
+            int r = queries[i][0], c = queries[i][1];
+            if (row.count(r) > 0 && row[r] > 0) {
+                ret[i] = 1;
+            } else if (col.count(c) > 0 && col[c] > 0) {
+                ret[i] = 1;
+            } else if (diagonal.count(r - c) > 0 && diagonal[r - c] > 0) {
+                ret[i] = 1;
+            } else if (antiDiagonal.count(r + c) > 0 && antiDiagonal[r + c] > 0) {
+                ret[i] = 1;
+            }
+            for (int x = r - 1; x <= r + 1; x++) {
+                for (int y = c - 1; y <= c + 1; y++) {
+                    if (x < 0 || y < 0 || x >= n || y >= n) {
+                        continue;
+                    }
+                    auto p = points.find({x, y});
+                    if (p != points.end()) {
+                        points.erase(p);
+                        row[x]--;
+                        col[y]--;
+                        diagonal[x - y]--;
+                        antiDiagonal[x + y]--;
+                    }
+                }
             }
         }
-      
-        return res;
+        return ret;
     }
 };
