b14c92753a17fab7bb24762b747d9491f6713a5a
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-02-12 18:04:13 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,12 +1,40 @@
 class Solution {
 public:
-    int minimumDifference(vector<int>& nums, int k) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int ans = INT_MAX;
-        for (int i = 0; i + k - 1 < n; ++i) {
-            ans = min(ans, nums[i + k - 1] - nums[i]);
+    vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    int numEnclaves(vector<vector<int>>& grid) {
+        this->m = grid.size();
+        this->n = grid[0].size();
+        this->visited = vector<vector<bool>>(m, vector<bool>(n, false));
+        for (int i = 0; i < m; i++) {
+            dfs(grid, i, 0);
+            dfs(grid, i, n - 1);
         }
-        return ans;
+        for (int j = 1; j < n - 1; j++) {
+            dfs(grid, 0, j);
+            dfs(grid, m - 1, j);
+        }
+        int enclaves = 0;
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    enclaves++;
+                }
+            }
+        }
+        return enclaves;
     }
+
+    void dfs(const vector<vector<int>> & grid, int row, int col) {
+        if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == 0 || visited[row][col]) {
+            return;
+        }
+        visited[row][col] = true;
+        for (auto & dir : dirs) {
+            dfs(grid, row + dir[0], col + dir[1]);
+        }
+    }
+private:
+    int m, n;
+    vector<vector<bool>> visited;
 };
