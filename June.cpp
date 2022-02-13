0340df990650fce88358f401b89da89a3f9a0eb5
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-02-13 21:57:31 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-    int numEnclaves(vector<vector<int>>& grid) {
-        this->m = grid.size();
-        this->n = grid[0].size();
-        this->visited = vector<vector<bool>>(m, vector<bool>(n, false));
-        for (int i = 0; i < m; i++) {
-            dfs(grid, i, 0);
-            dfs(grid, i, n - 1);
-        }
-        for (int j = 1; j < n - 1; j++) {
-            dfs(grid, 0, j);
-            dfs(grid, m - 1, j);
-        }
-        int enclaves = 0;
-        for (int i = 1; i < m - 1; i++) {
-            for (int j = 1; j < n - 1; j++) {
-                if (grid[i][j] == 1 && !visited[i][j]) {
-                    enclaves++;
-                }
+    int maxNumberOfBalloons(string text) {
+        vector<int> cnt(5);
+        for (auto & ch : text) {
+            if (ch == 'b') {
+                cnt[0]++;
+            } else if (ch == 'a') {
+                cnt[1]++;
+            } else if (ch == 'l') {
+                cnt[2]++;
+            } else if (ch == 'o') {
+                cnt[3]++;
+            } else if (ch == 'n') {
+                cnt[4]++;
             }
         }
-        return enclaves;
+        cnt[2] /= 2;
+        cnt[3] /= 2;
+        return *min_element(cnt.begin(), cnt.end());
     }
-
-    void dfs(const vector<vector<int>> & grid, int row, int col) {
-        if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == 0 || visited[row][col]) {
-            return;
-        }
-        visited[row][col] = true;
-        for (auto & dir : dirs) {
-            dfs(grid, row + dir[0], col + dir[1]);
-        }
-    }
-private:
-    int m, n;
-    vector<vector<bool>> visited;
 };
