8c5581c978f1db266c83e26bffb68576d3903c2f
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-02-15 23:36:27 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int low = 0, high = nums.size() - 1;
-        while (low < high) {
-            int mid = (high - low) / 2 + low;
-            if (nums[mid] == nums[mid ^ 1]) {
-                low = mid + 1;
-            } else {
-                high = mid;
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> ret;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                bool isMin = true, isMax = true;
+                for (int k = 0; k < n; k++) {
+                    if (matrix[i][k] < matrix[i][j]) {
+                        isMin = false;
+                        break;
+                    }
+                }
+                if (!isMin) {
+                    continue;
+                }
+                for (int k = 0; k < m; k++) {
+                    if (matrix[k][j] > matrix[i][j]) {
+                        isMax = false;
+                        break;
+                    }
+                }
+                if (isMax) {
+                    ret.push_back(matrix[i][j]);
+                }
             }
         }
-        return nums[low];
+        return ret;
     }
 };
