5af7a2d1fbab6082686c403fe172287b4bfe382e
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-02-18 23:35:01 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    int findCenter(vector<vector<int>>& edges) {
-        int n = edges.size() + 1;
-        vector<int> degrees(n + 1);
-        for (auto & edge : edges) {
-            degrees[edge[0]]++;
-            degrees[edge[1]]++;
-        }
-        for (int i = 1; ; i++) {
-            if (degrees[i] == n - 1) {
-                return i;
+    vector<vector<int>> dirs = {{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}};
+
+    double knightProbability(int n, int k, int row, int column) {
+        vector<vector<vector<double>>> dp(k + 1, vector<vector<double>>(n, vector<double>(n)));
+        for (int step = 0; step <= k; step++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (step == 0) {
+                        dp[step][i][j] = 1;
+                    } else {
+                        for (auto & dir : dirs) {
+                            int ni = i + dir[0], nj = j + dir[1];
+                            if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                                dp[step][i][j] += dp[step - 1][ni][nj] / 8;
+                            }
+                        }
+                    }
+                }
             }
         }
+        return dp[k][row][column];
     }
 };
