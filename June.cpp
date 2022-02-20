b7b55c00784789185d5bdc0a7483ceb6f2883b81
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-02-20 23:18:47 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
-    vector<int> pancakeSort(vector<int>& arr) {
-        vector<int> ret;
-        for (int n = arr.size(); n > 1; n--) {
-            int index = max_element(arr.begin(), arr.begin() + n) - arr.begin();
-            if (index == n - 1) {
-                continue;
-            }
-            reverse(arr.begin(), arr.begin() + index + 1);
-            reverse(arr.begin(), arr.begin() + n);
-            ret.push_back(index + 1);
-            ret.push_back(n);
+    bool isOneBitCharacter(vector<int> &bits) {
+        int n = bits.size(), i = 0;
+        while (i < n - 1) {
+            i += bits[i] + 1;
         }
-        return ret;
+        return i == n - 1;
     }
 };
