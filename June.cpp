812e4d7ae2deaafc21d10625f015d0de8dbeabf6
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-03-02 23:31:26 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    string convert(string s, int numRows) {
-        int n = s.length(), r = numRows;
-        if (r == 1 || r >= n) {
-            return s;
+    vector<long> getCandidates(const string& n) {
+        int len = n.length();
+        vector<long> candidates = {
+            (long)pow(10, len - 1) - 1,
+            (long)pow(10, len) + 1,
+        };
+        long selfPrefix = stol(n.substr(0, (len + 1) / 2));
+        for (int i : {selfPrefix - 1, selfPrefix, selfPrefix + 1}) {
+            string prefix = to_string(i);
+            string candidate = prefix + string(prefix.rbegin() + (len & 1), prefix.rend());
+            candidates.push_back(stol(candidate));
         }
-        int t = r * 2 - 2;
-        int c = (n + t - 1) / t * (r - 1);
-        vector<string> mat(r, string(c, 0));
-        for (int i = 0, x = 0, y = 0; i < n; ++i) {
-            mat[x][y] = s[i];
-            if (i % t < r - 1) {
-                ++x; // 向下移动
-            } else {
-                --x;
-                ++y; // 向右上移动
-            }
-        }
-        string ans;
-        for (auto &row : mat) {
-            for (char ch : row) {
-                if (ch) {
-                    ans += ch;
+        return candidates;
+    }
+
+    string nearestPalindromic(string n) {
+        long selfNumber = stol(n), ans = -1;
+        const vector<long>& candidates = getCandidates(n);
+        for (auto& candidate : candidates) {
+            if (candidate != selfNumber) {
+                if (ans == -1 ||
+                    abs(candidate - selfNumber) < abs(ans - selfNumber) ||
+                    abs(candidate - selfNumber) == abs(ans - selfNumber) && candidate < ans) {
+                    ans = candidate;
                 }
             }
         }
-        return ans;
+        return to_string(ans);
     }
 };
