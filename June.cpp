c0431a88fa120fb2f68b5a5a7fed47228534ed4f
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-03-04 21:03:30 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int addDigits(int num) {
-        while (num >= 10) {
-            int sum = 0;
-            while (num > 0) {
-                sum += num % 10;
-                num /= 10;
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        long long ret = 0;
+        for (int i = 0; i < n; i++) {
+            int minVal = INT_MAX, maxVal = INT_MIN;
+            for (int j = i; j < n; j++) {
+                minVal = min(minVal, nums[j]);
+                maxVal = max(maxVal, nums[j]);
+                ret += maxVal - minVal;
             }
-            num = sum;
         }
-        return num;
+        return ret;
     }
 };
