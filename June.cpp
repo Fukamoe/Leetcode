a4d12e078ee568c8b14a482e1a75e6a7a568e096
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-03-10 22:20:59 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,1 +1,18 @@
-盲猜一个1
+class Solution {
+public:
+    void helper(const Node* root, vector<int> & res) {
+        if (root == nullptr) {
+            return;
+        }
+        res.emplace_back(root->val);
+        for (auto & ch : root->children) {
+            helper(ch, res);
+        }
+    }
+
+    vector<int> preorder(Node* root) {
+        vector<int> res;
+        helper(root, res);
+        return res;
+    }
+};
