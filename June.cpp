4c4ab64a4c227808f1219d2bae6bfc2fef2359cc
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-03-11 23:08:37 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
-    void helper(const Node* root, vector<int> & res) {
-        if (root == nullptr) {
-            return;
+    long maxScore = 0;
+    int cnt = 0;
+    int n;
+    vector<vector<int>> children;
+
+    int dfs(int node) {
+        long score = 1;
+        int size = n - 1;
+        for (int c : children[node]) {
+            int t = dfs(c);
+            score *= t;
+            size -= t;
         }
-        res.emplace_back(root->val);
-        for (auto & ch : root->children) {
-            helper(ch, res);
+        if (node != 0) {
+            score *= size;
         }
+        if (score == maxScore) {
+            cnt++;
+        } else if (score > maxScore) {
+            maxScore = score;
+            cnt = 1;
+        }
+        return n - size;
     }
 
-    vector<int> preorder(Node* root) {
-        vector<int> res;
-        helper(root, res);
-        return res;
+    int countHighestScoreNodes(vector<int>& parents) {
+        this->n = parents.size();
+        this->children = vector<vector<int>>(n);
+        for (int i = 0; i < n; i++) {
+            int p = parents[i];
+            if (p != -1) {
+                children[p].emplace_back(i);
+            }
+        }
+        dfs(0);
+        return cnt;
     }
 };
