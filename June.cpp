48de1c39ede35b693dba3b5eafab7f99cc8b92bf
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-03-13 20:48:27 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
-    void helper(const Node* root, vector<int> & res) {
-        if (root == nullptr) {
-            return;
-        }
-        for (auto & ch : root->children) {
-            helper(ch, res);
-        }
-        res.emplace_back(root->val);
+    static const int MASK1 = 1 << 7;
+    static const int MASK2 = (1 << 7) + (1 << 6);
+
+    bool isValid(int num) {
+        return (num & MASK2) == MASK1;
     }
 
-    vector<int> postorder(Node* root) {
-        vector<int> res;
-        helper(root, res);
-        return res;
+    int getBytes(int num) {
+        if ((num & MASK1) == 0) {
+            return 1;
+        }
+        int n = 0;
+        int mask = MASK1;
+        while ((num & mask) != 0) {
+            n++;
+            if (n > 4) {
+                return -1;
+            }
+            mask >>= 1;
+        }
+        return n >= 2 ? n : -1;
+    }
+
+    bool validUtf8(vector<int>& data) {
+        int m = data.size();
+        int index = 0;
+        while (index < m) {
+            int num = data[index];
+            int n = getBytes(num);
+            if (n < 0 || index + n > m) {
+                return false;
+            }
+            for (int i = 1; i < n; i++) {
+                if (!isValid(data[index + i])) {
+                    return false;
+                }
+            }
+            index += n;
+        }
+        return true;
     }
 };
