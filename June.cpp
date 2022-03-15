539583a294c1a7c6eadf05f60ac883da9841d473
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-03-15 22:09:10 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        unordered_map<string, int> index;
-        for (int i = 0; i < list1.size(); i++) {
-            index[list1[i]] = i;
-        }
-
-        vector<string> ret;
-        int indexSum = INT_MAX;
-        for (int i = 0; i < list2.size(); i++) {
-            if (index.count(list2[i]) > 0) {
-                int j = index[list2[i]];
-                if (i + j < indexSum) {
-                    ret.clear();
-                    ret.push_back(list2[i]);
-                    indexSum = i + j;
-                } else if (i + j == indexSum) {
-                    ret.push_back(list2[i]);
+    int countMaxOrSubsets(vector<int>& nums) {
+        int n = nums.size(), maxValue = 0, cnt = 0, stateNumber = 1 << n;
+        for (int i = 0; i < stateNumber; i++) {
+            int cur = 0;
+            for (int j = 0; j < n; j++) {
+                if (((i >> j) & 1) == 1) {
+                    cur |= nums[j];
                 }
             }
+            if (cur == maxValue) {
+                cnt++;
+            } else if (cur > maxValue) {
+                maxValue = cur;
+                cnt = 1;
+            }
         }
-        return ret;
+        return cnt;
     }
 };
