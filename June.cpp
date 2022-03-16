24f12a44fb2bac095adbd1453231a5e641b5adc5
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-03-16 21:00:02 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,21 +1,60 @@
-class Solution {
+class AllOne {
+    list<pair<unordered_set<string>, int>> lst;
+    unordered_map<string, list<pair<unordered_set<string>, int>>::iterator> nodes;
+
 public:
-    int countMaxOrSubsets(vector<int>& nums) {
-        int n = nums.size(), maxValue = 0, cnt = 0, stateNumber = 1 << n;
-        for (int i = 0; i < stateNumber; i++) {
-            int cur = 0;
-            for (int j = 0; j < n; j++) {
-                if (((i >> j) & 1) == 1) {
-                    cur |= nums[j];
-                }
+    AllOne() {}
+
+    void inc(string key) {
+        if (nodes.count(key)) {
+            auto cur = nodes[key], nxt = next(cur);
+            if (nxt == lst.end() || nxt->second > cur->second + 1) {
+                unordered_set<string> s({key});
+                nodes[key] = lst.emplace(nxt, s, cur->second + 1);
+            } else {
+                nxt->first.emplace(key);
+                nodes[key] = nxt;
             }
-            if (cur == maxValue) {
-                cnt++;
-            } else if (cur > maxValue) {
-                maxValue = cur;
-                cnt = 1;
+            cur->first.erase(key);
+            if (cur->first.empty()) {
+                lst.erase(cur);
+            }
+        } else { // key 不在链表中
+            if (lst.empty() || lst.begin()->second > 1) {
+                unordered_set<string> s({key});
+                lst.emplace_front(s, 1);
+            } else {
+                lst.begin()->first.emplace(key);
+            }
+            nodes[key] = lst.begin();
+        }
+    }
+
+    void dec(string key) {
+        auto cur = nodes[key];
+        if (cur->second == 1) { // key 仅出现一次，将其移出 nodes
+            nodes.erase(key);
+        } else {
+            auto pre = prev(cur);
+            if (cur == lst.begin() || pre->second < cur->second - 1) {
+                unordered_set<string> s({key});
+                nodes[key] = lst.emplace(cur, s, cur->second - 1);
+            } else {
+                pre->first.emplace(key);
+                nodes[key] = pre;
             }
         }
-        return cnt;
+        cur->first.erase(key);
+        if (cur->first.empty()) {
+            lst.erase(cur);
+        }
+    }
+
+    string getMaxKey() {
+        return lst.empty() ? "" : *lst.rbegin()->first.begin();
+    }
+
+    string getMinKey() {
+        return lst.empty() ? "" : *lst.begin()->first.begin();
     }
 };
