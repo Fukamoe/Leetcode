f38b947d5810cc08a2d1494c5639173c845e2214
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-03-21 21:10:26 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
-    int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
-        int n = patience.size();       
-        vector<vector<int>> adj(n);
-        vector<bool> visit(n, false);  
-        for (auto & v : edges) {
-            adj[v[0]].emplace_back(v[1]);
-            adj[v[1]].emplace_back(v[0]);
-        }
+    unordered_set<int> hashTable;
 
-        queue<int> qu;
-        qu.emplace(0);
-        visit[0] = true;
-        int dist = 1;
-        int ans = 0;
-        while (!qu.empty()) {
-            int sz = qu.size();
-            for (int i = 0; i < sz; ++i) {
-                int curr = qu.front();
-                qu.pop();
-                for (auto & v : adj[curr]) {
-                    if (visit[v]) {
-                        continue;
-                    }
-                    qu.emplace(v);
-                    int time = patience[v] * ((2 * dist - 1) / patience[v]) + 2 * dist + 1;
-                    ans = max(ans, time);
-                    visit[v] = true;
-                }
-            }
-            dist++;
+    bool findTarget(TreeNode *root, int k) {
+        if (root == nullptr) {
+            return false;
         }
-        return ans;
+        if (hashTable.count(k - root->val)) {
+            return true;
+        }
+        hashTable.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
     }
 };
