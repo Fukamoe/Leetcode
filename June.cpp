3c5760a161c41b64ba61827f818845d45c8a27b9
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-03-25 21:00:10 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
-        int m = img.size(), n = img[0].size();
-        vector<vector<int>> ret(m, vector<int>(n));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int num = 0, sum = 0;
-                for (int x = i - 1; x <= i + 1; x++) {
-                    for (int y = j - 1; y <= j + 1; y++) {
-                        if (x >= 0 && x < m && y >= 0 && y < n) {
-                            num++;
-                            sum += img[x][y];
-                        }
-                    }
-                }
-                ret[i][j] = sum / num;
+    int trailingZeroes(int n) {
+        int ans = 0;
+        for (int i = 5; i <= n; i += 5) {
+            for (int x = i; x % 5 == 0; x /= 5) {
+                ++ans;
             }
         }
-        return ret;
+        return ans;
     }
 };
