a897ac4ee5845c55092fe4f033534255b2994b29
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-03-26 21:44:28 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
-    int trailingZeroes(int n) {
-        int ans = 0;
-        for (int i = 5; i <= n; i += 5) {
-            for (int x = i; x % 5 == 0; x /= 5) {
-                ++ans;
+    int calPoints(vector<string>& ops) {
+        int ret = 0;
+        vector<int> points;
+        for (auto &op : ops) {
+            int n = points.size();
+            switch (op[0]) {
+                case '+':
+                    ret += points[n - 1] + points[n - 2];
+                    points.push_back(points[n - 1] + points[n - 2]);
+                    break;
+                case 'D':
+                    ret += 2 * points[n - 1];
+                    points.push_back(2 * points[n - 1]);
+                    break;
+                case 'C':
+                    ret -= points[n - 1];
+                    points.pop_back();
+                    break;
+                default:
+                    ret += stoi(op);
+                    points.push_back(stoi(op));
+                    break;
             }
         }
-        return ans;
+        return ret;
     }
 };
