84cc99504ae61117ef1498e4de3da3e0f1008665
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-03-30 16:53:32 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
-    int maxConsecutiveChar(string& answerKey, int k, char ch) {
-        int n = answerKey.length();
-        int ans = 0;
-        for (int left = 0, right = 0, sum = 0; right < n; right++) {
-            sum += answerKey[right] != ch;
-            while (sum > k) {
-                sum -= answerKey[left++] != ch;
-            }
-            ans = max(ans, right - left + 1);
+    vector<int> busiestServers(int k, vector<int> &arrival, vector<int> &load) {
+        set<int> available;
+        for (int i = 0; i < k; i++) {
+            available.insert(i);
         }
-        return ans;
-    }
-
-    int maxConsecutiveAnswers(string answerKey, int k) {
-        return max(maxConsecutiveChar(answerKey, k, 'T'),
-                   maxConsecutiveChar(answerKey, k, 'F'));
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> busy;
+        vector<int> requests(k);
+        for (int i = 0; i < arrival.size(); i++) {
+            while (!busy.empty() && busy.top().first <= arrival[i]) {
+                available.insert(busy.top().second);
+                busy.pop();
+            }
+            if (available.empty()) {
+                continue;
+            }
+            auto p = available.lower_bound(i % k);
+            if (p == available.end()) {
+                p = available.begin();
+            }
+            requests[*p]++;
+            busy.emplace(arrival[i] + load[i], *p);
+            available.erase(p);
+        }
+        int maxRequest = *max_element(requests.begin(), requests.end());
+        vector<int> ret;
+        for (int i = 0; i < k; i++) {
+            if (requests[i] == maxRequest) {
+                ret.push_back(i);
+            }
+        }
+        return ret;
     }
 };
