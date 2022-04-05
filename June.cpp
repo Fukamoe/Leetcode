5246f69921a32ed68901644745793550f9c1c336
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-04-05 22:46:21 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,31 +1,24 @@
-class NumArray {
-private:
-    vector<int> sum; // sum[i] 表示第 i 个块的元素和
-    int size; // 块的大小
-    vector<int> &nums;
+class Solution {
+    bool isPrime(int x) {
+        if (x < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= x; ++i) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 public:
-    NumArray(vector<int>& nums) : nums(nums) {
-        int n = nums.size();
-        size = sqrt(n);
-        sum.resize((n + size - 1) / size); // n/size 向上取整
-        for (int i = 0; i < n; i++) {
-            sum[i / size] += nums[i];
+    int countPrimeSetBits(int left, int right) {
+        int ans = 0;
+        for (int x = left; x <= right; ++x) {
+            if (isPrime(__builtin_popcount(x))) {
+                ++ans;
+            }
         }
-    }
-
-    void update(int index, int val) {
-        sum[index / size] += val - nums[index];
-        nums[index] = val;
-    }
-
-    int sumRange(int left, int right) {
-        int b1 = left / size, i1 = left % size, b2 = right / size, i2 = right % size;
-        if (b1 == b2) { // 区间 [left, right] 在同一块中
-            return accumulate(nums.begin() + b1 * size + i1, nums.begin() + b1 * size + i2 + 1, 0);
-        }
-        int sum1 = accumulate(nums.begin() + b1 * size + i1, nums.begin() + b1 * size + size, 0);
-        int sum2 = accumulate(nums.begin() + b2 * size, nums.begin() + b2 * size + i2 + 1, 0);
-        int sum3 = accumulate(sum.begin() + b1 + 1, sum.begin() + b2, 0);
-        return sum1 + sum2 + sum3;
+        return ans;
     }
 };
