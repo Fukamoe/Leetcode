8433154a968dea92f2c6f56000543e4c7b80557c
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-04-07 23:04:11 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,55 +1,22 @@
 class Solution {
 public:
-    int findLongestNode(int u, vector<int> & parent, vector<vector<int>>& adj) {
-        int n = adj.size();
-        queue<int> qu;
-        vector<bool> visit(n);
-        qu.emplace(u);
-        visit[u] = true;
-        int node = -1;
-  
-        while (!qu.empty()) {
-            int curr = qu.front();
-            qu.pop();
-            node = curr;
-            for (auto & v : adj[curr]) {
-                if (!visit[v]) {
-                    visit[v] = true;
-                    parent[v] = curr;
-                    qu.emplace(v);
+    bool rotateString(string s, string goal) {
+        int m = s.size(), n = goal.size();
+        if (m != n) {
+            return false;
+        }
+        for (int i = 0; i < n; i++) {
+            bool flag = true;
+            for (int j = 0; j < n; j++) {
+                if (s[(i + j) % n] != goal[j]) {
+                    flag = false;
+                    break;
                 }
             }
+            if (flag) {
+                return true;
+            }
         }
-        return node;
-    }
-
-    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if (n == 1) {
-            return {0};
-        }
-        vector<vector<int>> adj(n);
-        for (auto & edge : edges) {
-            adj[edge[0]].emplace_back(edge[1]);
-            adj[edge[1]].emplace_back(edge[0]);
-        }
-        
-        vector<int> parent(n, -1);
-        /* 找到与节点 0 最远的节点 x */
-        int x = findLongestNode(0, parent, adj);
-        /* 找到与节点 x 最远的节点 y */
-        int y = findLongestNode(x, parent, adj);
-        /* 求出节点 x 到节点 y 的路径 */
-        vector<int> path;
-        parent[x] = -1;
-        while (y != -1) {
-            path.emplace_back(y);
-            y = parent[y];
-        }
-        int m = path.size();
-        if (m % 2 == 0) {
-            return {path[m / 2 - 1], path[m / 2]};
-        } else {
-            return {path[m / 2]};
-        }
+        return false;
     }
 };
