0f89dc7ea329db5aacfcc28fedda73c5b5f04347
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-04-17 22:11:13 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    int largestPalindrome(int n) {
-        if (n == 1) {
-            return 9;
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        unordered_set<string> bannedSet;
+        for (auto & word : banned) {
+            bannedSet.emplace(word);
         }
-        int upper = pow(10, n) - 1;
-        for (int left = upper;; --left) { // 枚举回文数的左半部分
-            long p = left;
-            for (int x = left; x > 0; x /= 10) {
-                p = p * 10 + x % 10; // 翻转左半部分到其自身末尾，构造回文数 p
-            }
-            for (long x = upper; x * x >= p; --x) {
-                if (p % x == 0) { // x 是 p 的因子
-                    return p % 1337;
+        int maxFrequency = 0;
+        unordered_map<string, int> frequencies;
+        string word;
+        int length = paragraph.size();
+        for (int i = 0; i <= length; i++) {
+            if (i < length && isalpha(paragraph[i])) {
+                word.push_back(tolower(paragraph[i]));
+            } else if (word.size() > 0) {
+                if (!bannedSet.count(word)) {
+                    frequencies[word]++;
+                    maxFrequency = max(maxFrequency, frequencies[word]);
                 }
+                word = "";
             }
         }
+        string mostCommon = "";
+        for (auto &[word , frequency] : frequencies) {
+            if (frequency == maxFrequency) {
+                mostCommon = word;
+                break;
+            }
+        }
+        return mostCommon;
     }
 };
