d67006740777d798039d67e3d479a99fded0edc2
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-04-20 23:43:15 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
-    vector<int> shortestToChar(string s, char c) {
-        int n = s.length();
-        vector<int> ans(n);
+    int lengthLongestPath(string input) {
+        int n = input.size();
+        int pos = 0;
+        int ans = 0;
+        stack<int> st;
 
-        for (int i = 0, idx = -n; i < n; ++i) {
-            if (s[i] == c) {
-                idx = i;
+        while (pos < n) {
+            /* 检测当前文件的深度 */
+            int depth = 1;
+            while (pos < n && input[pos] == '\t') {
+                pos++;
+                depth++;
             }
-            ans[i] = i - idx;
-        }
+            /* 统计当前文件名的长度 */   
+            int len = 0; 
+            bool isFile = false;     
+            while (pos < n && input[pos] != '\n') {
+                if (input[pos] == '.') {
+                    isFile = true;
+                }
+                len++;
+                pos++;
+            }
+            /* 跳过换行符 */
+            pos++;
 
-        for (int i = n - 1, idx = 2 * n; i >= 0; --i) {
-            if (s[i] == c) {
-                idx = i;
+            while (st.size() >= depth) {
+                st.pop();
             }
-            ans[i] = min(ans[i], idx - i);
+            if (!st.empty()) {
+                len += st.top() + 1;
+            }
+            if (isFile) {
+                ans = max(ans, len);
+            } else {
+                st.emplace(len);
+            }
         }
         return ans;
     }
