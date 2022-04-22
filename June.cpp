fa535c4f27c555b3d8052bd53e167fc8364cf661
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-04-22 23:11:55 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
-    string toGoatLatin(string sentence) {
-        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-
-        int n = sentence.size();
-        int i = 0, cnt = 1;
-        string ans;
-
-        while (i < n) {
-            int j = i;
-            while (j < n && sentence[j] != ' ') {
-                ++j;
-            }
-
-            ++cnt;
-            if (cnt != 2) {
-                ans += ' ';
-            }
-            if (vowels.count(sentence[i])) {
-                ans += sentence.substr(i, j - i) + 'm' + string(cnt, 'a');
-            }
-            else {
-                ans += sentence.substr(i + 1, j - i - 1) + sentence[i] + 'm' + string(cnt, 'a');
-            }
-
-            i = j + 1;
+    int maxRotateFunction(vector<int>& nums) {
+        int f = 0, n = nums.size();
+        int numSum = accumulate(nums.begin(), nums.end(), 0);
+        for (int i = 0; i < n; i++) {
+            f += i * nums[i];
         }
-
-        return ans;
+        int res = f;
+        for (int i = n - 1; i > 0; i--) {
+            f += numSum - n * nums[i];
+            res = max(res, f);
+        }
+        return res;
     }
 };
