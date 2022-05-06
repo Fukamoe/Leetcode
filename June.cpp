9d16224dd960fb0ad926158a7a054f8afda3e671
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-06 23:18:01 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,20 +1,13 @@
-class Solution {
+class RecentCounter {
+    queue<int> q;
 public:
-    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        if (k == 0) {
-            return 0;
+    RecentCounter() {}
+
+    int ping(int t) {
+        q.push(t);
+        while (q.front() < t - 3000) {
+            q.pop();
         }
-        int n = nums.size();
-        vector<double> logPrefix(n + 1);
-        for (int i = 0; i < n; i++) {
-            logPrefix[i + 1] = logPrefix[i] + log(nums[i]);
-        }
-        double logk = log(k);
-        int ret = 0;
-        for (int j = 0; j < n; j++) {
-            int l = upper_bound(logPrefix.begin(), logPrefix.begin() + j + 1, logPrefix[j + 1] - log(k) + 1e-10) - logPrefix.begin();
-            ret += j + 1 - l;
-        }
-        return ret;
+        return q.size();
     }
 };
