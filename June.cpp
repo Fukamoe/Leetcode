bc217c14662472cf868f23e8ebe419def4a45f41
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-10 23:58:48 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -5,8 +5,5 @@ public:
         vector<int> perm(n + 1);
         for (int i = 0; i < n; ++i) {
             perm[i] = s[i] == 'I' ? lo++ : hi--;
-        }
-        perm[n] = lo; // 最后剩下一个数，此时 lo == hi
-        return perm;
-    }
+       
 };
