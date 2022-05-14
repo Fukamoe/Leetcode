4ba330d8aed75f8a66226f8cee30462240614695
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-14 23:40:01 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    bool oneEditAway(string first, string second) {
-        int m = first.size(), n = second.size();
-        if (n - m == 1) {
-            return oneInsert(first, second);
-        } else if (m - n == 1) {
-            return oneInsert(second, first);
-        } else if (m == n) {
-            bool foundDifference = false;
-            for (int i = 0; i < m; i++) {
-                if (first[i] != second[i]) {
-                    if (!foundDifference) {
-                        foundDifference = true;
-                    } else {
-                        return false;
+    int minStickers(vector<string>& stickers, string target) {
+        int m = target.size();
+        vector<int> dp(1 << m, -1);
+        dp[0] = 0;
+        function<int(int)> helper = [&](int mask) {
+            if (dp[mask] != -1) {
+                return dp[mask];
+            }
+            dp[mask] = m + 1;
+            for (auto & sticker : stickers) {
+                int left = mask;
+                vector<int> cnt(26);
+                for (char & c : sticker) {
+                    cnt[c - 'a']++;
+                }
+                for (int i = 0; i < m; i++) {
+                    if ((mask >> i & 1) && cnt[target[i] - 'a'] > 0) {
+                        cnt[target[i] - 'a']--;
+                        left ^= 1 << i;
                     }
                 }
+                if (left < mask) {
+                    dp[mask] = min(dp[mask], helper(left) + 1);
+                }
             }
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    bool oneInsert(string shorter, string longer) {
-        int length1 = shorter.size(), length2 = longer.size();
-        int index1 = 0, index2 = 0;
-        while (index1 < length1 && index2 < length2) {
-            if (shorter[index1] == longer[index2]) {
-                index1++;
-            }
-            index2++;
-            if (index2 - index1 > 1) {
-                return false;
-            }
-        }
-        return true;
+            return dp[mask];
+        };
+        int res = helper((1 << m) - 1);
+        return res > m ? -1 : res;
     }
 };
