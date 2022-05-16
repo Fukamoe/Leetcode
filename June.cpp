c0b1ed87069c0b83fdf9654f2a0653eb12c73a51
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-16 23:29:55 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    double triangleArea(int x1, int y1, int x2, int y2, int x3, int y3) {
-        return 0.5 * abs(x1 * y2 + x2 * y3 + x3 * y1 - x1 * y3 - x2 * y1 - x3 * y2);
-    }
-
-    double largestTriangleArea(vector<vector<int>> & points) {
-        int n = points.size();
-        double ret = 0.0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    ret = max(ret, triangleArea(points[i][0], points[i][1], points[j][0], points[j][1], points[k][0], points[k][1]));
-                }
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        stack<TreeNode*> st;
+        TreeNode *prev = nullptr, *curr = root;
+        while (!st.empty() || curr != nullptr) {
+            while (curr != nullptr) {
+                st.emplace(curr);
+                curr = curr->left;
             }
+            curr = st.top();
+            st.pop();
+            if (prev == p) {
+                return curr;
+            }
+            prev = curr;
+            curr = curr->right;
         }
-        return ret;
+        return nullptr;
     }
 };
