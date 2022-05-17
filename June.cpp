f6d1105c0b24035ac03ccfef957e5a4411814e7a
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-17 23:54:24 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-        stack<TreeNode*> st;
-        TreeNode *prev = nullptr, *curr = root;
-        while (!st.empty() || curr != nullptr) {
-            while (curr != nullptr) {
-                st.emplace(curr);
-                curr = curr->left;
-            }
-            curr = st.top();
-            st.pop();
-            if (prev == p) {
-                return curr;
-            }
-            prev = curr;
-            curr = curr->right;
+    bool isAlienSorted(vector<string>& words, string order) {
+        vector<int> index(26);
+        for (int i = 0; i < order.size(); i++) {
+            index[order[i] - 'a'] = i;
         }
-        return nullptr;
+        for (int i = 1; i < words.size(); i++) {
+            bool valid = false;
+            for (int j = 0; j < words[i - 1].size() && j < words[i].size(); j++) {
+                int prev = index[words[i - 1][j] - 'a'];
+                int curr = index[words[i][j] - 'a'];
+                if (prev < curr) {
+                    valid = true;
+                    break;
+                } else if (prev > curr) {
+                    return false;
+                }
+            }
+            if (!valid) {
+                /* 比较两个字符串的长度 */
+                if (words[i - 1].size() > words[i].size()) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 };
