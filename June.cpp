466450991da025654f18295af2ee4abb463f0934
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-19 23:19:09 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-    int findKthNumber(int m, int n, int k) {
-        int left = 1, right = m * n;
-        while (left < right) {
-            int x = left + (right - left) / 2;
-            int count = x / n * n;
-            for (int i = x / n + 1; i <= m; ++i) {
-                count += x / i;
-            }
-            if (count >= k) {
-                right = x;
-            } else {
-                left = x + 1;
-            }
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), ret = 0, x = nums[n / 2];
+        for (int i = 0; i < n; i++) {
+            ret += abs(nums[i] - x);
         }
-        return left;
+        return ret;
     }
 };
