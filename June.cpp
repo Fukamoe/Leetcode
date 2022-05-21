bf2b8475ed085c7729c6e78f5eaed0d09e8c9137
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-21 22:58:30 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    vector<int> findRightInterval(vector<vector<int>>& intervals) {
-        vector<pair<int, int>> startIntervals;
-        int n = intervals.size();
-        for (int i = 0; i < n; i++) {
-            startIntervals.emplace_back(intervals[i][0], i);
-        }
-        sort(startIntervals.begin(), startIntervals.end());
-
-        vector<int> ans(n, -1);
-        for (int i = 0; i < n; i++) {
-            auto it = lower_bound(startIntervals.begin(), startIntervals.end(), make_pair(intervals[i][1], 0));
-            if (it != startIntervals.end()) {
-                ans[i] = it->second;
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_set<int> found;
+        for (int num: nums) {
+            if (found.count(num)) {
+                return num;
             }
+            found.insert(num);
         }
-        return ans;
+        // 不可能的情况
+        return -1;
     }
 };
