70394ee37a6e3feabfd50a6fb2134d69ac45cfaf
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-22 20:55:15 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
-    int repeatedNTimes(vector<int>& nums) {
-        unordered_set<int> found;
-        for (int num: nums) {
-            if (found.count(num)) {
-                return num;
-            }
-            found.insert(num);
+    unordered_map<int, bool> memo;
+
+    bool canIWin(int maxChoosableInteger, int desiredTotal) {
+        if ((1 + maxChoosableInteger) * (maxChoosableInteger) / 2 < desiredTotal) {
+            return false;
         }
-        // 不可能的情况
-        return -1;
+        return dfs(maxChoosableInteger, 0, desiredTotal, 0);
+    }
+
+    bool dfs(int maxChoosableInteger, int usedNumbers, int desiredTotal, int currentTotal) {
+        if (!memo.count(usedNumbers)) {
+            bool res = false;
+            for (int i = 0; i < maxChoosableInteger; i++) {
+                if (((usedNumbers >> i) & 1) == 0) {
+                    if (i + 1 + currentTotal >= desiredTotal) {
+                        res = true;
+                        break;
+                    }
+                    if (!dfs(maxChoosableInteger, usedNumbers | (1 << i), desiredTotal, currentTotal + i + 1)) {
+                        res = true;
+                        break;
+                    }
+                }
+            }
+            memo[usedNumbers] = res;
+        }
+        return memo[usedNumbers];
     }
 };
