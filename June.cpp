b4c735778fc01d2c7e852fedd1ed74fea8d0a3ba
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-23 23:04:39 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,31 +1,70 @@
 class Solution {
 public:
-    unordered_map<int, bool> memo;
+    int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-    bool canIWin(int maxChoosableInteger, int desiredTotal) {
-        if ((1 + maxChoosableInteger) * (maxChoosableInteger) / 2 < desiredTotal) {
-            return false;
+    int bfs(vector<vector<int>>& forest, int sx, int sy, int tx, int ty) {
+        if (sx == tx && sy == ty) {
+            return 0;
         }
-        return dfs(maxChoosableInteger, 0, desiredTotal, 0);
-    }
 
-    bool dfs(int maxChoosableInteger, int usedNumbers, int desiredTotal, int currentTotal) {
-        if (!memo.count(usedNumbers)) {
-            bool res = false;
-            for (int i = 0; i < maxChoosableInteger; i++) {
-                if (((usedNumbers >> i) & 1) == 0) {
-                    if (i + 1 + currentTotal >= desiredTotal) {
-                        res = true;
-                        break;
-                    }
-                    if (!dfs(maxChoosableInteger, usedNumbers | (1 << i), desiredTotal, currentTotal + i + 1)) {
-                        res = true;
-                        break;
+        int row = forest.size();
+        int col = forest[0].size();
+        int step = 0;
+        queue<pair<int, int>> qu;
+        vector<vector<bool>> visited(row, vector<bool>(col, false));         
+        qu.emplace(sx, sy);
+        visited[sx][sy] = true;
+        while (!qu.empty()) {
+            step++;
+            int sz = qu.size();
+            for (int i = 0; i < sz; ++i) {
+                auto [cx, cy] = qu.front();
+                qu.pop();
+                for (int j = 0; j < 4; ++j) {
+                    int nx = cx + dirs[j][0];
+                    int ny = cy + dirs[j][1];
+                    if (nx >= 0 && nx < row && ny >= 0 && ny < col) {
+                        if (!visited[nx][ny] && forest[nx][ny] > 0) {
+                            if (nx == tx && ny == ty) {
+                                return step;
+                            }
+                            qu.emplace(nx, ny);
+                            visited[nx][ny] = true;
+                        }
                     }
                 }
             }
-            memo[usedNumbers] = res;
         }
-        return memo[usedNumbers];
+        return -1;
+    }
+
+    int cutOffTree(vector<vector<int>>& forest) {
+        vector<pair<int, int>> trees;
+        int row = forest.size();
+        int col = forest[0].size();
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (forest[i][j] > 1) {
+                    trees.emplace_back(i, j);
+                }
+            }
+        }
+        sort(trees.begin(), trees.end(), [&](const pair<int, int> & a, const pair<int, int> & b) {
+            return forest[a.first][a.second] < forest[b.first][b.second];
+        });
+
+        int cx = 0;
+        int cy = 0;
+        int ans = 0;
+        for (int i = 0; i < trees.size(); ++i) {
+            int steps = bfs(forest, cx, cy, trees[i].first, trees[i].second);
+            if (steps == -1) {
+                return -1;
+            }
+            ans += steps;
+            cx = trees[i].first;
+            cy = trees[i].second;
+        }
+        return ans;
     }
 };
