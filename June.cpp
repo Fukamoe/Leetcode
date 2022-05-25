482d2ab1e803fd7c1a6f195ca3619753dab28cdc
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-25 22:43:49 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    bool isUnivalTree(TreeNode* root) {
-        if (!root) {
-            return true;
-        }
-        if (root->left) {
-            if (root->val != root->left->val || !isUnivalTree(root->left)) {
-                return false;
+    int findSubstringInWraproundString(string p) {
+        vector<int> dp(26);
+        int k = 0;
+        for (int i = 0; i < p.length(); ++i) {
+            if (i && (p[i] - p[i - 1] + 26) % 26 == 1) { // 字符之差为 1 或 -25
+                ++k;
+            } else {
+                k = 1;
             }
+            dp[p[i] - 'a'] = max(dp[p[i] - 'a'], k);
         }
-        if (root->right) {
-            if (root->val != root->right->val || !isUnivalTree(root->right)) {
-                return false;
-            }
-        }
-        return true;
+        return accumulate(dp.begin(), dp.end(), 0);
     }
 };
