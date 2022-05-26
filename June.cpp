25ac1c16a0cf80322cb6f45a12f98cfa220e0872
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-26 22:43:06 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    int findSubstringInWraproundString(string p) {
-        vector<int> dp(26);
-        int k = 0;
-        for (int i = 0; i < p.length(); ++i) {
-            if (i && (p[i] - p[i - 1] + 26) % 26 == 1) { // 字符之差为 1 或 -25
-                ++k;
-            } else {
-                k = 1;
+    vector<int> fallingSquares(vector<vector<int>>& positions) {
+        int n = positions.size();
+        vector<int> heights(n);
+        for (int i = 0; i < n; i++) {
+            int left1 = positions[i][0], right1 = positions[i][0] + positions[i][1] - 1;
+            heights[i] = positions[i][1];
+            for (int j = 0; j < i; j++) {
+                int left2 = positions[j][0], right2 = positions[j][0] + positions[j][1] - 1;
+                if (right1 >= left2 && right2 >= left1) {
+                    heights[i] = max(heights[i], heights[j] + positions[i][1]);
+                }
             }
-            dp[p[i] - 'a'] = max(dp[p[i] - 'a'], k);
         }
-        return accumulate(dp.begin(), dp.end(), 0);
+        for (int i = 1; i < n; i++) {
+            heights[i] = max(heights[i], heights[i - 1]);
+        }
+        return heights;
     }
 };
