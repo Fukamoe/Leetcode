0e732b53831fbbb26534e3447cf3e088052af544
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-28 21:45:01 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int findClosest(vector<string>& words, string word1, string word2) {
-        int length = words.size();
-        int ans = length;
-        int index1 = -1, index2 = -1;
-        for (int i = 0; i < length; i++) {
-            string word = words[i];
-            if (words[i] == word1) {
-                index1 = i;
-            } else if (words[i] == word2) {
-                index2 = i;
+    string removeOuterParentheses(string s) {
+        string res;
+        stack<char> st;
+        for (auto c : s) {
+            if (c == ')') {
+                st.pop();
             }
-            if (index1 >= 0 && index2 >= 0) {
-                ans = min(ans, abs(index1 - index2));
+            if (!st.empty()) {
+                res.push_back(c);
+            }
+            if (c == '(') {
+                st.emplace(c);
             }
         }
-        return ans;
+        return res;
     }
 };
