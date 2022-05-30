d9945fb9787365d818b1f6553ff90d72277c4834
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-30 22:34:56 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,56 +1,17 @@
 class Solution {
 public:
-    string validIPAddress(string queryIP) {
-        if (queryIP.find('.') != string::npos) {
-            // IPv4
-            int last = -1;
-            for (int i = 0; i < 4; ++i) {
-                int cur = (i == 3 ? queryIP.size() : queryIP.find('.', last + 1));
-                if (cur == string::npos) {
-                    return "Neither";
-                }
-                if (cur - last - 1 < 1 || cur - last - 1 > 3) {
-                    return "Neither";
-                }
-                int addr = 0;
-                for (int j = last + 1; j < cur; ++j) {
-                    if (!isdigit(queryIP[j])) {
-                        return "Neither";
-                    }
-                    addr = addr * 10 + (queryIP[j] - '0');
-                }
-                if (addr > 255) {
-                    return "Neither";
-                }
-                if (addr > 0 && queryIP[last + 1] == '0') {
-                    return "Neither";
-                }
-                if (addr == 0 && cur - last - 1 > 1) {
-                    return "Neither";
-                }
-                last = cur;
-            }
-            return "IPv4";
+    int dfs(TreeNode *root, int val) {
+        if (root == nullptr) {
+            return 0;
         }
-        else {
-            // IPv6
-            int last = -1;
-            for (int i = 0; i < 8; ++i) {
-                int cur = (i == 7 ? queryIP.size() : queryIP.find(':', last + 1));
-                if (cur == string::npos) {
-                    return "Neither";
-                }
-                if (cur - last - 1 < 1 || cur - last - 1 > 4) {
-                    return "Neither";
-                }
-                for (int j = last + 1; j < cur; ++j) {
-                    if (!isdigit(queryIP[j]) && !('a' <= tolower(queryIP[j]) && tolower(queryIP[j]) <= 'f')) {
-                        return "Neither";
-                    }
-                }
-                last = cur;
-            }
-            return "IPv6";
+        val = (val << 1) | root->val;
+        if (root->left == nullptr && root->right == nullptr) {
+            return val;
         }
+        return dfs(root->left, val) + dfs(root->right, val);
+    }
+
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root, 0);
     }
 };
