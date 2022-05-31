e445e0059cb18a9e50d9adcead081da51b80ec58
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-05-31 22:07:16 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,17 +1,71 @@
 class Solution {
 public:
-    int dfs(TreeNode *root, int val) {
-        if (root == nullptr) {
-            return 0;
+    const int VISITING = 1, VISITED = 2;
+    unordered_map<char, vector<char>> edges;
+    unordered_map<char, int> states;
+    bool valid = true;
+    string order;
+    int index;
+
+    string alienOrder(vector<string>& words) {
+        int length = words.size();
+        for (string & word : words) {
+            int wordLength = word.size();
+            for (int j = 0; j < wordLength; j++) {
+                char c = word[j];
+                if (!edges.count(c)) {
+                    edges[c] = vector<char>();
+                }
+            }
         }
-        val = (val << 1) | root->val;
-        if (root->left == nullptr && root->right == nullptr) {
-            return val;
+        for (int i = 1; i < length && valid; i++) {
+            addEdge(words[i - 1], words[i]);
         }
-        return dfs(root->left, val) + dfs(root->right, val);
+        order = string(edges.size(), ' ');
+        index = edges.size() - 1;
+        for (auto [u, _] : edges) {
+            if (!states.count(u)) {
+                dfs(u);
+            }
+        }
+        if (!valid) {
+            return "";
+        }
+        return order;
     }
 
-    int sumRootToLeaf(TreeNode* root) {
-        return dfs(root, 0);
+    void addEdge(string before, string after) {
+        int length1 = before.size(), length2 = after.size();
+        int length = min(length1, length2);
+        int index = 0;
+        while (index < length) {
+            char c1 = before[index], c2 = after[index];
+            if (c1 != c2) {
+                edges[c1].emplace_back(c2);
+                break;
+            }
+            index++;
+        }
+        if (index == length && length1 > length2) {
+            valid = false;
+        }
+    }
+
+    void dfs(char u) {
+        states[u] = VISITING;
+        for (char v : edges[u]) {
+            if (!states.count(v)) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            } else if (states[v] == VISITING) {
+                valid = false;
+                return;
+            }
+        }
+        states[u] = VISITED;
+        order[index] = u;
+        index--;
     }
 };
