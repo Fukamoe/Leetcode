cdfe5e3bae00da1a268d516b18080ea26d21c3ef
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-06-05 23:27:42 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,19 +1,18 @@
 class Solution {
+private:
+    mt19937 gen{random_device{}()};
+    uniform_real_distribution<double> dis;
+    double xc, yc, r;
+
 public:
-    int numUniqueEmails(vector<string> &emails) {
-        unordered_set<string> emailSet;
-        for (auto &email: emails) {
-            string local;
-            for (char c: email) {
-                if (c == '+' || c == '@') {
-                    break;
-                }
-                if (c != '.') {
-                    local += c;
-                }
+    Solution(double radius, double x_center, double y_center): dis(-radius, radius), xc(x_center), yc(y_center), r(radius) {}
+    
+    vector<double> randPoint() {
+        while (true) {
+            double x = dis(gen), y = dis(gen);
+            if (x * x + y * y <= r * r) {
+                return {xc + x, yc + y};
             }
-            emailSet.emplace(local + email.substr(email.find('@')));
         }
-        return emailSet.size();
     }
 };
