5ca015cbcf55722ab1a4c50bed0ee1049dd6c1b1
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-06-08 23:23:26 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,31 +1,8 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1;
-        int high = 0;
-        for (int pile : piles) {
-            high = max(high, pile);
-        }
-        int k = high;
-        while (low < high) {
-            int speed = (high - low) / 2 + low;
-            long time = getTime(piles, speed);
-            if (time <= h) {
-                k = speed;
-                high = speed;
-            } else {
-                low = speed + 1;
-            }
-        }
-        return k;
-    }
-
-    long getTime(const vector<int>& piles, int speed) {
-        long time = 0;
-        for (int pile : piles) {
-            int curTime = (pile + speed - 1) / speed;
-            time += curTime;
-        }
-        return time;
+    bool isBoomerang(vector<vector<int>>& points) {
+        vector<int> v1 = {points[1][0] - points[0][0], points[1][1] - points[0][1]};
+        vector<int> v2 = {points[2][0] - points[0][0], points[2][1] - points[0][1]};
+        return v1[0] * v2[1] - v1[1] * v2[0] != 0;
     }
 };
