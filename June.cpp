62fff86aa9099cf64d53fa0a45199246e3651ffe
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-06-16 23:18:54 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int smallestDistancePair(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size(), left = 0, right = nums.back() - nums.front();
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                int i = lower_bound(nums.begin(), nums.begin() + j, nums[j] - mid) - nums.begin();
-                cnt += j - i;
+    int findPairs(vector<int>& nums, int k) {
+        unordered_set<int> visited;
+        unordered_set<int> res;
+        for (int num : nums) {
+            if (visited.count(num - k)) {
+                res.emplace(num - k);
             }
-            if (cnt >= k) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+            if (visited.count(num + k)) {
+                res.emplace(num);
             }
+            visited.emplace(num);
         }
-        return left;
+        return res.size();
     }
 };
