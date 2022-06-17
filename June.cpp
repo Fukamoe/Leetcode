ea48657de1905cca2c4752465df93bf185f088cb
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-06-17 21:58:56 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
-    int findPairs(vector<int>& nums, int k) {
-        unordered_set<int> visited;
-        unordered_set<int> res;
-        for (int num : nums) {
-            if (visited.count(num - k)) {
-                res.emplace(num - k);
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+        int top = 0;
+        int i = -1;
+        while (top < n) {
+            i++;
+            if (arr[i] != 0) {
+                top++;
+            } else {
+                top += 2;
             }
-            if (visited.count(num + k)) {
-                res.emplace(num);
-            }
-            visited.emplace(num);
         }
-        return res.size();
+        int j = n - 1;
+        if (top == n + 1) {
+            arr[j] = 0;
+            j--;
+            i--;
+        } 
+        while (j >= 0) {
+            arr[j] = arr[i];
+            j--;
+            if (!arr[i]) {
+                arr[j] = arr[i];
+                j--;
+            } 
+            i--;
+        }
     }
 };
