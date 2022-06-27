633b03ba5b952f6ce9211862469ef9ce85c73af2
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-06-27 20:21:43 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,31 +1,31 @@
 class Solution {
-    unordered_map<int, int> b2w;
-    int bound;
-
 public:
-    Solution(int n, vector<int> &blacklist) {
-        int m = blacklist.size();
-        bound = n - m;
-        unordered_set<int> black;
-        for (int b: blacklist) {
-            if (b >= bound) {
-                black.emplace(b);
-            }
-        }
-
-        int w = bound;
-        for (int b: blacklist) {
-            if (b < bound) {
-                while (black.count(w)) {
-                    ++w;
+    int findLUSlength(vector<string>& strs) {
+        auto is_subseq = [](const string& s, const string& t) -> bool {
+            int pt_s = 0, pt_t = 0;
+            while (pt_s < s.size() && pt_t < t.size()) {
+                if (s[pt_s] == t[pt_t]) {
+                    ++pt_s;
                 }
-                b2w[b] = w++;
+                ++pt_t;
+            }
+            return pt_s == s.size();
+        };
+
+        int n = strs.size();
+        int ans = -1;
+        for (int i = 0; i < n; ++i) {
+            bool check = true;
+            for (int j = 0; j < n; ++j) {
+                if (i != j && is_subseq(strs[i], strs[j])) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                ans = max(ans, static_cast<int>(strs[i].size()));
             }
         }
-    }
-
-    int pick() {
-        int x = rand() % bound;
-        return b2w.count(x) ? b2w[x] : x;
+        return ans;
     }
 };
