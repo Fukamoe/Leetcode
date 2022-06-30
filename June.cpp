f5bc5b36331e468f173e90a1349e9b909d1c83d2
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-06-30 22:18:44 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,22 +1,35 @@
+const int MOD = 1e9 + 7;
+
 class Solution {
-private:
-    unordered_map<int, string> dataBase;
-    int id;
-
 public:
-    Solution() {
-        id = 0;
+    int numPrimeArrangements(int n) {
+        int numPrimes = 0;
+        for (int i = 1; i <= n; i++) {
+            if (isPrime(i)) {
+                numPrimes++;
+            }
+        }
+        return (int) (factorial(numPrimes) * factorial(n - numPrimes) % MOD);
     }
 
-    string encode(string longUrl) {
-        id++;
-        dataBase[id] = longUrl;
-        return string("http://tinyurl.com/") + to_string(id);
+    bool isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    string decode(string shortUrl) {
-        int p = shortUrl.rfind('/') + 1;
-        int key = stoi(shortUrl.substr(p, int(shortUrl.size()) - p));
-        return dataBase[key];
+    long factorial(int n) {
+        long res = 1;
+        for (int i = 1; i <= n; i++) {
+            res *= i;
+            res %= MOD;
+        }
+        return res;
     }
 };
