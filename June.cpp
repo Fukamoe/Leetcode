40a67e708e6a7a72fbf1a0843e31d3eff64ebac3
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-07-01 23:19:59 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,35 +1,56 @@
-const int MOD = 1e9 + 7;
-
 class Solution {
 public:
-    int numPrimeArrangements(int n) {
-        int numPrimes = 0;
-        for (int i = 1; i <= n; i++) {
-            if (isPrime(i)) {
-                numPrimes++;
+    const int ADDITION = -1;
+    const int SUBTRACTION = -2;
+    const int MULTIPLICATION = -3;
+
+    vector<int> dfs(vector<vector<vector<int>>>& dp, int l, int r, const vector<int>& ops) {
+        if (dp[l][r].empty()) {
+            if (l == r) {
+                dp[l][r].push_back(ops[l]);
+            } else {
+                for (int i = l; i < r; i += 2) {
+                    auto left = dfs(dp, l, i, ops);
+                    auto right = dfs(dp, i + 2, r, ops);
+                    for (auto& lv : left) {
+                        for (auto& rv : right) {
+                            if (ops[i + 1] == ADDITION) {
+                                dp[l][r].push_back(lv + rv);
+                            } else if (ops[i + 1] == SUBTRACTION) {
+                                dp[l][r].push_back(lv - rv);
+                            } else {
+                                dp[l][r].push_back(lv * rv);
+                            }
+                        }
+                    }
+                }
             }
         }
-        return (int) (factorial(numPrimes) * factorial(n - numPrimes) % MOD);
+        return dp[l][r];
     }
 
-    bool isPrime(int n) {
-        if (n == 1) {
-            return false;
-        }
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                return false;
+    vector<int> diffWaysToCompute(string expression) {
+        vector<int> ops;
+        for (int i = 0; i < expression.size();) {
+            if (!isdigit(expression[i])) {
+                if (expression[i] == '+') {
+                    ops.push_back(ADDITION);
+                } else if (expression[i] == '-') {
+                    ops.push_back(SUBTRACTION);
+                } else {
+                    ops.push_back(MULTIPLICATION);
+                }
+                i++;
+            } else {
+                int t = 0;
+                while (i < expression.size() && isdigit(expression[i])) {
+                    t = t * 10 + expression[i] - '0';
+                    i++;
+                }
+                ops.push_back(t);
             }
         }
-        return true;
-    }
-
-    long factorial(int n) {
-        long res = 1;
-        for (int i = 1; i <= n; i++) {
-            res *= i;
-            res %= MOD;
-        }
-        return res;
+        vector<vector<vector<int>>> dp((int) ops.size(), vector<vector<int>>((int) ops.size()));
+        return dfs(dp, 0, ops.size() - 1, ops);
     }
 };
