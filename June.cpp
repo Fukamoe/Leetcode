043a566d371cb5242a74b3b129052d59de936375
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-07-02 22:51:38 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,56 +1,21 @@
 class Solution {
 public:
-    const int ADDITION = -1;
-    const int SUBTRACTION = -2;
-    const int MULTIPLICATION = -3;
-
-    vector<int> dfs(vector<vector<vector<int>>>& dp, int l, int r, const vector<int>& ops) {
-        if (dp[l][r].empty()) {
-            if (l == r) {
-                dp[l][r].push_back(ops[l]);
-            } else {
-                for (int i = l; i < r; i += 2) {
-                    auto left = dfs(dp, l, i, ops);
-                    auto right = dfs(dp, i + 2, r, ops);
-                    for (auto& lv : left) {
-                        for (auto& rv : right) {
-                            if (ops[i + 1] == ADDITION) {
-                                dp[l][r].push_back(lv + rv);
-                            } else if (ops[i + 1] == SUBTRACTION) {
-                                dp[l][r].push_back(lv - rv);
-                            } else {
-                                dp[l][r].push_back(lv * rv);
-                            }
-                        }
-                    }
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        int n = stations.size();
+        vector<long> dp(n + 1);
+        dp[0] = startFuel;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (dp[j] >= stations[i][0]) {
+                    dp[j + 1] = max(dp[j + 1], dp[j] + stations[i][1]);
                 }
             }
         }
-        return dp[l][r];
-    }
-
-    vector<int> diffWaysToCompute(string expression) {
-        vector<int> ops;
-        for (int i = 0; i < expression.size();) {
-            if (!isdigit(expression[i])) {
-                if (expression[i] == '+') {
-                    ops.push_back(ADDITION);
-                } else if (expression[i] == '-') {
-                    ops.push_back(SUBTRACTION);
-                } else {
-                    ops.push_back(MULTIPLICATION);
-                }
-                i++;
-            } else {
-                int t = 0;
-                while (i < expression.size() && isdigit(expression[i])) {
-                    t = t * 10 + expression[i] - '0';
-                    i++;
-                }
-                ops.push_back(t);
+        for (int i = 0; i <= n; i++) {
+            if (dp[i] >= target) {
+                return i;
             }
         }
-        vector<vector<vector<int>>> dp((int) ops.size(), vector<vector<int>>((int) ops.size()));
-        return dfs(dp, 0, ops.size() - 1, ops);
+        return -1;
     }
 };
