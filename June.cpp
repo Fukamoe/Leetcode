ae4939678b8fd8c7ba3790a3fd20590dd61e3495
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-07-05 23:17:43 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,21 +1,14 @@
-class Solution {
-public:
-    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-        int n = arr.size();
-        sort(arr.begin(), arr.end());
+class MyCalendar {
+    vector<pair<int, int>> booked;
 
-        int best = INT_MAX;
-        vector<vector<int>> ans;
-        for (int i = 0; i < n - 1; ++i) {
-            if (int delta = arr[i + 1] - arr[i]; delta < best) {
-                best = delta;
-                ans = {{arr[i], arr[i + 1]}};
-            }
-            else if (delta == best) {
-                ans.emplace_back(initializer_list<int>{arr[i], arr[i + 1]});
+public:
+    bool book(int start, int end) {
+        for (auto &[l, r] : booked) {
+            if (l < end && start < r) {
+                return false;
             }
         }
-
-        return ans;
+        booked.emplace_back(start, end);
+        return true;
     }
 };
