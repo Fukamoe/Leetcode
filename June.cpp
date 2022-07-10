a6233641a39dc1235ab914c467921f9b18b1fdf0
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-07-10 22:18:29 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
-    int lenLongestFibSubseq(vector<int>& arr) {
-        unordered_map<int, int> indices;
-        int n = arr.size();
-        for (int i = 0; i < n; i++) {
-            indices[arr[i]] = i;
-        }
-        vector<vector<int>> dp(n, vector<int>(n));
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i - 1; j >= 0 && arr[j] * 2 > arr[i]; j--) {
-                int k = -1;
-                if (indices.count(arr[i] - arr[j])) {
-                    k = indices[arr[i] - arr[j]];
+    int cherryPickup(vector<vector<int>> &grid) {
+        int n = grid.size();
+        vector<vector<vector<int>>> f(n * 2 - 1, vector<vector<int>>(n, vector<int>(n, INT_MIN)));
+        f[0][0][0] = grid[0][0];
+        for (int k = 1; k < n * 2 - 1; ++k) {
+            for (int x1 = max(k - n + 1, 0); x1 <= min(k, n - 1); ++x1) {
+                int y1 = k - x1;
+                if (grid[x1][y1] == -1) {
+                    continue;
                 }
-                if (k >= 0) {
-                    dp[j][i] = max(dp[k][j] + 1, 3);
+                for (int x2 = x1; x2 <= min(k, n - 1); ++x2) {
+                    int y2 = k - x2;
+                    if (grid[x2][y2] == -1) {
+                        continue;
+                    }
+                    int res = f[k - 1][x1][x2]; // 都往右
+                    if (x1) {
+                        res = max(res, f[k - 1][x1 - 1][x2]); // 往下，往右
+                    }
+                    if (x2) {
+                        res = max(res, f[k - 1][x1][x2 - 1]); // 往右，往下
+                    }
+                    if (x1 && x2) {
+                        res = max(res, f[k - 1][x1 - 1][x2 - 1]); // 都往下
+                    }
+                    res += grid[x1][y1];
+                    if (x2 != x1) { // 避免重复摘同一个樱桃
+                        res += grid[x2][y2];
+                    }
+                    f[k][x1][x2] = res;
                 }
-                ans = max(ans, dp[j][i]);
             }
         }
-        return ans;
+        return max(f.back().back().back(), 0);
     }
 };
