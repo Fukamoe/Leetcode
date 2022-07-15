513fbc77c84f5216f775175aa11ecc30a9de8a79
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-07-15 23:03:31 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,22 +1,22 @@
-class WordFilter {
-private:
-    unordered_map<string, int> dict;
+class Solution {
 public:
-    WordFilter(vector<string>& words) {
-        for (int i = 0; i < words.size(); i++) {
-            int m = words[i].size();
-            string word = words[i];
-            for (int prefixLength = 1; prefixLength <= m; prefixLength++) {
-                for (int suffixLength = 1; suffixLength <= m; suffixLength++) {
-                    string key = word.substr(0, prefixLength) + '#' + word.substr(m - suffixLength);
-                    dict[key] = i;
-                }
+    Node* intersect(Node* quadTree1, Node* quadTree2) {
+        if (quadTree1->isLeaf) {
+            if (quadTree1->val) {
+                return new Node(true, true);
             }
+            return new Node(quadTree2->val, quadTree2->isLeaf, quadTree2->topLeft, quadTree2->topRight, quadTree2->bottomLeft, quadTree2->bottomRight);
         }
-    }
-    
-    int f(string pref, string suff) {
-        string target = pref + '#' + suff;
-        return dict.count(target) ? dict[target] : -1;
+        if (quadTree2->isLeaf) {
+            return intersect(quadTree2, quadTree1);
+        }
+        Node* o1 = intersect(quadTree1->topLeft, quadTree2->topLeft);
+        Node* o2 = intersect(quadTree1->topRight, quadTree2->topRight);
+        Node* o3 = intersect(quadTree1->bottomLeft, quadTree2->bottomLeft);
+        Node* o4 = intersect(quadTree1->bottomRight, quadTree2->bottomRight);
+        if (o1->isLeaf && o2->isLeaf && o3->isLeaf && o4->isLeaf && o1->val == o2->val && o1->val == o3->val && o1->val == o4->val) {
+            return new Node(o1->val, true);
+        }
+        return new Node(false, false, o1, o2, o3, o4);
     }
 };
