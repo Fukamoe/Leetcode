7d2c8ed8c2d0fdb2e1fc165f9c98c33897b1e0d7
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-07-22 23:03:14 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
-    TreeNode* pruneTree(TreeNode* root) {
-        if (!root) {
-            return nullptr;
+    void help(vector<vector<int>>& intervals, vector<vector<int>>& temp, int pos, int num) {
+        for (int i = pos; i >= 0; i--) {
+            if (intervals[i][1] < num) {
+                break;
+            }
+            temp[i].push_back(num);
         }
-        root->left = pruneTree(root->left);
-        root->right = pruneTree(root->right);
-        if (!root->left && !root->right && !root->val) {
-            return nullptr;
+    }
+
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        int res = 0;
+        int m = 2;
+        sort(intervals.begin(), intervals.end(), [&](vector<int>& a, vector<int>& b) {
+            if (a[0] == b[0]) {
+                return a[1] > b[1];
+            }
+            return a[0] < b[0];
+        });
+        vector<vector<int>> temp(n);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = intervals[i][0], k = temp[i].size(); k < m; j++, k++) {
+                res++;
+                help(intervals, temp, i - 1, j);
+            }
         }
-        return root;
-    }   
+        return res;
+    }
 };
