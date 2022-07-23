1683011586ee413c32576e3dbc0ce8a5a4f4c700
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-07-23 22:54:36 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-    void help(vector<vector<int>>& intervals, vector<vector<int>>& temp, int pos, int num) {
-        for (int i = pos; i >= 0; i--) {
-            if (intervals[i][1] < num) {
-                break;
-            }
-            temp[i].push_back(num);
-        }
-    }
-
-    int intersectionSizeTwo(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        int res = 0;
-        int m = 2;
-        sort(intervals.begin(), intervals.end(), [&](vector<int>& a, vector<int>& b) {
-            if (a[0] == b[0]) {
-                return a[1] > b[1];
-            }
-            return a[0] < b[0];
-        });
-        vector<vector<int>> temp(n);
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = intervals[i][0], k = temp[i].size(); k < m; j++, k++) {
-                res++;
-                help(intervals, temp, i - 1, j);
+    bool sequenceReconstruction(vector<int>& nums, vector<vector<int>>& sequences) {
+        int n = nums.size();
+        vector<int> indegrees(n + 1);
+        vector<unordered_set<int>> graph(n + 1);
+        for (auto &sequence : sequences) {
+            for (int i = 1; i < sequence.size(); i++) {
+                int prev = sequence[i - 1], next = sequence[i];
+                if (!graph[prev].count(next)) {
+                    graph[prev].emplace(next);
+                    indegrees[next]++;
+                }
             }
         }
-        return res;
+        queue<int> qu;
+        for (int i = 1; i <= n; i++) {
+            if (indegrees[i] == 0) {
+                qu.emplace(i);
+            }
+        }
+        while (!qu.empty()) {
+            if (qu.size() > 1) {
+                return false;
+            }
+            int num = qu.front();
+            qu.pop();
+            for (int next : graph[num]) {
+                indegrees[next]--;
+                if (indegrees[next] == 0) {
+                    qu.emplace(next);
+                }
+            }
+        }
+        return true;
     }
 };
