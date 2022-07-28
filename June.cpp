58086b2a2a35b2cad6edec1da0c927d4dde1a63d
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-07-28 21:56:57 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
-    string fractionAddition(string expression) {
-        long long denominator = 0, numerator = 1; // 分子，分母
-        int index = 0, n = expression.size();
-        while (index < n) {
-            // 读取分子
-            long long denominator1 = 0, sign = 1;
-            if (expression[index] == '-' || expression[index] == '+') {
-                sign = expression[index] == '-' ? -1 : 1;
-                index++;
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> sortedArr = arr;
+        sort(sortedArr.begin(), sortedArr.end());
+        unordered_map<int, int> ranks;
+        vector<int> ans(arr.size());
+        for (auto &a : sortedArr) {
+            if (!ranks.count(a)) {
+                ranks[a] = ranks.size() + 1;
             }
-            while (index < n && isdigit(expression[index])) {
-                denominator1 = denominator1 * 10 + expression[index] - '0';
-                index++;
-            }
-            denominator1 = sign * denominator1;
-            index++;
-
-            // 读取分母
-            long long numerator1 = 0;
-            while (index < n && isdigit(expression[index])) {
-                numerator1 = numerator1 * 10 + expression[index] - '0';
-                index++;
-            }
-
-            denominator = denominator * numerator1 + denominator1 * numerator;
-            numerator *= numerator1;
         }
-        if (denominator == 0) {
-            return "0/1";
+        for (int i = 0; i < arr.size(); i++) {
+            ans[i] = ranks[arr[i]];
         }
-        long long g = gcd(abs(denominator), numerator); // 获取最大公约数
-        return to_string(denominator / g) + "/" + to_string(numerator / g);
+        return ans;
     }
 };
