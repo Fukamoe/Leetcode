fb949468afd9d2a89f4805e44ca704f2be7ca470
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-08-01 23:32:27 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,29 +1,9 @@
 class Solution {
-    vector<int> sum;
-
-    void dfs(TreeNode *node, int level) {
-        if (level == sum.size()) {
-            sum.push_back(node->val);
-        } else {
-            sum[level] += node->val;
-        }
-        if (node->left) {
-            dfs(node->left, level + 1);
-        }
-        if (node->right) {
-            dfs(node->right, level + 1);
-        }
-    }
-
 public:
-    int maxLevelSum(TreeNode *root) {
-        dfs(root, 0);
-        int ans = 0;
-        for (int i = 0; i < sum.size(); ++i) {
-            if (sum[i] > sum[ans]) {
-                ans = i;
-            }
+    string generateTheString(int n) {
+        if (n % 2 == 1) {
+            return string(n, 'a');
         }
-        return ans + 1; // 层号从 1 开始
+        return string(n - 1, 'a') + 'b';
     }
 };
