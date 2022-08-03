40174548b9eff80fe966f543b4ddfd15b4b2a93d
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-08-03 22:06:49 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,53 +1,21 @@
-class MyCircularQueue {
-private:
-    int front;
-    int rear;
-    int capacity;
-    vector<int> elements;
-
+class Solution {
 public:
-    MyCircularQueue(int k) {
-        this->capacity = k + 1;
-        this->elements = vector<int>(capacity);
-        rear = front = 0;
-    }
-
-    bool enQueue(int value) {
-        if (isFull()) {
-            return false;
+    string orderlyQueue(string s, int k) {
+        if (k == 1) {
+            string smallest = s;
+            int n = s.size();
+            for (int i = 1; i < n; i++) {
+                char c = s[0];
+                s = s.substr(1);
+                s.push_back(c);
+                if (s < smallest) {
+                    smallest = s;
+                }
+            }
+            return smallest;
+        } else {
+            sort(s.begin(), s.end());
+            return s;
         }
-        elements[rear] = value;
-        rear = (rear + 1) % capacity;
-        return true;
-    }
-
-    bool deQueue() {
-        if (isEmpty()) {
-            return false;
-        }
-        front = (front + 1) % capacity;
-        return true;
-    }
-
-    int Front() {
-        if (isEmpty()) {
-            return -1;
-        }
-        return elements[front];
-    }
-
-    int Rear() {
-        if (isEmpty()) {
-            return -1;
-        }
-        return elements[(rear - 1 + capacity) % capacity];
-    }
-
-    bool isEmpty() {
-        return rear == front;
-    }
-
-    bool isFull() {
-        return ((rear + 1) % capacity) == front;
     }
 };
