1a4f50b035ec22dd84ea61f36f35b4dd89f6da4b
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-08-05 22:30:12 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    vector<int> minSubsequence(vector<int>& nums) {
-        int total = accumulate(nums.begin(), nums.end(), 0);
-        sort(nums.begin(), nums.end());
-        vector<int> ans;
-        int curr = 0;
-        for (int i = nums.size() - 1; i >= 0; --i) {
-            curr += nums[i];
-            ans.emplace_back(nums[i]);
-            if (total - curr < curr) {
-                break;
-            }
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if (root == nullptr) {
+            return nullptr;
         }
-        return ans;
+        if (depth == 1) {
+            return new TreeNode(val, root, nullptr);
+        }
+        if (depth == 2) {
+            root->left = new TreeNode(val, root->left, nullptr);
+            root->right = new TreeNode(val, nullptr, root->right);
+        } else {
+            root->left = addOneRow(root->left, val, depth - 1);
+            root->right = addOneRow(root->right, val, depth - 1);
+        }
+        return root;
     }
 };
