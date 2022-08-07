3cfab4ed555e58e2c08bcca021de58104d22bf20
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-08-07 21:46:46 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-    vector<string> stringMatching(vector<string>& words) {
-        vector<string> ret;
-        for (int i = 0; i < words.size(); i++) {
-            for (int j = 0; j < words.size(); j++) {
-                if (i != j && words[j].find(words[i]) != string::npos) {
-                    ret.push_back(words[i]);
-                    break;
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        stack<pair<int, int>> st; // {idx, 开始运行的时间}
+        vector<int> res(n, 0);
+        for (auto& log : logs) {
+            char type[10];
+            int idx, timestamp;
+            sscanf(log.c_str(), "%d:%[^:]:%d", &idx, type, &timestamp);
+            if (type[0] == 's') {
+                if (!st.empty()) {
+                    res[st.top().first] += timestamp - st.top().second;
+                    st.top().second = timestamp;
+                }
+                st.emplace(idx, timestamp);
+            } else {
+                auto t = st.top();
+                st.pop();
+                res[t.first] += timestamp - t.second + 1;
+                if (!st.empty()) {
+                    st.top().second = timestamp + 1;
                 }
             }
         }
-        return ret;
+        return res;
     }
 };
