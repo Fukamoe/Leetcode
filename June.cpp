ca70a24c96b3c8e38dc6edaee26505467cf9a40d
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-08-11 18:03:21 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
-    string solveEquation(string equation) {
-        int factor = 0, val = 0;
-        int index = 0, n = equation.size(), sign1 = 1; // 等式左边默认系数为正
-        while (index < n) {
-            if (equation[index] == '=') {
-                sign1 = -1; // 等式右边默认系数为负
-                index++;
-                continue;
-            }
-
-            int sign2 = sign1, number = 0;
-            bool valid = false; // 记录 number 是否有效
-            if (equation[index] == '-' || equation[index] == '+') { // 去掉前面的符号
-                sign2 = (equation[index] == '-') ? -sign1 : sign1;
-                index++;
-            }
-            while (index < n && isdigit(equation[index])) {
-                number = number * 10 + (equation[index] - '0');
-                index++;
-                valid = true;
-            }
-
-            if (index < n && equation[index] == 'x') { // 变量
-                factor += valid ? sign2 * number : sign2;
-                index++;
-            } else { // 数值
-                val += sign2 * number;
+    string reformat(string s) {
+        int sum_digit = 0;
+        for (auto& c : s) {
+            if (isdigit(c)) {
+                sum_digit++;
             }
         }
-
-        if (factor == 0) {
-            return val == 0 ? "Infinite solutions" : "No solution";
+        int sum_alpha = s.size() - sum_digit;
+        if (abs(sum_digit - sum_alpha) > 1) {
+            return "";
         }
-        return string("x=") + to_string(-val / factor);
+        bool flag = sum_digit > sum_alpha;
+        for (int i = 0, j = 1; i < s.size(); i += 2) {
+            if (isdigit(s[i]) != flag) {
+                while (isdigit(s[j]) != flag) {
+                    j += 2;
+                }
+                swap(s[i], s[j]);
+            }
+        }
+        return s;
     }
 };
