3b5af1256f04318ecb172647e2469fe3b9d4e657
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-08-14 23:22:28 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int maxChunksToSorted(vector<int>& arr) {
-        unordered_map<int, int> cnt;
-        int res = 0;
-        vector<int> sortedArr = arr;
-        sort(sortedArr.begin(), sortedArr.end());
-        for (int i = 0; i < sortedArr.size(); i++) {
-            int x = arr[i], y = sortedArr[i];
-            cnt[x]++;
-            if (cnt[x] == 0) {
-                cnt.erase(x);
+    int maxScore(string s) {
+        int ans = 0;
+        int n = s.size();
+        for (int i = 1; i < n; i++) {
+            int score = 0;
+            for (int j = 0; j < i; j++) {
+                if (s[j] == '0') {
+                    score++;
+                }
             }
-            cnt[y]--;
-            if (cnt[y] == 0) {
-                cnt.erase(y);
+            for (int j = i; j < n; j++) {
+                if (s[j] == '1') {
+                    score++;
+                }
             }
-            if (cnt.size() == 0) {
-                res++;
-            }
+            ans = max(ans, score);
         }
-        return res;
+        return ans;
     }
 };
