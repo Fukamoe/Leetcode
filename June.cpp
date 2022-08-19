28f1c142ff17b939fb1346d3f9350c9bfd36bfd9
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-08-19 22:29:36 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
-    int maxEqualFreq(vector<int>& nums) {
-        unordered_map<int, int> freq, count;
-        int res = 0, maxFreq = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (count[nums[i]] > 0) {
-                freq[count[nums[i]]]--;
-            }
-            count[nums[i]]++;
-            maxFreq = max(maxFreq, count[nums[i]]);
-            freq[count[nums[i]]]++;
-            bool ok = maxFreq == 1 ||
-                    freq[maxFreq] * maxFreq + freq[maxFreq - 1] * (maxFreq - 1) == i + 1 && freq[maxFreq] == 1 ||
-                    freq[maxFreq] * maxFreq + 1 == i + 1 && freq[1] == 1;
-            if (ok) {
-                res = max(res, i + 1);
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int n = startTime.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+                ans++;
             }
         }
-        return res;
+        return ans;
     }
 };
