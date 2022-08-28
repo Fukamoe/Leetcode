add7f67d6dfc5dce2dddeddf8dea4c8311b9bc11
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-08-28 21:03:57 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-    int widthOfBinaryTree(TreeNode* root) {
-        unsigned long long res = 1;
-        vector<pair<TreeNode *, unsigned long long>> arr;
-        arr.emplace_back(root, 1L);
-        while (!arr.empty()) {
-            vector<pair<TreeNode *, unsigned long long>> tmp;
-            for (auto &[node, index] : arr) {
-                if (node->left) {
-                    tmp.emplace_back(node->left, index * 2);
-                }
-                if (node->right) {
-                    tmp.emplace_back(node->right, index * 2 + 1);
-                }
-            }
-            res = max(res, arr.back().second - arr[0].second + 1);
-            arr = move(tmp);
+    int zeta(long x) {
+        int res = 0;
+        while (x) {
+            res += x / 5;
+            x /= 5;
         }
         return res;
+    }
+
+    long long help(int k) {
+        long long r = 5LL * k;
+        long long l = 0;
+        while (l <= r) {
+            long long mid = (l + r) / 2;
+            if (zeta(mid) < k) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return r + 1;
+    }
+
+    int preimageSizeFZF(int k) {
+        return help(k + 1) - help(k);
     }
 };
