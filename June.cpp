43e297a4efdf466f0048dc59c259339875da63fd
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-08-30 22:41:43 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
-    vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ans(2 * n);
-        for (int i = 0; i < n; i++) {
-            ans[2 * i] = nums[i];
-            ans[2 * i + 1] = nums[i + n];
+    TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+        TreeNode* parent = nullptr;
+        TreeNode* cur = root;
+        while (cur) {
+            if (val > cur->val) {
+                if (!parent) {
+                    return new TreeNode(val, root, nullptr);
+                }
+                TreeNode* node = new TreeNode(val, cur, nullptr);
+                parent->right = node;
+                return root;
+            }
+            else {
+                parent = cur;
+                cur = cur->right;
+            }
         }
-        return ans;
+        parent->right = new TreeNode(val);
+        return root;
     }
 };
