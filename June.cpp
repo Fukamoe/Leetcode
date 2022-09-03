5a9aa6f23fdfd8e254910499e5d8d1b3943f4e5b
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-09-03 22:43:20 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,27 +1,16 @@
 class Solution {
-private:
-    int res;
-
 public:
-    int longestUnivaluePath(TreeNode* root) {
-        res = 0;
-        dfs(root);
-        return res;
-    }
-
-    int dfs(TreeNode *root) {
-        if (root == nullptr) {
-            return 0;
+    int findLongestChain(vector<vector<int>>& pairs) {
+        int n = pairs.size();
+        sort(pairs.begin(), pairs.end());
+        vector<int> dp(n, 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (pairs[i][0] > pairs[j][1]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
         }
-        int left = dfs(root->left), right = dfs(root->right);
-        int left1 = 0, right1 = 0;
-        if (root->left && root->left->val == root->val) {
-            left1 = left + 1;
-        }
-        if (root->right && root->right->val == root->val) {
-            right1 = right + 1;
-        }
-        res = max(res, left1 + right1);
-        return max(left1, right1);
+        return dp[n - 1];
     }
 };
