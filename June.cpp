9e467c616823df7479f22589d692ad399b57bd84
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-09-05 20:39:32 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int numSpecial(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size();
-        vector<int> rowsSum(m);
-        vector<int> colsSum(n);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                rowsSum[i] += mat[i][j];
-                colsSum[j] += mat[i][j];
-            }
-        }
-        int res = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 1 && rowsSum[i] == 1 && colsSum[j] == 1) {
-                    res++;
-                }
-            }
-        }
-        return res;
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        dfs(root);
+        return {repeat.begin(), repeat.end()};
     }
+
+    string dfs(TreeNode* node) {
+        if (!node) {
+            return "";
+        }
+        string serial = to_string(node->val) + "(" + dfs(node->left) + ")(" + dfs(node->right) + ")";
+        if (auto it = seen.find(serial); it != seen.end()) {
+            repeat.insert(it->second);
+        }
+        else {
+            seen[serial] = node;
+        }
+        return serial;
+    }
+
+private:
+    unordered_map<string, TreeNode*> seen;
+    unordered_set<TreeNode*> repeat;
 };
