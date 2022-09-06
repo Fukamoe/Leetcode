5f311a147a5a6f8680231324fa467a51de87e62e
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-09-06 23:24:02 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        dfs(root);
-        return {repeat.begin(), repeat.end()};
+    int uniqueLetterString(string s) {
+        unordered_map<char, vector<int>> index;
+        for (int i = 0; i < s.size(); i++) {
+            index[s[i]].emplace_back(i);
+        }
+        int res = 0;
+        for (auto &&[_, arr]: index) {
+            arr.insert(arr.begin(), -1);
+            arr.emplace_back(s.size());
+            for (int i = 1; i < arr.size() - 1; i++) {
+                res += (arr[i] - arr[i - 1]) * (arr[i + 1] - arr[i]);
+            }
+        }
+        return res;
     }
-
-    string dfs(TreeNode* node) {
-        if (!node) {
-            return "";
-        }
-        string serial = to_string(node->val) + "(" + dfs(node->left) + ")(" + dfs(node->right) + ")";
-        if (auto it = seen.find(serial); it != seen.end()) {
-            repeat.insert(it->second);
-        }
-        else {
-            seen[serial] = node;
-        }
-        return serial;
-    }
-
-private:
-    unordered_map<string, TreeNode*> seen;
-    unordered_set<TreeNode*> repeat;
 };
