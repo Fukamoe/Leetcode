8bc4c755c528beb521308315b7c158fab759dd67
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-09-11 22:05:48 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    TreeNode* trimBST(TreeNode* root, int low, int high) {
-        if (root == nullptr) {
-            return nullptr;
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        int n = quality.size();
+        vector<int> h(n, 0);
+        iota(h.begin(), h.end(), 0);
+        sort(h.begin(), h.end(), [&](int& a, int& b) {
+            return quality[a] * wage[b] > quality[b] * wage[a];
+        });
+        double res = 1e9;
+        double totalq = 0.0;
+        priority_queue<int, vector<int>, less<int>> q;
+        for (int i = 0; i < k - 1; i++) {
+            totalq += quality[h[i]];
+            q.push(quality[h[i]]);
         }
-        if (root->val < low) {
-            return trimBST(root->right, low, high);
-        } else if (root->val > high) {
-            return trimBST(root->left, low, high);
-        } else {
-            root->left = trimBST(root->left, low, high);
-            root->right = trimBST(root->right, low, high);
-            return root;
+        for (int i = k - 1; i < n; i++) {
+            int idx = h[i];
+            totalq += quality[idx];
+            q.push(quality[idx]);
+            double totalc = ((double) wage[idx] / quality[idx]) * totalq;
+            res = min(res, totalc);
+            totalq -= q.top();
+            q.pop();
         }
+        return res;
     }
 };
