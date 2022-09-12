ea99a16951bcf723c83602479f7fc0d581d85735
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-09-12 22:15:32 +0800

    Create June.cpp

--- June.cpp
+++ June.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
-    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
-        int n = quality.size();
-        vector<int> h(n, 0);
-        iota(h.begin(), h.end(), 0);
-        sort(h.begin(), h.end(), [&](int& a, int& b) {
-            return quality[a] * wage[b] > quality[b] * wage[a];
-        });
-        double res = 1e9;
-        double totalq = 0.0;
-        priority_queue<int, vector<int>, less<int>> q;
-        for (int i = 0; i < k - 1; i++) {
-            totalq += quality[h[i]];
-            q.push(quality[h[i]]);
+    int specialArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), greater<int>());
+        int n = nums.size();
+        for (int i = 1; i <= n; ++i) {
+            if (nums[i - 1] >= i && (i == n || nums[i] < i)) {
+                return i;
+            }
         }
-        for (int i = k - 1; i < n; i++) {
-            int idx = h[i];
-            totalq += quality[idx];
-            q.push(quality[idx]);
-            double totalc = ((double) wage[idx] / quality[idx]) * totalq;
-            res = min(res, totalc);
-            totalq -= q.top();
-            q.pop();
-        }
-        return res;
+        return -1;
     }
 };
