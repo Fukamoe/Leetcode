180d05de6e7b45fc59cd11d38192effc8bfeea51
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-09-15 20:44:44 +0800

    Create June.cpp

--- June.cpp
+++ June.cpp
@@ -1,9 +1,27 @@
 class Solution {
 public:
-    double trimMean(vector<int>& arr) {
-        int n = arr.size();
-        sort(arr.begin(), arr.end());
-        int partialSum = accumulate(arr.begin() + n / 20, arr.begin() + (19 * n / 20), 0);
-        return partialSum / (n * 0.9);
+    int flipLights(int n, int presses) {
+        unordered_set<int> seen;
+        for (int i = 0; i < 1 << 4; i++) {
+            vector<int> pressArr(4);
+            for (int j = 0; j < 4; j++) {
+                pressArr[j] = (i >> j) & 1;
+            }
+            int sum = accumulate(pressArr.begin(), pressArr.end(), 0);
+            if (sum % 2 == presses % 2 && sum <= presses) {
+                int status = pressArr[0] ^ pressArr[1] ^ pressArr[3];
+                if (n >= 2) {
+                    status |= (pressArr[0] ^ pressArr[1]) << 1;
+                }
+                if (n >= 3) {
+                    status |= (pressArr[0] ^ pressArr[2]) << 2;
+                }
+                if (n >= 4) {
+                    status |= (pressArr[0] ^ pressArr[1] ^ pressArr[3]) << 3;
+                }
+                seen.emplace(status);
+            }
+        }
+        return seen.size();
     }
 };
