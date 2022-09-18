0336d91e2cc5560a35d245afb24ecc087d61fddb
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-09-18 22:02:10 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,15 +1,1 @@
-class Solution {
-public:
-    int maxLengthBetweenEqualCharacters(string s) {
-        vector<int> firstIndex(26, -1);
-        int maxLength = -1;
-        for (int i = 0; i < s.size(); i++) {
-            if (firstIndex[s[i] - 'a'] < 0) {
-                firstIndex[s[i] - 'a'] = i;
-            } else {
-                maxLength = max(maxLength, i - firstIndex[s[i] - 'a'] - 1);
-            }
-        }
-        return maxLength;
-    }
-};
+https://www.bilibili.com/video/BV1A8411b7DW?is_story_h5=false&p=1&share_from=ugc&share_medium=android&share_plat=android&share_session_id=edffff37-2bee-4e5a-b2c6-47877189d952&share_source=COPY&share_tag=s_i&timestamp=1663508347&unique_k=dyAnrPZ
