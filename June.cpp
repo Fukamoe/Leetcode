e2e4513fd298ee0be39037d674a4500c042b4ad8
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-09-20 19:52:46 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
-    vector<int> frequencySort(vector<int>& nums) {
-        unordered_map<int, int> cnt;
-        for (int num : nums) {
-            cnt[num]++;
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int all = accumulate(nums.begin(), nums.end(), 0);
+        if (all % k > 0) {
+            return false;
         }
-        sort(nums.begin(), nums.end(), [&](const int a, const int b) {
-            if (cnt[a] != cnt[b]) {
-                return cnt[a] < cnt[b];
+        int per = all / k; 
+        sort(nums.begin(), nums.end());
+        if (nums.back() > per) {
+            return false;
+        }
+        int n = nums.size();
+        vector<bool> dp(1 << n, true);
+        function<bool(int,int)> dfs = [&](int s, int p)->bool {
+            if (s == 0) {
+                return true;
             }
-            return a > b;
-        });
-        return nums;
+            if (!dp[s]) {
+                return dp[s];
+            }
+            dp[s] = false;
+            for (int i = 0; i < n; i++) {
+                if (nums[i] + p > per) {
+                    break;
+                }
+                if ((s >> i) & 1) {
+                    if (dfs(s ^ (1 << i), (p + nums[i]) % per)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        };
+        return dfs((1 << n) - 1, 0);
     }
 };
