ddfac20652bbcb97aa91973a4c61c47a9e021104
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-09-28 23:02:32 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
-    bool CheckPermutation(string s1, string s2) {
-        if (s1.length() != s2.length()) {
-            return false;
+    int getKthMagicNumber(int k) {
+        vector<int> factors = {3, 5, 7};
+        unordered_set<long> seen;
+        priority_queue<long, vector<long>, greater<long>> heap;
+        seen.insert(1L);
+        heap.push(1L);
+        int magic = 0;
+        for (int i = 0; i < k; i++) {
+            long curr = heap.top();
+            heap.pop();
+            magic = (int)curr;
+            for (int factor : factors) {
+                long next = curr * factor;
+                if (!seen.count(next)) {
+                    seen.insert(next);
+                    heap.push(next);
+                }
+            }
         }
-        sort(s1.begin(), s1.end());
-        sort(s2.begin(), s2.end());
-        return s1 == s2;
+        return magic;
     }
 };
