0feceec061ffb992e374333fc3c5fa7c947b656e
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-09-30 22:52:34 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    bool isFlipedString(string s1, string s2) {
-        int m = s1.size(), n = s2.size();
-        if (m != n) {
-            return false;
-        }
-        if (n == 0) {
-            return true;
-        }
-        for (int i = 0; i < n; i++) {
-            bool flag = true;
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> row(m), col(n);
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (s1[(i + j) % n] != s2[j]) {
-                    flag = false;
-                    break;
+                if (!matrix[i][j]) {
+                    row[i] = col[j] = true;
                 }
             }
-            if (flag) {
-                return true;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        return false;
     }
 };
