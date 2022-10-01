c5bd0218920204a8ffc3b094156d52684a111bff
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-10-01 22:16:20 +0800

    Create June.cpp

--- June.cpp
+++ June.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<int> row(m), col(n);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!matrix[i][j]) {
-                    row[i] = col[j] = true;
-                }
+    string reformatNumber(string number) {
+        string digits;
+        for (char ch: number) {
+            if (isdigit(ch)) {
+                digits.push_back(ch);
             }
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (row[i] || col[j]) {
-                    matrix[i][j] = 0;
+
+        int n = digits.size();
+        int pt = 0;
+        string ans;
+        while (n) {
+            if (n > 4) {
+                ans += digits.substr(pt, 3) + "-";
+                pt += 3;
+                n -= 3;
+            }
+            else {
+                if (n == 4) {
+                    ans += digits.substr(pt, 2) + "-" + digits.substr(pt + 2, 2);
                 }
+                else {
+                    ans += digits.substr(pt, n);
+                }
+                break;
             }
         }
+        return ans;
     }
 };
