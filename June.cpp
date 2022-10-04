f8292dabb02008fb2e6c45dd9da7c03d6c29e71c
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-10-04 21:55:35 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -4,3 +4,23 @@ public:
         return s.find("01") == string::npos;
     }
 };
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int ans = 0;
+        int leftCount = 0;
+        for (auto &c : s) {
+            if (c == '(') {
+                leftCount++;
+            } else {
+                if (leftCount > 0) {
+                    leftCount--;
+                } else {
+                    ans++;
+                }
+            }
+        }
+        ans += leftCount;
+        return ans;
+    }
+};
