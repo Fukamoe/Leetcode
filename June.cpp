fe0222eaa501f6fb810dacfb04d273092702d8ae
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-10-17 23:00:44 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    bool dfs(int curnode, int nowcolor, vector<int>& color, const vector<vector<int>>& g) {
-        color[curnode] = nowcolor;
-        for (auto& nextnode : g[curnode]) {
-            if (color[nextnode] && color[nextnode] == color[curnode]) {
-                return false;
-            }
-            if (!color[nextnode] && !dfs(nextnode, 3 ^ nowcolor, color, g)) {
-                return false;
-            }
-        }
-        return true;
-    }
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        unordered_map<int, int> cnt;
 
-    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        vector<int> color(n + 1, 0);
-        vector<vector<int>> g(n + 1);
-        for (auto& p : dislikes) {
-            g[p[0]].push_back(p[1]);
-            g[p[1]].push_back(p[0]);
-        }
-        for (int i = 1; i <= n; ++i) {
-            if (color[i] == 0 && !dfs(i, 1, color, g)) {
-                return false;
+        int left = 0, ans = 0;
+        for (int right = 0; right < n; ++right) {
+            ++cnt[fruits[right]];
+            while (cnt.size() > 2) {
+                auto it = cnt.find(fruits[left]);
+                --it->second;
+                if (it->second == 0) {
+                    cnt.erase(it);
+                }
+                ++left;
             }
+            ans = max(ans, right - left + 1);
         }
-        return true;
+        return ans;
     }
 };
