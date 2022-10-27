b348c56df2cb7b1999de689c6fe2511fdc1abd8c
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-10-27 23:36:26 +0800

    Create June.cpp

--- June.cpp
+++ June.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    int shortestSubarray(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<long> preSumArr(n + 1);
-        for (int i = 0; i < n; i++) {
-            preSumArr[i + 1] = preSumArr[i] + nums[i];
-        }
-        int res = n + 1;
-        deque<int> qu;
-        for (int i = 0; i <= n; i++) {
-            long curSum = preSumArr[i];
-            while (!qu.empty() && curSum - preSumArr[qu.front()] >= k) {
-                res = min(res, i - qu.front());
-                qu.pop_front();
+    int arraySign(vector<int>& nums) {
+        int sign = 1;
+        for (auto num : nums) {
+            if (num == 0) {
+                return 0;
             }
-            while (!qu.empty() && preSumArr[qu.back()] >= curSum) {
-                qu.pop_back();
+            if (num < 0) {
+                sign = -sign;
             }
-            qu.push_back(i);
         }
-        return res < n + 1 ? res : -1;
+        return sign;
     }
 };
