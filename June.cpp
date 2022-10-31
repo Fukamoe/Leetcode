509bee081243e7ceabf2a9e6580112492268dc2b
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-10-31 22:26:33 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    vector<string> letterCasePermutation(string s) {
-        vector<string> ans;
-        queue<string> qu;
-        qu.emplace("");
-        while (!qu.empty()) {
-            string &curr = qu.front();
-            if (curr.size() == s.size()) {
-                ans.emplace_back(curr);
-                qu.pop();
-            } else {
-                int pos = curr.size();
-                if (isalpha(s[pos])) {
-                    string next = curr;
-                    next.push_back(s[pos] ^ 32);
-                    qu.emplace(next);
-                }
-                curr.push_back(s[pos]);                
-            }
+    int magicalString(int n) {
+        if (n < 4) {
+            return 1;
         }
-        return ans;
+        string s(n, '0');
+        s[0] = '1', s[1] = '2', s[2] = '2';
+        int res = 1;
+        int i = 2;
+        int j = 3;
+        while (j < n) {
+            int size = s[i] - '0';
+            int num = 3 - (s[j - 1] - '0');
+            while (size > 0 && j < n) {
+                s[j] = '0' + num;
+                if (num == 1) {
+                    ++res;
+                }
+                ++j;
+                --size;
+            }
+            ++i;
+        }
+        return res;
     }
 };
