6923cbc0ff9e2930c442596d7b84dd42e685b15a
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-11-01 23:28:39 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -25,3 +25,17 @@ public:
         return res;
     }
 };
+class Solution {
+public:
+    string join(vector<string>& words) {
+        string ret = "";
+        for (auto &s : words) {
+            ret += s;
+        }
+        return ret;
+    }
+
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        return join(word1) == join(word2);
+    }
+};
