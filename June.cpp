9698c93354e561abc0c0ab7fc47941cc8e6d05ab
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-11-04 22:56:32 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int maxRepeating(string sequence, string word) {
-        int n = sequence.size(), m = word.size();
-        if (n < m) {
-            return 0;
+    int reachNumber(int target) {
+        target = abs(target);
+        int k = 0;
+        while (target > 0) {
+            k++;
+            target -= k;
         }
-
-        vector<int> f(n);
-        for (int i = m - 1; i < n; ++i) {
-            bool valid = true;
-            for (int j = 0; j < m; ++j) {
-                if (sequence[i - m + j + 1] != word[j]) {
-                    valid = false;
-                    break;
-                }
-            }
-            if (valid) {
-                f[i] = (i == m - 1 ? 0 : f[i - m]) + 1;
-            }
+        return target % 2 == 0 ? k : k + 1 + k % 2;
+    }
+};
+class Solution {
+public:
+    int reachNumber(int target) {
+        target = abs(target);
+        int k = 0;
+        while (target > 0) {
+            k++;
+            target -= k;
         }
-        
-        return *max_element(f.begin(), f.end());
+        return target % 2 == 0 ? k : k + 1 + k % 2;
     }
 };
