b0c4459b6f6ff86804f5dbee94a00894a973cc84
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-11-13 22:28:06 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,15 +1,13 @@
-const long long mod = 1e9 + 7;
 class Solution {
 public:
-    int numTilings(int n) {
-        vector<vector<long long>> dp(n + 1, vector<long long>(4));
-        dp[0][3] = 1;
-        for (int i = 1; i <= n; i++) {
-            dp[i][0] = dp[i - 1][3];
-            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod;
-            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % mod;
-            dp[i][3] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][3]) % mod;
+    string customSortString(string order, string s) {
+        vector<int> val(26);
+        for (int i = 0; i < order.size(); ++i) {
+            val[order[i] - 'a'] = i + 1;
         }
-        return dp[n][3];
+        sort(s.begin(), s.end(), [&](char c0, char c1) {
+            return val[c0 - 'a'] < val[c1 - 'a'];
+        });
+        return s;
     }
 };
