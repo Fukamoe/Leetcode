7c2f8d4d2f3bb01ef7c77000b7bddee3a3e97032
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-11-20 22:33:30 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
-    int largestAltitude(vector<int>& gain) {
-        int ans = 0, sum = 0;
-        for (int x: gain) {
-            sum += x;
-            ans = max(ans, sum);
-        }
-        return ans;
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<double> row = {(double)poured};
+        for (int i = 1; i <= query_row; i++) {
+            vector<double> nextRow(i + 1, 0.0);
+            for (int j = 0; j < row.size(); j++) {
+                double volume = row[j];
+                if (volume > 1) {
+                    nextRow[j] += (volume - 1) / 2;
+                    nextRow[j + 1] += (volume - 1) / 2;
+                }
+            }
+            row = nextRow;
+        }            
+        return min(1.0, row[query_glass]);
     }
 };
