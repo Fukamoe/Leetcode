78003326f0f6022cbf74a8a83361ca28951ce868
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-11-22 22:53:57 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    double soupServings(int n) {
-        n = ceil((double) n / 25);
-        if (n >= 179) {
-            return 1.0;
-        }
-        vector<vector<double>> dp(n + 1, vector<double>(n + 1));
-        dp[0][0] = 0.5;
-        for (int i = 1; i <= n; i++) {
-            dp[0][i] = 1.0;
-        }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = (dp[max(0, i - 4)][j] + dp[max(0, i - 3)][max(0, j - 1)] +
-                           dp[max(0, i - 2)][max(0, j - 2)] + dp[max(0, i - 1)][max(0, j - 3)]) / 4.0;
+    const int MOD = 1e9 + 7;
+    int nthMagicalNumber(int n, int a, int b) {
+        long long l = min(a, b);
+        long long r = (long long) n * min(a, b);
+        int c = lcm(a, b);
+        while (l <= r) {
+            long long mid = (l + r) / 2;
+            long long cnt = mid / a + mid / b - mid / c;
+            if (cnt >= n) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
-        return dp[n][n];
+        return (r + 1) % MOD;
     }
 };
