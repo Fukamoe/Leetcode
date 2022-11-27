5188e544ad88153947265f4f0bc4d214c5656d66
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-11-27 22:36:24 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,42 +1,21 @@
 class Solution {
 public:
-    int encode(int u, int v, int n) {
-        return u * n + v;
-    }
-
-    int reachableNodes(vector<vector<int>>& edges, int maxMoves, int n) {
-        vector<vector<pair<int, int>>> adList(n);
-        for (auto &edge : edges) {
-            int u = edge[0], v = edge[1], nodes = edge[2];
-            adList[u].emplace_back(v, nodes);
-            adList[v].emplace_back(u, nodes);
-        }
-
-        unordered_map<int, int> used;
-        unordered_set<int> visited;
-        int reachableNodes = 0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.emplace(0, 0);
-        while (!pq.empty() && pq.top().first <= maxMoves) {
-            auto [step, u] = pq.top();
-            pq.pop();
-            if (visited.count(u)) {
-                continue;
-            }
-            visited.emplace(u);
-            reachableNodes++;
-            for (auto [v, nodes] : adList[u]) {
-                if (nodes + step + 1 <= maxMoves && !visited.count(v)) {
-                    pq.emplace(nodes + step + 1, v);
-                }
-                used[encode(u, v, n)] = min(nodes, maxMoves - step);
+    bool check(vector<int>& nums) {
+        int n = nums.size(), x = 0;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] < nums[i - 1]) {
+                x = i;
+                break;
             }
         }
-
-        for (auto &edge : edges) {
-            int u = edge[0], v = edge[1], nodes = edge[2];
-            reachableNodes += min(nodes, used[encode(u, v, n)] + used[encode(v, u, n)]);
+        if (x == 0) {
+            return true;
         }
-        return reachableNodes;
+        for (int i = x + 1; i < n; ++i) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+        }
+        return nums[0] >= nums[n - 1];
     }
 };
