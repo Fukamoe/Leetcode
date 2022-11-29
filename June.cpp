1b4068d5d61208b5529912afc5a8da16f63e0326
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-11-29 21:28:20 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    double largestSumOfAverages(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<double> prefix(n + 1);
-        for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + nums[i];
-        }
-        vector<vector<double>> dp(n + 1, vector<double>(k + 1));
-        for (int i = 1; i <= n; i++) {
-            dp[i][1] = prefix[i] / i;
-        }
-        for (int j = 2; j <= k; j++) {
-            for (int i = j; i <= n; i++) {
-                for (int x = j - 1; x < i; x++) {
-                    dp[i][j] = max(dp[i][j], dp[x][j - 1] + (prefix[i] - prefix[x]) / (i - x));
-                }
+    int minOperations(string s) {
+
+    }
+};class Solution {
+public:
+    int minOperations(string s) {
+        int cnt = 0;
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            if (c != ('0' + i % 2)) {
+                cnt++;
             }
         }
-        return dp[n][k];
+        return min(cnt, (int)s.size() - cnt);
     }
 };
