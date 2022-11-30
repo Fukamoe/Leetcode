219b8180332451b4ce90e08c64bffda2fc5f5736
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-11-30 20:38:37 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -16,3 +16,39 @@ public:
         return min(cnt, (int)s.size() - cnt);
     }
 };
+class Solution {
+public:
+    int minOperations(string s) {
+
+    }
+};class Solution {
+public:
+    int minOperations(string s) {
+        int cnt = 0;
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            if (c != ('0' + i % 2)) {
+                cnt++;
+            }
+        }
+        return min(cnt, (int)s.size() - cnt);
+    }
+};
+class Solution {
+public:
+    int minOperations(string s) {
+
+    }
+};class Solution {
+public:
+    int minOperations(string s) {
+        int cnt = 0;
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            if (c != ('0' + i % 2)) {
+                cnt++;
+            }
+        }
+        return min(cnt, (int)s.size() - cnt);
+    }
+};
