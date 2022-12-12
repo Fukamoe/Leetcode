35054b9659525ca1d85e8f7a0172ecbc5b0de9c8
Fuka <76830297+Fukamoe@users.noreply.github.com>
2022-12-12 21:34:38 +0800

    Update June.cpp

--- June.cpp
+++ June.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums) {
-        int pre = nums[0] - 1, res = 0;
-        for (int num : nums) {
-            pre = max(pre + 1, num);
-            res += pre - num;
+    int beautySum(string s) {
+        int res = 0;
+        for (int i = 0; i < s.size(); i++) {
+            vector<int> cnt(26);
+            int maxFreq = 0;
+            for (int j = i; j < s.size(); j++) {
+                cnt[s[j] - 'a']++;
+                maxFreq = max(maxFreq, cnt[s[j] - 'a']); 
+                int minFreq = s.size();
+                for (int k = 0; k < 26; k++) {
+                    if (cnt[k] > 0) {
+                        minFreq = min(minFreq, cnt[k]);
+                    }
+                }
+                res += maxFreq - minFreq;
+            }
         }
         return res;
     }
